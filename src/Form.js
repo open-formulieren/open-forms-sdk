4c101d22d993d406e97b3ef7b85d691cbc5a2a8d
Sergei Maertens <sergei@maykinmedia.nl>
2021-04-09 16:23:05 +0200

    :sparkles: Add some markup for form rendering

--- src/Form.js
+++ src/Form.js
@@ -9,28 +9,37 @@ import PropTypes from 'prop-types';
  * @param  {Object} options.form The form definition from the Open Forms API
  * @return {JSX}
  */
-const Form = ({ form }) => {
-    return (
-        <>I am a form!</>
+ const Form = ({ form, titleComponent='h2' }) => {
+  const { name, steps } = form;
+  const Title = `${titleComponent}`;
+  return (
+    <div className="card">
+      <header className="card__header">
+        <Title className="title">{name}</Title>
+      </header>
+      <div className="card__body">
+        {JSON.stringify(steps)}
+      </div>
+    </div>
     );
 };
 
 Form.propTypes = {
-    form: PropTypes.shape({
-        uuid: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        loginRequired: PropTypes.bool.isRequired,
-        product: PropTypes.object,
-        slug: PropTypes.string.isRequired,
-        url: PropTypes.string.isRequired,
-        steps: PropTypes.arrayOf(PropTypes.shape({
-            uuid: PropTypes.string.isRequired,
-            formDefinition: PropTypes.string.isRequired,
-            index: PropTypes.number.isRequired,
-            url: PropTypes.string.isRequired,
-        })).isRequired,
-    }).isRequired,
+  titleComponent: PropTypes.string,
+  form: PropTypes.shape({
+    uuid: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    loginRequired: PropTypes.bool.isRequired,
+    product: PropTypes.object,
+    slug: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    steps: PropTypes.arrayOf(PropTypes.shape({
+      uuid: PropTypes.string.isRequired,
+      formDefinition: PropTypes.string.isRequired,
+      index: PropTypes.number.isRequired,
+      url: PropTypes.string.isRequired,
+    })).isRequired,
+  }).isRequired,
 };
-
 
 export { Form };
