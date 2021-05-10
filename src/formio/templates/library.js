229bed2fdc72fc13f0302ee77bf31d8e3077eacb
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2021-05-10 14:54:13 +0200

    :wrench: Register templates

--- src/formio/templates/library.js
+++ src/formio/templates/library.js
@@ -1,8 +1,16 @@
-import { default as TextTemplate } from './text';
+import { default as ButtonTemplate } from './button';
+import { default as CheckboxTemplate } from './checkbox';
 import { default as LabelTemplate } from './label';
+import { default as RadioTemplate } from './radio';
+import { default as SelectTemplate } from './select';
+import { default as TextTemplate } from './text';
 
 const OFLibrary = {
+  button: {form: ButtonTemplate},
+  checkbox: {form: CheckboxTemplate},
   label: {form: LabelTemplate},
+  radio: {form: RadioTemplate},
+  select: {form: SelectTemplate},
   input: {form: TextTemplate},
 };
 
