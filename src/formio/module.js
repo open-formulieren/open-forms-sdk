5002981ffd32c6be8fa3a186bc07411603e08b69
silvia <silvia@maykinmedia.nl>
2021-05-14 14:54:36 +0200

    :sparkles: Added IBAN field

--- src/formio/module.js
+++ src/formio/module.js
@@ -1,10 +1,12 @@
 import TextField from './components/TextField';
 import TextArea from './components/TextArea';
+import IBANField from "./components/IBANField";
 
 const FormIOModule = {
   components: {
     textfield: TextField,
     textarea: TextArea,
+    iban: IBANField,
   },
 };
 
