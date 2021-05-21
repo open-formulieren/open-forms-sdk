ca3400db899e821d7b3027721012d07e0cd238c3
Sergei Maertens <sergei@maykinmedia.nl>
2021-05-21 14:52:50 +0200

    :lipstick: Style free content in the form

--- src/formio/components/Content.js
+++ src/formio/components/Content.js
@@ -6,12 +6,6 @@ import { applyPrefix } from '../utils';
  * Extend the default text field to modify it to our needs.
  */
 class Content extends Formio.Components.components.content {
-  get inputInfo() {
-    const info = super.inputInfo;
-    // change the default CSS classes
-    info.attr.class = applyPrefix('input');
-    return info;
-  }
 }
 
 
