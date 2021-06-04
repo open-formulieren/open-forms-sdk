02fb18ac03c24a6ddee04c388149373b1f742346
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2021-06-04 15:36:17 +0200

    :construction: Add template to custom templates

--- src/formio/templates/component.js
+++ src/formio/templates/component.js
@@ -1,22 +1,12 @@
 import { applyPrefix } from '../utils';
 
-// const TEMPLATE = `
-// <div id="{{ctx.id}}" class="{{ctx.classes}}"{% if (ctx.styles) { %} styles="{{ctx.styles}}"{% } %} ref="component">
-//   <div ref="messageContainer" class="${applyPrefix('errors')}"></div>
-//   {% if (ctx.visible) { %}
-//   {{ctx.children}}
-//   {% } %}
-// </div>
-// `;
-
-
 const TEMPLATE = `
-<div id="{{ctx.id}}" class="{{ctx.classes}}" style="display: table;width: 100%; table-layout: fixed;" ref="component">
-    <div style="display: table-row">
-        {{ctx.children}}
-    </div>
+<div id="{{ctx.id}}" class="{{ctx.classes}}"{% if (ctx.styles) { %} styles="{{ctx.styles}}"{% } %} ref="component">
+  <div ref="messageContainer" class="${applyPrefix('errors')}"></div>
+  {% if (ctx.visible) { %}
+  {{ctx.children}}
+  {% } %}
 </div>
 `;
-
 
 export default TEMPLATE;
