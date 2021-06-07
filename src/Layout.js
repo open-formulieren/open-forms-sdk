127c46096d8416755d816f6dfbbe940677a4aca9
Sergei Maertens <sergei@maykinmedia.nl>
2021-06-07 17:55:44 +0200

    :lipstick: Add Layout components

---
+++ src/Layout.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {getBEMClassName} from './utils';
+
+
+const Layout = ({ children, component='div' }) => {
+  const className = getBEMClassName('layout');
+  const Component = `${component}`;
+  return (
+    <Component className={className}>
+      {children}
+    </Component>
+  );
+};
+
+Layout.propTypes = {
+  children: PropTypes.node,
+  component: PropTypes.string,
+};
+
+
+
+const LayoutRow = ({ children, modifiers=[], component='section' }) => {
+  const className = getBEMClassName('layout__row', modifiers);
+  const Component = `${component}`;
+  return (
+    <Component className={className}>
+      {children}
+    </Component>
+  );
+};
+
+LayoutRow.propTypes = {
+  children: PropTypes.node,
+  modifiers: PropTypes.arrayOf(PropTypes.string),
+  component: PropTypes.string,
+};
+
+
+
+const LayoutColumn = ({ children, modifiers=[], component='div' }) => {
+  const className = getBEMClassName('layout__column', modifiers);
+  const Component = `${component}`;
+  return (
+    <Component className={className}>
+      {children}
+    </Component>
+  );
+};
+
+LayoutColumn.propTypes = {
+  children: PropTypes.node,
+  modifiers: PropTypes.arrayOf(PropTypes.string),
+  component: PropTypes.string,
+};
+
+
+export default Layout;
+export {Layout, LayoutRow, LayoutColumn};
