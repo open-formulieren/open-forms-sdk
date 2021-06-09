b2c5c76e6247fa374d0fb2f2f023d3d7a76db987
Sergei Maertens <sergei@maykinmedia.nl>
2021-06-09 14:54:21 +0200

    :sparkles: Correctly highlight currently active step in sidebar

--- src/FormStepsSidebar.js
+++ src/FormStepsSidebar.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useRouteMatch } from 'react-router-dom';
 
 import Anchor from './Anchor';
 import Card from './Card';
 import List from './List';
 
 
+const getModifiers = (active) => {
+  const base = ['inherit', 'hover'];
+  const extra = active
+    ? ['active']
+    : ['muted', 'indent'];
+  return [...base, ...extra];
+};
+
+
+
 const FormStepsSidebar = ({ title, steps }) => {
+  const summaryMatch = useRouteMatch("/overzicht");
+  const stepMatch = useRouteMatch("/stap/:step");
+  const isStartPage = summaryMatch == null && stepMatch == null;
+  // figure out the slug from the currently active step IF we're looking at a step
+  const stepSlug = stepMatch ? stepMatch.params.step : '';
+
   return (
     <Card caption={title} captionComponent="h3">
       <List ordered>
+        <Anchor href="#" modifiers={getModifiers(isStartPage)}>Inloggen</Anchor>
         {
           steps.map( step => (
-            <Anchor href="#" modifiers={['inherit',/* 'active',*/ 'hover', 'muted', 'indent']} key={step.uuid}>
+            <Anchor href="#" modifiers={getModifiers(step.slug === stepSlug)} key={step.uuid}>
               {step.formDefinition}
             </Anchor>
           ) )
         }
+        <Anchor href="#" modifiers={getModifiers(!!summaryMatch)}>Overzicht</Anchor>
       </List>
     </Card>
   );
@@ -29,6 +48,7 @@ FormStepsSidebar.propTypes = {
     url: PropTypes.string.isRequired,
     uuid: PropTypes.string.isRequired,
     index: PropTypes.number.isRequired,
+    slug: PropTypes.string.isRequired,
     formDefinition: PropTypes.string.isRequired,
   })).isRequired,
 };
