2165b932ce0d85201e5d6d2be972e192ac8d0285
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2021-06-09 15:56:54 +0200

    :sparkles: Add date component

--- src/formio/module.js
+++ src/formio/module.js
@@ -10,6 +10,7 @@ import Email from "./components/Email";
 import Currency from './components/Currency';
 import {default as NumberComponent} from './components/Number';
 import ContentComponent from './components/Content';
+import Date from './components/Date';
 
 const FormIOModule = {
   components: {
@@ -25,6 +26,7 @@ const FormIOModule = {
     currency: Currency,
     number: NumberComponent,
     content: ContentComponent,
+    date: Date,
   },
 };
 
