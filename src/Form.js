7fd607822794e649453e1fa1975f53182214c123
Sergei Maertens <sergei@maykinmedia.nl>
2021-06-09 10:52:56 +0200

    :sparkles: Extract the base path for future URL routing

--- src/Form.js
+++ src/Form.js
@@ -67,7 +67,9 @@ const reducer = (draft, action) => {
  * @param  {Object} options.form The form definition from the Open Forms API
  * @return {JSX}
  */
- const Form = ({ form }) => {
+ const Form = ({ base, form }) => {
+  console.log(base);
+
   // extract the declared properties and configuration
   const {steps} = form;
   const config = useContext(ConfigContext);
@@ -131,6 +133,7 @@ const reducer = (draft, action) => {
 };
 
 Form.propTypes = {
+  base: PropTypes.string.isRequired,
   form: PropTypes.shape({
     uuid: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
