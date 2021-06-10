7c88c995b3931c37f7418aee4e34d5ea3e599def
Sergei Maertens <sergei@maykinmedia.nl>
2021-06-10 15:29:41 +0200

    :sparkles: Display step progress in sidebar

--- src/FormStepsSidebar.js
+++ src/FormStepsSidebar.js
@@ -5,34 +5,67 @@ import { useRouteMatch } from 'react-router-dom';
 import Anchor from './Anchor';
 import Card from './Card';
 import List from './List';
+import FAIcon from './FAIcon';
 
 
-const getModifiers = (active) => {
-  const base = ['inherit', 'hover'];
-  const extra = active
-    ? ['active']
-    : ['muted', 'indent'];
-  return [...base, ...extra];
+const getModifiers = (active, completed) => {
+  return [
+    'inherit',
+    'hover',
+    active ? 'active' : 'muted',
+    completed ? undefined : 'indent',
+  ].filter( mod => mod !== undefined );
 };
 
 
 
-const FormStepsSidebar = ({ title, steps }) => {
+const SidebarStepStatus = ({isCurrent, step, completed=false}) => {
+  return (
+    <Anchor href="#" modifiers={getModifiers(isCurrent, completed)}>
+      { completed ? <FAIcon icon="check" modifiers={['small']} aria-hidden="true" /> : null }
+      {step.formDefinition}
+    </Anchor>
+  );
+};
+
+
+SidebarStepStatus.propTypes = {
+  isCurrent: PropTypes.bool.isRequired,
+  completed: PropTypes.bool,
+  step: PropTypes.shape({
+    url: PropTypes.string.isRequired,
+    uuid: PropTypes.string.isRequired,
+    index: PropTypes.number.isRequired,
+    slug: PropTypes.string.isRequired,
+    formDefinition: PropTypes.string.isRequired,
+  }),
+};
+
+
+const FormStepsSidebar = ({ title, submission, steps }) => {
   const summaryMatch = useRouteMatch("/overzicht");
   const stepMatch = useRouteMatch("/stap/:step");
   const isStartPage = summaryMatch == null && stepMatch == null;
   // figure out the slug from the currently active step IF we're looking at a step
   const stepSlug = stepMatch ? stepMatch.params.step : '';
+  const hasSubmission = !!submission;
 
   return (
     <Card caption={title} captionComponent="h3">
       <List ordered>
-        <Anchor href="#" modifiers={getModifiers(isStartPage)}>Inloggen</Anchor>
+        <Anchor href="#" modifiers={getModifiers(isStartPage, hasSubmission)}>
+          { hasSubmission ? <FAIcon icon="check" modifiers={['small']} aria-hidden="true" /> : null }
+          Inloggen
+        </Anchor>
         {
-          steps.map( step => (
-            <Anchor href="#" modifiers={getModifiers(step.slug === stepSlug)} key={step.uuid}>
-              {step.formDefinition}
-            </Anchor>
+          steps.map( (step, index) => (
+            <SidebarStepStatus
+              key={step.uuid}
+              step={step}
+              completed={submission && submission.steps[index].completed}
+              isCurrent={step.slug === stepSlug}
+              slug={step.slug}
+            />
           ) )
         }
         <Anchor href="#" modifiers={getModifiers(!!summaryMatch)}>Overzicht</Anchor>
@@ -44,6 +77,13 @@ const FormStepsSidebar = ({ title, steps }) => {
 
 FormStepsSidebar.propTypes = {
   title: PropTypes.string,
+  submission: PropTypes.shape({
+    steps: PropTypes.arrayOf(PropTypes.shape({
+      completed: PropTypes.bool.isRequired,
+      available: PropTypes.bool.isRequired,
+      // and more...
+    })),
+  }),
   steps: PropTypes.arrayOf(PropTypes.shape({
     url: PropTypes.string.isRequired,
     uuid: PropTypes.string.isRequired,
