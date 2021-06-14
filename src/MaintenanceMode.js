5e4538af37fa77354abd6fdaf78708b62d3d116c
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2021-06-14 14:56:12 +0200

    :memo: Update on screen text

--- src/MaintenanceMode.js
+++ src/MaintenanceMode.js
@@ -7,7 +7,6 @@ const MaintenanceMode = ({ title }) => {
     <Card title={title}>
       <Body>
         This form is currently undergoing maintenance and can not be accessed at the moment.
-        Please try again later.
       </Body>
     </Card>
   )
