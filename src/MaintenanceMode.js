8fce0f2e1e7ad0b23ec1afbc267ae55cf641f162
Sergei Maertens <sergei@maykinmedia.nl>
2021-06-21 17:45:21 +0200

    :lipstick: Visually make alert clear

--- src/MaintenanceMode.js
+++ src/MaintenanceMode.js
@@ -1,13 +1,23 @@
 import Body from "./Body";
 import Card from "./Card";
+import FAIcon from "./FAIcon";
 
+
+import {getBEMClassName} from './utils';
 
 const MaintenanceMode = ({ title }) => {
+  const alertClassName = getBEMClassName('alert', ['info']);
   return (
     <Card title={title}>
-      <Body>
-        This form is currently undergoing maintenance and can not be accessed at the moment.
-      </Body>
+      <div className={alertClassName}>
+        <span className={getBEMClassName('alert__icon')}>
+          <FAIcon icon="info" />
+        </span>
+
+        <Body>
+          This form is currently undergoing maintenance and can not be accessed at the moment.
+        </Body>
+      </div>
     </Card>
   )
 };
