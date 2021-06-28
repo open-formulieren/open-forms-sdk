759c1ee30552b2e0e6bb2b86364ff012787cf5bf
sheameyers <shea.meyers@maykinmedia.nl>
2021-06-28 13:31:05 +0200

    :sparkles: [Refs open-formulieren/open-forms#200] Add phone number component

---
+++ src/formio/components/PhoneNumberField.js
@@ -0,0 +1,41 @@
+import {Formio} from "formiojs";
+import {applyPrefix} from "../utils";
+
+const PhoneNumber = Formio.Components.components.phoneNumber;
+
+
+const PhoneNumberValidator = {
+  key: "validate.phoneNumber",
+  hasLabel: true,
+  message(component) {
+    return component.t(component.errorMessage('Invalid Phone Number'), {
+      field: component.errorLabel,
+      data: component.data
+    });
+  },
+  check(component, setting, value) {
+    if (!value || value === '+') {
+      return true;
+    }
+    let regex = /^\+{0,1}[- 0-9]{1,}$/;
+    return regex.test(value);
+  }
+};
+
+
+class PhoneNumberField extends PhoneNumber {
+
+    get inputInfo() {
+      const info = super.inputInfo;
+      // change the default CSS classes
+      info.attr.class = applyPrefix('input');
+      return info;
+    }
+
+    init() {
+      super.init();
+      this.validator.validators.custom = PhoneNumberValidator;
+    }
+}
+
+export default PhoneNumberField;
