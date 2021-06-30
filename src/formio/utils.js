d7160636992516527386213610fc1e9364a58e16
sheameyers <shea.meyers@maykinmedia.nl>
2021-06-30 14:39:47 +0200

    :recycle: [Refs open-formulieren/open-forms#213] Remove redundant check in validating bsn function

--- src/formio/utils.js
+++ src/formio/utils.js
@@ -13,15 +13,11 @@ const isValidBsn = (value) => {
     }
 
     // Formula taken from https://nl.wikipedia.org/wiki/Burgerservicenummer#11-proef
-    const newValue = (9 * parseInt(value[0])) + (8 * parseInt(value[1])) + (7 * parseInt(value[2])) +
-                      (6 * parseInt(value[3])) + (5 * parseInt(value[4])) + (4 * parseInt(value[5])) +
-                      (3 * parseInt(value[6])) + (2 * parseInt(value[7])) + (-1 * parseInt(value[8]));
+    const elevenTestValue = (9 * parseInt(value[0])) + (8 * parseInt(value[1])) + (7 * parseInt(value[2])) +
+                            (6 * parseInt(value[3])) + (5 * parseInt(value[4])) + (4 * parseInt(value[5])) +
+                            (3 * parseInt(value[6])) + (2 * parseInt(value[7])) + (-1 * parseInt(value[8]));
 
-    if (isNaN(newValue)) {
-      return false;
-    }
-
-    return newValue % 11 === 0;
+    return elevenTestValue % 11 === 0;
 };
 
 export { applyPrefix, isValidBsn };
