aba54e263ba22dc5121ec7c1aec6b9a0d46150e6
sheameyers <shea.meyers@maykinmedia.nl>
2021-06-30 14:28:17 +0200

    :sparkles: [Refs open-formulieren/open-forms#213] Add function for testing if bsn is valid

--- src/formio/utils.js
+++ src/formio/utils.js
@@ -7,4 +7,21 @@ const applyPrefix = (name) => {
   return `${PREFIX}-${name}`;
 };
 
-export { applyPrefix };
+const isValidBsn = (value) => {
+    if (value.length !== 9) {
+      return false;
+    }
+
+    // Formula taken from https://nl.wikipedia.org/wiki/Burgerservicenummer#11-proef
+    const newValue = (9 * parseInt(value[0])) + (8 * parseInt(value[1])) + (7 * parseInt(value[2])) +
+                      (6 * parseInt(value[3])) + (5 * parseInt(value[4])) + (4 * parseInt(value[5])) +
+                      (3 * parseInt(value[6])) + (2 * parseInt(value[7])) + (-1 * parseInt(value[8]));
+
+    if (isNaN(newValue)) {
+      return false;
+    }
+
+    return newValue % 11 === 0;
+};
+
+export { applyPrefix, isValidBsn };
