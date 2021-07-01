1d4f879b2d144a9d958464a05084b2bc2ce0a3a1
sheameyers <shea.meyers@maykinmedia.nl>
2021-07-01 10:39:32 +0200

    :fire: Refs open-formulieren/open-forms#213 Remove custom validator

--- src/formio/components/PostcodeField.js
+++ src/formio/components/PostcodeField.js
@@ -1,25 +1,7 @@
 import {Formio} from "react-formio";
 import {applyPrefix} from "../utils";
-import {POSTCODE_REGEX} from "../constants";
 
 const TextField = Formio.Components.components.textfield;
-
-const PostcodeValidator = {
-  key: "validate.postcode",
-  hasLabel: true,
-  message(component) {
-    return component.t(component.errorMessage('Invalid Postcode'), {
-      field: component.errorLabel,
-      data: component.data
-    });
-  },
-  check(component, setting, value) {
-    if (!value) {
-      return true;
-    }
-    return POSTCODE_REGEX.test(value);
-  }
-};
 
 
 class PostcodeField extends TextField {
@@ -28,7 +10,11 @@ class PostcodeField extends TextField {
         type: 'postcode',
         label: 'Postcode',
         key: 'postcode',
-        inputMask: '9999 AA'
+        inputMask: '9999 AA',
+        validateOn: 'blur',
+        validate: {
+          customMessage: 'Invalid Postcode'
+        }
     }, ...extend);
   }
 
@@ -38,14 +24,6 @@ class PostcodeField extends TextField {
     info.attr.class = applyPrefix('input');
     return info;
   }
-
-  init() {
-        super.init();
-        // Override mask validator check since we want to
-        // do our own validation
-        this.validator.validators.mask.check = () => true;
-        this.validator.validators.custom = PostcodeValidator;
-    }
 }
 
 export default PostcodeField;
--- src/formio/constants.js
+++ src/formio/constants.js
@@ -1,5 +1,3 @@
 const PREFIX = 'openforms';
 
-const POSTCODE_REGEX = /^[0-9]{4} [a-zA-Z]{2}$/;
-
-export { PREFIX, POSTCODE_REGEX };
+export { PREFIX };
