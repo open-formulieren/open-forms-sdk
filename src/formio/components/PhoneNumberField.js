a9a4b7bb8aaeb27f48fe3faaf86c9d69e1574710
sheameyers <shea.meyers@maykinmedia.nl>
2021-07-02 12:11:49 +0200

    :bug: Refs open-formulieren/open-forms#200 Fix custom validation

--- src/formio/components/PhoneNumberField.js
+++ src/formio/components/PhoneNumberField.js
@@ -1,13 +1,14 @@
 import {Formio} from "formiojs";
 import {applyPrefix} from "../utils";
-import {PHONE_NUMBER_REGEX} from "../constants";
 
 const PhoneNumber = Formio.Components.components.phoneNumber;
 
 
+const PHONE_NUMBER_REGEX = /^\+{0,1}[- 0-9]{0,}$/;
+
+
 const PhoneNumberValidator = {
   key: "validate.phoneNumber",
-  hasLabel: true,
   message(component) {
     return component.t(component.errorMessage('Invalid Phone Number'), {
       field: component.errorLabel,
@@ -15,6 +16,9 @@ const PhoneNumberValidator = {
     });
   },
   check(component, setting, value) {
+    if (component.type !== "phoneNumber") {
+      return true;
+    }
     return PHONE_NUMBER_REGEX.test(value);
   }
 };
@@ -22,16 +26,17 @@ const PhoneNumberValidator = {
 
 class PhoneNumberField extends PhoneNumber {
 
+    constructor(component, options, data) {
+      super(component, options, data);
+      this.validator.validators.phoneNumber = PhoneNumberValidator;
+      this.validators.push("phoneNumber");
+    }
+
     get inputInfo() {
       const info = super.inputInfo;
       // change the default CSS classes
       info.attr.class = applyPrefix('input');
       return info;
-    }
-
-    init() {
-      super.init();
-      this.validator.validators.custom = PhoneNumberValidator;
     }
 }
 
--- src/formio/constants.js
+++ src/formio/constants.js
@@ -1,5 +1,3 @@
 const PREFIX = 'openforms';
 
-const PHONE_NUMBER_REGEX = /^\+{0,1}[- 0-9]{0,}$/;
-
-export { PREFIX, PHONE_NUMBER_REGEX };
+export { PREFIX };
