94a257fbb77728fddb99f22420ec7f121a563a56
sheameyers <shea.meyers@maykinmedia.nl>
2021-07-05 14:42:31 +0200

    :bug: [Refs open-formulieren/open-forms#213] Add back type

--- src/formio/components/PostcodeField.js
+++ src/formio/components/PostcodeField.js
@@ -4,6 +4,7 @@ import TextField from "./TextField";
 class PostcodeField extends TextField {
   static schema(...extend) {
     return TextField.schema({
+        type: 'postcode',
         label: 'Postcode',
         key: 'postcode',
         inputMask: '9999 AA',
