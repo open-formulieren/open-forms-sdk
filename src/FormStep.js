f8e1d670706ea3d3157db677c0191e1143519a6e
sheameyers <shea.meyers@maykinmedia.nl>
2021-07-05 15:55:49 +0200

    :sparkles: Refs open-formulieren/open-forms#207 Get buttons text for form steps from api

--- src/FormStep.js
+++ src/FormStep.js
@@ -133,11 +133,11 @@ const FormStep = ({ form, submission, onStepSubmitted }) => {
                   variant="anchor"
                   component="a"
                   onClick={onPrevPage}
-                >Vorige pagina</Button>
+                >{formStep.previousText}</Button>
               </ToolbarList>
               <ToolbarList>
-                <Button type="button" variant="secondary" name="save" onClick={onFormSave} disabled>Tussentijds opslaan</Button>
-                <Button type="submit" variant="primary" name="next">Volgende</Button>
+                <Button type="button" variant="secondary" name="save" onClick={onFormSave} disabled>{formStep.saveText}</Button>
+                <Button type="submit" variant="primary" name="next">{formStep.nextText}</Button>
               </ToolbarList>
             </Toolbar>
           </form>
