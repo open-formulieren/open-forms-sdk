abc9b9ac5a7d8038842a03e49496e206852ff39c
Sergei Maertens <sergei@maykinmedia.nl>
2021-07-12 16:08:59 +0200

    :recycle: Refactor Form Type into single source of truth

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,4 @@
 import React, {useContext} from 'react';
-import PropTypes from 'prop-types';
 import { useImmerReducer } from "use-immer";
 import {
   Switch,
@@ -20,6 +19,7 @@ import { Layout, LayoutRow, LayoutColumn } from './Layout';
 import RequireSubmission from './RequireSubmission';
 import SubmissionConfirmation from "./SubmissionConfirmation";
 import Summary from './Summary';
+import Types from './types';
 
 /**
  * Create a submission instance from a given form instance
@@ -207,31 +207,7 @@ const reducer = (draft, action) => {
 };
 
 Form.propTypes = {
-  form: PropTypes.shape({
-    uuid: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    loginRequired: PropTypes.bool.isRequired,
-    loginOptions: PropTypes.arrayOf(PropTypes.shape({
-      identifier: PropTypes.string.isRequired,
-      label: PropTypes.string.isRequired,
-      url: PropTypes.string.isRequired,
-      logo: PropTypes.shape({
-        title: PropTypes.string.isRequired,
-        imageSrc: PropTypes.string.isRequired,
-        href: PropTypes.string,
-      }),
-    })).isRequired,
-    product: PropTypes.object,
-    slug: PropTypes.string.isRequired,
-    url: PropTypes.string.isRequired,
-    maintenanceMode: PropTypes.bool.isRequired,
-    steps: PropTypes.arrayOf(PropTypes.shape({
-      uuid: PropTypes.string.isRequired,
-      formDefinition: PropTypes.string.isRequired,
-      index: PropTypes.number.isRequired,
-      url: PropTypes.string.isRequired,
-    })).isRequired,
-  }).isRequired,
+  form: Types.Form.isRequired,
 };
 
 export { Form };
--- src/FormStart.js
+++ src/FormStart.js
@@ -8,6 +8,7 @@ import Body from './Body';
 import MaintenanceMode from './MaintenanceMode';
 import useQuery from './hooks/useQuery';
 import AuthenticationOutage, { useDetectAuthenticationOutage } from './AuthenticationOutage';
+import Types from './types';
 
 const START_FORM_QUERY_PARAM = '_start';
 
@@ -127,31 +128,7 @@ const FormStart = ({ form, onFormStart }) => {
 };
 
 FormStart.propTypes = {
-  form: PropTypes.shape({
-    uuid: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    loginRequired: PropTypes.bool.isRequired,
-    loginOptions: PropTypes.arrayOf(PropTypes.shape({
-      identifier: PropTypes.string.isRequired,
-      label: PropTypes.string.isRequired,
-      url: PropTypes.string.isRequired,
-      logo: PropTypes.shape({
-        title: PropTypes.string.isRequired,
-        imageSrc: PropTypes.string.isRequired,
-        href: PropTypes.string,
-      }),
-    })).isRequired,
-    product: PropTypes.object,
-    slug: PropTypes.string.isRequired,
-    url: PropTypes.string.isRequired,
-    maintenanceMode: PropTypes.bool.isRequired,
-    steps: PropTypes.arrayOf(PropTypes.shape({
-      uuid: PropTypes.string.isRequired,
-      formDefinition: PropTypes.string.isRequired,
-      index: PropTypes.number.isRequired,
-      url: PropTypes.string.isRequired,
-    })).isRequired,
-  }).isRequired,
+  form: Types.Form.isRequired,
   onFormStart: PropTypes.func.isRequired,
 };
 
