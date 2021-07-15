48d5997e54e578373d990fe791f3950352f39354
sheameyers <shea.meyers@maykinmedia.nl>
2021-07-15 17:09:29 +0200

    :sparkles: Refs open-formulieren/open-forms#463 Add kvk validation to number component

--- src/formio/components/Number.js
+++ src/formio/components/Number.js
@@ -1,11 +1,21 @@
 import { Formio } from 'react-formio';
 
 import { applyPrefix } from '../utils';
+import KvkValidatorMapping from "../validators/kvk";
 
 /**
  * Extend the default text field to modify it to our needs.
  */
 class Number extends Formio.Components.components.number {
+
+  constructor(component, options, data) {
+    super(component, options, data);
+    for (let plugin of this.component.validate.plugins) {
+      this.validator.validators[plugin] = KvkValidatorMapping[plugin];
+      this.validators.push(plugin);
+    }
+  }
+
   get inputInfo() {
     const info = super.inputInfo;
     // change the default CSS classes
