8d3343fdd8a16283902d465a9545dfef24e772fb
sheameyers <shea.meyers@maykinmedia.nl>
2021-08-05 14:27:40 +0200

    :sparkles: Refs open-formulieren/open-forms#524 Override file component to add functionality

---
+++ src/formio/components/FileField.js
@@ -0,0 +1,37 @@
+import { Formio } from 'react-formio';
+
+/**
+ * Extend the default file field to modify it to our needs.
+ */
+class FileField extends Formio.Components.components.file {
+
+  get browseOptions() {
+    // This code is copied from https://github.com/formio/formio.js/blob/v4.13.0/src/components/file/File.js#L281-L304
+    // Since we are using FormIO v4.12.7 only image based files are supported
+    // If are are using v4.13.x or higher this can probably be deleted
+
+    const options = {};
+
+    if (this.component.multiple) {
+      options.multiple = true;
+    }
+    //use "accept" attribute only for desktop devices because of its limited support by mobile browsers
+    if (!this.isMobile.any) {
+      const filePattern = this.component.filePattern.trim() || '';
+      const imagesPattern = 'image/*';
+
+      if (this.imageUpload && (!filePattern || filePattern === '*')) {
+        options.accept = imagesPattern;
+      } else if (this.imageUpload && !filePattern.includes(imagesPattern)) {
+        options.accept = `${imagesPattern},${filePattern}`;
+      } else {
+        options.accept = filePattern;
+      }
+    }
+
+    return options;
+  }
+}
+
+
+export default FileField;
