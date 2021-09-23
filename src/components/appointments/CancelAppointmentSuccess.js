82fcef2da7e36fd691786825e77aac0042c6c304
Sergei Maertens <sergei@maykinmedia.nl>
2021-09-23 16:02:57 +0200

    :recycle: Set up appointment cancellation success page properly

--- src/components/appointments/CancelAppointmentSuccess.js
+++ src/components/appointments/CancelAppointmentSuccess.js
@@ -3,15 +3,15 @@ import Card from 'components/Card';
 import {FormattedMessage} from "react-intl";
 import React from "react";
 
-const CancelAppointmentSuccess = () => {
-
-  return (
-    <Card title="Afspraak Geannuleerd">
-        <Body>
-          <FormattedMessage id="appointmentCancelledSuccess" defaultMessage="Uw afspraak is geannuleerd" />
-        </Body>
-    </Card>
-  );
-};
+const CancelAppointmentSuccess = () => (
+  <Card title={<FormattedMessage description="Appointment cancelled title" defaultMessage="Appointment cancelled" />}>
+      <Body modifiers={['big']}>
+        <FormattedMessage
+          description="Appointment cancellated body"
+          defaultMessage="Your appointment has been cancelled"
+        />
+      </Body>
+  </Card>
+);
 
 export default CancelAppointmentSuccess;
