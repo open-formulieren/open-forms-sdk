dc53f702108495e80a4e03b6abed55201bff3b05
Bryan de Jong <bryandejong99@gmail.com>
2021-10-06 15:35:08 +0200

    feat: use utrecht bem for textfield

--- src/formio/components/TextField.js
+++ src/formio/components/TextField.js
@@ -22,7 +22,7 @@ class TextField extends Formio.Components.components.textfield {
   get inputInfo() {
     const info = super.inputInfo;
     // change the default CSS classes
-    info.attr.class = applyPrefix('input');
+    info.attr.class = 'utrecht-textbox';
     return info;
   }
 
