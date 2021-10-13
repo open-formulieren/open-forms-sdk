15b3e487c166dee2cfb3e5952cb860965f983449
sheameyers <shea.meyers@maykinmedia.nl>
2021-10-13 12:19:12 +0200

    :construction: Refs open-formulieren/open-forms#286 - Get our map working in summary page

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,7 @@
-import React, { useEffect } from 'react';
+import {useContext, useEffect} from 'react';
+import _uniqueId from 'lodash/uniqueId';
 import PropTypes from 'prop-types';
+import {ConfigContext} from 'Context';
 
 import * as L from 'leaflet';
 import { TILE_LAYERS, DEFAULT_LAT_LON, DEFAULT_ZOOM, MAP_DEFAULTS } from '../formio/components/map/constants';
@@ -7,20 +9,53 @@ import { TILE_LAYERS, DEFAULT_LAT_LON, DEFAULT_ZOOM, MAP_DEFAULTS } from '../for
 
 const Map = () => {
 
+  const id = _uniqueId();
+  let {baseUrl} = useContext(ConfigContext);
+  baseUrl = baseUrl.replaceAll("/api/v1/", "");
+
   useEffect(() => {
-    let mymap = L.map('mapid').setView([51.505, -0.09], 13);
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      maxZoom: 18,
-      id: 'mapbox/streets-v11',
-      tileSize: 512,
-      zoomOffset: -1,
-      accessToken: 'pk.eyJ1Ijoic2hlYW1leWVycyIsImEiOiJja3VncTZpc3gwYnNzMnFteTZjeXp2M3E0In0.v5LSs4Hd4xfrRqInXVy3dw'
-    }).addTo(mymap);
+    // fix leaflet images import - https://github.com/Leaflet/Leaflet/issues/4968
+    delete L.Icon.Default.prototype._getIconUrl;
+
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: `${baseUrl}/static/bundles/images/marker-icon-2x.png`,
+      iconUrl: `${baseUrl}/static/bundles/images/marker-icon.png`,
+      shadowUrl: `${baseUrl}/static/bundles/images/marker-shadow.png`,
+    });
+
+    // Prevent exception if container is already initialized
+    const container = L.DomUtil.get(`map-${id}`);
+    if (container !== null) {
+      container._leaflet_id = null;
+    }
+
+    const map = L.map(`map-${id}`, MAP_DEFAULTS);
+
+    const tiles = L.tileLayer(TILE_LAYERS.url, TILE_LAYERS.options);
+
+    map.addLayer(tiles);
+
+    let marker = L.marker(DEFAULT_LAT_LON).addTo(map);
+
+    // Attempt to get the user's current location and set the marker to that
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        map.removeLayer(marker);
+        const newLatLng = [position.coords.latitude, position.coords.longitude];
+        marker = L.marker(newLatLng).addTo(map);
+        map.setView(newLatLng, DEFAULT_ZOOM);
+      });
+    }
+
+    map.on('click', (e) => {
+      map.removeLayer(marker);
+      const newLatLng = [e.latlng.lat, e.latlng.lng];
+      marker = L.marker(newLatLng).addTo(map);
+    });
   });
 
   return (
-    <div id="mapid" style={{ height: "400px", position: "relative"}} />
+    <div id={"map-" + id} style={{ height: "400px", position: "relative" }}/>
   );
 };
 
