6e17f820927378eee18e9768cf0059108e3de6b6
sheameyers <shea.meyers@maykinmedia.nl>
2021-10-13 13:37:34 +0200

    :recycle: Refs open-formulieren/open-forms#286 - Clean up code

--- src/components/Map.js
+++ src/components/Map.js
@@ -9,45 +9,11 @@ import { TILE_LAYERS, DEFAULT_LAT_LON, DEFAULT_ZOOM, MAP_DEFAULTS } from '../map
 
 const Map = ({ disabled=false, initialCoordinates=DEFAULT_LAT_LON}) => {
 
-  const id = _uniqueId();
+  const mapId = _uniqueId('map-');
   let {baseUrl} = useContext(ConfigContext);
   baseUrl = baseUrl.replaceAll("/api/v1/", "");
 
-  useEffect(() => {
-    // fix leaflet images import - https://github.com/Leaflet/Leaflet/issues/4968
-    delete L.Icon.Default.prototype._getIconUrl;
-
-    L.Icon.Default.mergeOptions({
-      iconRetinaUrl: `${baseUrl}/static/bundles/images/marker-icon-2x.png`,
-      iconUrl: `${baseUrl}/static/bundles/images/marker-icon.png`,
-      shadowUrl: `${baseUrl}/static/bundles/images/marker-shadow.png`,
-    });
-
-    // Prevent exception if container is already initialized
-    const container = L.DomUtil.get(`map-${id}`);
-    if (container !== null) {
-      container._leaflet_id = null;
-    }
-
-    const map = L.map(`map-${id}`, MAP_DEFAULTS);
-
-    const tiles = L.tileLayer(TILE_LAYERS.url, TILE_LAYERS.options);
-
-    map.addLayer(tiles);
-    map.setView(initialCoordinates, DEFAULT_ZOOM);
-
-    let marker = L.marker(initialCoordinates).addTo(map);
-
-    if (disabled) {
-      map.dragging.disable();
-      map.touchZoom.disable();
-      map.doubleClickZoom.disable();
-      map.scrollWheelZoom.disable();
-      map.boxZoom.disable();
-      map.keyboard.disable();
-      if (map.tap) map.tap.disable();
-      document.getElementById(`map-${id}`).style.cursor = 'default';
-    } else {
+  const initializeInterableMap = (map, marker) => {
       // Attempt to get the user's current location and set the marker to that
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((position) => {
@@ -63,11 +29,53 @@ const Map = ({ disabled=false, initialCoordinates=DEFAULT_LAT_LON}) => {
         const newLatLng = [e.latlng.lat, e.latlng.lng];
         marker = L.marker(newLatLng).addTo(map);
       });
+  };
+
+  const disableMap = (map) => {
+      map.dragging.disable();
+      map.touchZoom.disable();
+      map.doubleClickZoom.disable();
+      map.scrollWheelZoom.disable();
+      map.boxZoom.disable();
+      map.keyboard.disable();
+      if (map.tap) map.tap.disable();
+      document.getElementById(mapId).style.cursor = 'default';
+  };
+
+  useEffect(() => {
+    // fix leaflet images import - https://github.com/Leaflet/Leaflet/issues/4968
+    delete L.Icon.Default.prototype._getIconUrl;
+
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: `${baseUrl}/static/bundles/images/marker-icon-2x.png`,
+      iconUrl: `${baseUrl}/static/bundles/images/marker-icon.png`,
+      shadowUrl: `${baseUrl}/static/bundles/images/marker-shadow.png`,
+    });
+
+    // Prevent exception if container is already initialized
+    const container = L.DomUtil.get(mapId);
+    if (container !== null) {
+      container._leaflet_id = null;
+    }
+
+    const map = L.map(mapId, MAP_DEFAULTS);
+
+    const tiles = L.tileLayer(TILE_LAYERS.url, TILE_LAYERS.options);
+
+    map.addLayer(tiles);
+    map.setView(initialCoordinates, DEFAULT_ZOOM);
+
+    let marker = L.marker(initialCoordinates).addTo(map);
+
+    if (disabled) {
+      disableMap(map);
+    } else {
+      initializeInterableMap(map, marker);
     }
   });
 
   return (
-    <div id={"map-" + id} style={{ height: "400px", position: "relative" }}/>
+    <div id={mapId} style={{ height: "400px", position: "relative" }}/>
   );
 };
 
