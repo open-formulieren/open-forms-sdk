0c37a41b3cd197067430c9f717c2eb36c2b86e89
Bryan de Jong <bryandejong99@gmail.com>
2021-10-15 13:36:40 +0200

    feat: style checkbox with utrecht bem classes

--- src/formio/templates/checkbox.js
+++ src/formio/templates/checkbox.js
@@ -1,7 +1,5 @@
-import { applyPrefix } from '../utils';
-
 const TEMPLATE = `
-<div class="${applyPrefix('checkbox')}">
+<div class="utrecht-form-field utrecht-form-field--checkbox utrecht-form-field--distanced">
     <{{ctx.input.type}}
       ref="input"
       {% for (var attr in ctx.input.attr) { %}
@@ -9,13 +7,13 @@ const TEMPLATE = `
       {% } %}
       id="{{ctx.instance.id}}-{{ctx.component.key}}"
       {% if (ctx.checked) { %}checked=true{% } %}
+      class="utrecht-form-field__input utrecht-checkbox"
     >
-
         {{ctx.input.content}}
     </{{ctx.input.type}}>
 
-    <div class="${applyPrefix('checkbox__checkmark')}"></div>
-    <label class="${applyPrefix('checkbox__label')} {{ctx.input.labelClass}}" for="{{ctx.instance.id}}-{{ctx.component.key}}">
+<!--    <div class="checkbox__checkmark"></div>-->
+    <label class="utrecht-form-field__label utrecht-form-field__label--checkbox utrecht-form-label utrecht-form-label--checkbox" for="{{ctx.instance.id}}-{{ctx.component.key}}">
         {{ctx.input.label}}&nbsp;
         {% if (ctx.component.tooltip) { %}
             <i ref="tooltip" class="{{ctx.iconClass('question-sign')}}"></i>
