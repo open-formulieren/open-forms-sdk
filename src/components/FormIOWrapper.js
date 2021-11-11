de493e20b682c9403b8a2531c0a15751b36c5ebf
silvia <silvia@maykinmedia.nl>
2021-11-11 15:54:57 +0100

    :bug: [#103] CloneDeep the submission data

--- src/components/FormIOWrapper.js
+++ src/components/FormIOWrapper.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Form } from 'react-formio';
+import _ from 'lodash';
 
-const FormIOWrapper = React.forwardRef((props, ref) => (
-  <Form {...props} ref={ref} />
-));
+const FormIOWrapper = React.forwardRef((props, ref) => {
+  const updatedProps = {...props, submission: _.cloneDeep(props.submission)};
+  return (<Form {...updatedProps} ref={ref} />);
+});
 
 export default FormIOWrapper;
