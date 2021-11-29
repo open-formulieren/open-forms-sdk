c5eb35721976190bfaa6df2f078c572fe7368396
Sergei Maertens <sergei@maykinmedia.nl>
2021-11-29 10:41:13 +0100

    :lipstick: More dropdown styling
    * Make select explicitly white background on all browsers
    * Improve multi select styling
    * Set select size hard to 3 items (will scroll on more items)
    * Do not display caret dropdown on multiple select

--- src/formio/templates/select.js
+++ src/formio/templates/select.js
@@ -2,6 +2,7 @@ const TEMPLATE = `
 <select
   ref="{{ctx.input.ref ? ctx.input.ref : 'selectContainer'}}"
   {{ ctx.input.multiple ? 'multiple' : '' }}
+  {{ ctx.input.multiple ? 'size="3"' : '' }}
 
   {% for (var attr in ctx.input.attr) { %}
     {{attr}}="{{ctx.input.attr[attr]}}"
