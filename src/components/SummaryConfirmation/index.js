368896bdc9356a560f34013d94e9da806084aa9d
silvia <silvia@maykinmedia.nl>
2021-12-16 11:57:49 +0100

    :white_check_mark: [open-formulieren/open-forms#924] Test summary page

---
+++ src/components/SummaryConfirmation/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {SUBMISSION_ALLOWED} from 'components/constants';
+import PrivacyCheckbox from 'components/PrivacyCheckbox';
+import {Toolbar, ToolbarList} from 'components/Toolbar';
+import Button from 'components/Button';
+import {Literal} from 'components/Literal';
+
+
+const SummaryConfirmation = ({
+  submissionAllowed,
+  privacy: { requiresPrivacyConsent, policyAccepted, privacyLabel },
+  onPrivacyCheckboxChange,
+  onPrevPage,
+}) => {
+  const canSubmit = (submissionAllowed === SUBMISSION_ALLOWED.yes);
+
+  const displayPrivacyNotice = canSubmit && requiresPrivacyConsent;
+  const submitDisabled = requiresPrivacyConsent && !policyAccepted;
+  return (
+    <>
+      {
+        displayPrivacyNotice
+        ? (<PrivacyCheckbox value={policyAccepted} label={privacyLabel} onChange={onPrivacyCheckboxChange} />)
+        : null
+      }
+      <Toolbar modifiers={['mobile-reverse-order', 'bottom']}>
+        <ToolbarList>
+          <Button variant="anchor" component="a" onClick={onPrevPage}>
+            <Literal name="previousText" />
+          </Button>
+        </ToolbarList>
+        <ToolbarList>
+          {
+            canSubmit
+            ? (
+              <Button type="submit" variant="primary" name="confirm" disabled={submitDisabled}>
+               <Literal name="confirmText" />
+              </Button>
+            )
+            : null
+          }
+        </ToolbarList>
+      </Toolbar>
+    </>
+  );
+};
+
+SummaryConfirmation.propTypes = {
+  submissionAllowed: PropTypes.string.isRequired,
+  privacy: PropTypes.shape({
+    requiresPrivacyConsent: PropTypes.bool.isRequired,
+    policyAccepted: PropTypes.bool.isRequired,
+    privacyLabel: PropTypes.string.isRequired,
+  }).isRequired,
+  onPrivacyCheckboxChange: PropTypes.func.isRequired,
+  onPrevPage: PropTypes.func.isRequired,
+};
+
+
+export default SummaryConfirmation;
---
+++ src/components/SummaryConfirmation/test.spec.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+
+import {SUBMISSION_ALLOWED} from 'components/constants';
+import {LiteralsProvider} from 'components/Literal';
+import SummaryConfirmation from './index';
+
+
+let container = null;
+
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+
+const PRIVACY = {
+  requiresPrivacyConsent: false,
+  policyAccepted: false,
+  privacyLabel: 'This is privacy'
+};
+
+const LITERALS = {
+  confirmText: {value: '', resolved: 'Submit form'},
+  previousText: {value: '', resolved: 'Previous step'},
+};
+
+
+it('Summary of non-submittable form, button is NOT present', () => {
+  const mockFunction = jest.fn();
+
+  act(() => {
+    render(
+      <LiteralsProvider literals={LITERALS}>
+        <SummaryConfirmation
+          submissionAllowed={SUBMISSION_ALLOWED.no_with_overview}
+          privacy={PRIVACY}
+          onPrivacyCheckboxChange={mockFunction}
+          onPrevPage={mockFunction}
+        />
+      </LiteralsProvider>,
+      container
+    );
+  });
+
+  const buttons = container.getElementsByClassName('openforms-toolbar__list-item');
+
+  expect(buttons.length).toEqual(1);
+  expect(buttons[0].textContent).toEqual('Previous step');
+});
+
+
+it('Summary of submittable form, button IS present', () => {
+  const mockFunction = jest.fn();
+
+  act(() => {
+    render(
+      <LiteralsProvider literals={LITERALS}>
+        <SummaryConfirmation
+          submissionAllowed={SUBMISSION_ALLOWED.yes}
+          privacy={PRIVACY}
+          onPrivacyCheckboxChange={mockFunction}
+          onPrevPage={mockFunction}
+        />
+      </LiteralsProvider>,
+      container
+    );
+  });
+
+  const buttons = container.getElementsByClassName('openforms-toolbar__list-item');
+
+  expect(buttons.length).toEqual(2);
+  expect(buttons[0].textContent).toEqual('Previous step');
+  expect(buttons[1].textContent).toEqual('Submit form');
+});
