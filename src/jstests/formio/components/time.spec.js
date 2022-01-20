9d51a8bb13e1fc9af751f89fe8de870949abefdb
silvia <silvia@maykinmedia.nl>
2022-01-20 17:19:20 +0100

    :white_check_mark: [open-formulieren/open-forms#1152] Add test for time validation

--- src/jstests/formio/components/time.spec.js
+++ src/jstests/formio/components/time.spec.js
@@ -11,8 +11,8 @@ Formio.use(OpenFormsModule);
 describe('Time Component', () => {
   test('Time component with min/max time validation', (done) => {
     let formJSON = _.cloneDeep(timeForm);
-    formJSON.components[0]['minTime'] = '09:00:00';
-    formJSON.components[0]['maxTime'] = '17:00:00';
+    formJSON.components[0].minTime = '09:00:00';
+    formJSON.components[0].maxTime = '17:00:00';
 
     const validValues = [
       '09:00:00',
@@ -86,5 +86,130 @@ describe('Time Component', () => {
     };
 
     testValidity(validValues);
+  });
+
+  test('Time component with only min time validation', (done) => {
+    let formJSON = _.cloneDeep(timeForm);
+    formJSON.components[0].minTime = '09:00:00';
+
+    const validValues = [
+      '17:00:00'
+    ];
+
+    const invalidValues = [
+      '08:00:00',
+    ];
+
+    const testValidity = (values, valid) => {
+      values.forEach((value) => {
+        const element = document.createElement('div');
+
+        Formio.createForm(element, formJSON).then(form => {
+          form.setPristine(false);
+          const component = form.getComponent('time');
+          const changed = component.setValue(value);
+          expect(changed).toBeTruthy();
+
+          setTimeout(() => {
+            if (valid) {
+              expect(!!component.error).toBeFalsy();
+            } else {
+              expect(!!component.error).toBeTruthy();
+            }
+
+            if (value === invalidValues[0]) {
+              done();
+            }
+          }, 300);
+        }).catch(done);
+      });
+    };
+
+    testValidity(validValues, true);
+    testValidity(invalidValues, false);
+  });
+
+  test('Time component with only max time validation', (done) => {
+    let formJSON = _.cloneDeep(timeForm);
+    formJSON.components[0].maxTime = '09:00:00';
+
+    const validValues = [
+      '08:00:00'
+    ];
+
+    const invalidValues = [
+      '17:00:00',
+    ];
+
+    const testValidity = (values, valid) => {
+      values.forEach((value) => {
+        const element = document.createElement('div');
+
+        Formio.createForm(element, formJSON).then(form => {
+          form.setPristine(false);
+          const component = form.getComponent('time');
+          const changed = component.setValue(value);
+          expect(changed).toBeTruthy();
+
+          setTimeout(() => {
+            if (valid) {
+              expect(!!component.error).toBeFalsy();
+            } else {
+              expect(!!component.error).toBeTruthy();
+            }
+
+            if (value === invalidValues[0]) {
+              done();
+            }
+          }, 300);
+        }).catch(done);
+      });
+    };
+
+    testValidity(validValues, true);
+    testValidity(invalidValues, false);
+  });
+
+  test('Time component with min time boundary larger than max time boundary', (done) => {
+    let formJSON = _.cloneDeep(timeForm);
+    formJSON.components[0].maxTime = '01:00:00';
+    formJSON.components[0].minTime = '08:00:00';
+
+    const validValues = [
+      '09:00:00',
+      '00:30:00'
+    ];
+
+    const invalidValues = [
+      '02:00:00',
+    ];
+
+    const testValidity = (values, valid) => {
+      values.forEach((value) => {
+        const element = document.createElement('div');
+
+        Formio.createForm(element, formJSON).then(form => {
+          form.setPristine(false);
+          const component = form.getComponent('time');
+          const changed = component.setValue(value);
+          expect(changed).toBeTruthy();
+
+          setTimeout(() => {
+            if (valid) {
+              expect(!!component.error).toBeFalsy();
+            } else {
+              expect(!!component.error).toBeTruthy();
+            }
+
+            if (value === invalidValues[0]) {
+              done();
+            }
+          }, 300);
+        }).catch(done);
+      });
+    };
+
+    testValidity(validValues, true);
+    testValidity(invalidValues, false);
   });
 });
