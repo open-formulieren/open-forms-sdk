1962f80d828ad85f53c7f3815d6a050256b372af
silvia <silvia@maykinmedia.nl>
2022-01-20 17:20:04 +0100

    :construction: [open-formulieren/open-forms#1152] Handle additional time case validation

--- src/formio/validators/MinMaxTimeValidator.js
+++ src/formio/validators/MinMaxTimeValidator.js
@@ -1,5 +1,29 @@
 import moment from 'moment';
 
+const validateTimeBoundaries = (minBoundary, maxBoundary, timeValue) => {
+  const minTime = minBoundary ? moment(minBoundary, 'HH:mm:ss') : null;
+  const maxTime = maxBoundary ? moment(maxBoundary, 'HH:mm:ss') : null;
+  const parsedValue =  moment(timeValue, 'HH:mm:ss');
+
+  // Case 0: no boundaries given
+  if ( !minTime && !maxTime) {
+    return true;
+  }
+
+  // Case 1: only one boundary is given
+  if ( !minTime || !maxTime ) {
+    if ( minTime ) return parsedValue >= minTime;
+    if ( maxTime ) return parsedValue < maxTime;
+  } else {
+    // Case 2: min boundary is smaller than max boundary
+    if ( minTime < maxTime ) {
+      return parsedValue >= minTime && parsedValue < maxTime;
+    } else {
+      // Case 3: min boundary is bigger than max boundary (it's the next day. For example min = 08:00, max = 01:00)
+      return !(parsedValue >= maxTime && parsedValue < minTime);
+    }
+  }
+};
 
 const MinMaxTimeValidator = {
   key: 'validate.timeMinMax',
@@ -15,13 +39,7 @@ const MinMaxTimeValidator = {
   check(component, setting, value) {
     if (!value) return true;
 
-    const minTime = component.component.minTime ? moment(component.component.minTime, 'HH:mm:ss') : null;
-    const maxTime = component.component.maxTime ? moment(component.component.maxTime, 'HH:mm:ss') : null;
-    const parsedValue =  moment(value, 'HH:mm:ss');
-
-    if (minTime && parsedValue < minTime) return false;
-    if (maxTime && parsedValue >= maxTime) return false;
-    return true;
+    return validateTimeBoundaries(component.component.minTime, component.component.maxTime, value);
   }
 };
 
