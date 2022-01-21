0132346a0f6e8c057da64e45ae161d14426a5a13
silvia <silvia@maykinmedia.nl>
2022-01-21 16:24:54 +0100

    :white_check_mark: Avoid showing NaNs in summary page

--- src/components/FormStepSummary/fixtures.js
+++ src/components/FormStepSummary/fixtures.js
@@ -80,7 +80,7 @@ export const testStepDataSelectMultivalue = {
 };
 
 
-export const testStepUploadFile = {
+export const testStepEmptyFields = {
   "submissionStep": {
     "id": "31bd9fc8-57b7-4b32-bae7-a0b6d8c076cc",
     "name": "Details",
@@ -99,6 +99,28 @@ export const testStepUploadFile = {
     "components": [
       {
         "id": "em3xsol",
+        "key": "amountToPay",
+        "type": "currency",
+        "input": true,
+        "multiple": false,
+        "label": "Amount to Pay",
+        "data": {
+          "value": undefined,
+        }
+      },
+      {
+        "id": "em3xsal",
+        "key": "nPeople",
+        "type": "number",
+        "input": true,
+        "multiple": false,
+        "label": "Number of people",
+        "data": {
+          "value": undefined,
+        }
+      },
+      {
+        "id": "em3xsel",
         "key": "uploadFile",
         "type": "file",
         "input": true,
@@ -107,30 +129,9 @@ export const testStepUploadFile = {
         "data": {
           "value": [],
         }
-      }
-    ]
-  }
-};
-
-export const testStepUploadMultiFile = {
-  "submissionStep": {
-    "id": "31bd9fc8-57b7-4b32-bae7-a0b6d8c076cc",
-    "name": "Details",
-    "url": "http://testserver.nl/api/v1/submissions/774db9fc-1c0f-42ef-a7f7-b96d8c7d5905/steps/31bd9fc8-57b7-4b32-bae7-a0b6d8c076cc",
-    "formStep": "http://testserver.nl/api/v1/forms/3ef4c822-c6c7-496d-99d6-897c1d1c1219/steps/31bd9fc8-57b7-4b32-bae7-a0b6d8c076cc",
-    "isApplicable": true,
-    "completed": true,
-    "optional": false,
-    "canSubmit": true
-  },
-  "title": "Test empty upload file field",
-  "data": {
-    "uploadFile": []
-  },
-  "configuration": {
-    "components": [
+      },
       {
-        "id": "em3xsol",
+        "id": "em3xsul",
         "key": "uploadFile",
         "type": "file",
         "input": true,
