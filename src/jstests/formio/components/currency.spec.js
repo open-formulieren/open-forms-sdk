5e3f49b5af0b1332141a776c0050af4d933c38ed
silvia <silvia@maykinmedia.nl>
2022-02-02 12:18:29 +0100

    :white_check_mark: [open-formulieren/open-forms#1270] Test formatting with decimalLimit=0

---
+++ src/jstests/formio/components/currency.spec.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import _ from 'lodash';
+import {Formio} from 'react-formio';
+
+import OpenFormsModule from 'formio/module';
+import {currencyForm} from './fixtures/currency';
+
+// Use our custom components
+Formio.use(OpenFormsModule);
+
+describe('Currency Component', () => {
+  test('Currency component with 0 decimalLimit formatted correctly', (done) => {
+    let formJSON = _.cloneDeep(currencyForm);
+
+    const element = document.createElement('div');
+
+    Formio.createForm(element, formJSON).then(form => {
+      form.setPristine(false);
+      const component = form.getComponent('currency');
+      const formattedValue = component.getValueAsString(1);
+
+      expect(formattedValue).toEqual('€1');
+
+      done();
+
+    }).catch(done);
+  });
+});
