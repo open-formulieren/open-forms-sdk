5605235984f546471157bcad96a8b2a7e7cf8d4b
SilviaAmAm <silvia@maykinmedia.nl>
2022-03-18 15:25:06 +0100

    :white_check_mark: [open-formulieren/open-forms#1449] Test max number of files

---
+++ src/jstests/formio/components/file.spec.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {Formio} from 'react-formio';
+import _ from 'lodash';
+
+// Use our custom components
+import OpenFormsModule from 'formio/module';
+
+
+Formio.use(OpenFormsModule);
+
+
+const maxNFilesForm = {
+  type: 'form',
+  components: [
+    {label: 'Upload multiple files', key: 'multipleFiles', type: 'file', multiple: true, maxNumberOfFiles: 2},
+  ],
+  title: 'Test Files form',
+	display: 'Test Files form',
+	name: 'testFilesForm',
+	path: 'testFilesForm',
+};
+
+
+describe('Multiple File Component', () => {
+  test('Uploading 1 file gives no error', (done) => {
+    let formJSON = _.cloneDeep(maxNFilesForm);
+
+    const element = document.createElement('div');
+
+    Formio.createForm(element, formJSON).then(form => {
+      form.setPristine(false);
+      const component = form.getComponent('multipleFiles');
+      component.upload([
+          {name: 'File 1', size: '50'},
+      ]);
+
+      expect(!!component.error).toBeFalsy();
+
+      done();
+
+    }).catch(done);
+  });
+
+  test('Uploading 3 files gives an error', (done) => {
+    let formJSON = _.cloneDeep(maxNFilesForm);
+
+    const element = document.createElement('div');
+
+    Formio.createForm(element, formJSON).then(form => {
+      form.setPristine(false);
+      const component = form.getComponent('multipleFiles');
+      component.upload([
+          {name: 'File 1', size: '50'},
+          {name: 'File 2', size: '50'},
+          {name: 'File 3', size: '50'}
+      ]);
+
+      expect(!!component.error).toBeTruthy();
+      expect(component.error.message).toEqual('Too many files added. The maximum allowed number of files is 2.');
+      done();
+
+    }).catch(done);
+  });
+
+});
+
+
