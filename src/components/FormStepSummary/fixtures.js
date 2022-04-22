94a2de1b4b54a72f605b806a38724e38d5560ab7
SilviaAmAm <silvia@maykinmedia.nl>
2022-04-22 11:37:13 +0200

    :white_check_mark: [open-formulieren/open-forms#1527] Test hidden fields in summary

--- src/components/FormStepSummary/fixtures.js
+++ src/components/FormStepSummary/fixtures.js
@@ -186,3 +186,49 @@ export const testStepEmptyFields = {
     ]
   }
 };
+
+export const testStepHiddenFields = {
+  "submissionStep": {
+    "id": "31bd9fc8-57b7-4b32-bae7-a0b6d8c076cc",
+    "name": "Details",
+    "url": "http://testserver.nl/api/v1/submissions/774db9fc-1c0f-42ef-a7f7-b96d8c7d5905/steps/31bd9fc8-57b7-4b32-bae7-a0b6d8c076cc",
+    "formStep": "http://testserver.nl/api/v1/forms/3ef4c822-c6c7-496d-99d6-897c1d1c1219/steps/31bd9fc8-57b7-4b32-bae7-a0b6d8c076cc",
+    "isApplicable": true,
+    "completed": true,
+    "optional": false,
+    "canSubmit": true
+  },
+  "title": "Test hidden fields",
+  "data": {
+    "visibleField": "Some data",
+    "hiddenField": ""
+  },
+  "configuration": {
+    "components": [
+      {
+        "id": "em3xsol",
+        "key": "visibleField",
+        "type": "textfield",
+        "input": true,
+        "multiple": false,
+        "label": "Visible field",
+        "hidden": false,
+        "data": {
+          "value": undefined,
+        }
+      },
+      {
+        "id": "em3xsal",
+        "key": "hiddenField",
+        "type": "textfield",
+        "input": true,
+        "multiple": false,
+        "hidden": true,
+        "label": "Hidden field",
+        "data": {
+          "value": undefined,
+        }
+      }
+    ]
+  }
+};
