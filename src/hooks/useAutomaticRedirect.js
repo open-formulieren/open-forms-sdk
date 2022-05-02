cafc793e5d4a4b58c57b1a707c7ea6434832484d
SilviaAmAm <silvia@maykinmedia.nl>
2022-05-02 16:40:16 +0200

    :bug: [open-formulieren/open-forms#1533] Test for location before redirecting

--- src/hooks/useAutomaticRedirect.js
+++ src/hooks/useAutomaticRedirect.js
@@ -1,20 +1,32 @@
-import {useEffect} from 'react';
+import {useContext, useEffect} from 'react';
+import {useLocation} from 'react-router-dom';
 
 import useStartSubmission from './useStartSubmission';
 import {getLoginRedirectUrl} from 'components/utils';
+import {ConfigContext} from '../Context';
 
 
 const useAutomaticRedirect = (form) => {
+  const location = useLocation();
+  const {basePath} = useContext(ConfigContext);
+
+  // In basePath, the end / is stripped
+  let pathname = location.pathname;
+  if (pathname.endsWith('/')) {
+    pathname = pathname.slice(0, pathname.length - 1);
+  }
+
+  const isStartPage = pathname === basePath;
   const autoRedirectUrl = getLoginRedirectUrl(form);
   const doStart = useStartSubmission();
 
-  const shouldAutomaticallyRedirect = !doStart && autoRedirectUrl;
+  const shouldAutomaticallyRedirect = isStartPage && !doStart && autoRedirectUrl;
 
   useEffect(() => {
     if (shouldAutomaticallyRedirect) {
       window.location = autoRedirectUrl;
     }
-  }, [shouldAutomaticallyRedirect, autoRedirectUrl])
+  }, [shouldAutomaticallyRedirect, autoRedirectUrl]);
 
   return shouldAutomaticallyRedirect;
 };
--- src/sdk.js
+++ src/sdk.js
@@ -104,7 +104,7 @@ class OpenForm {
     ReactDOM.render(
       <React.StrictMode>
         <IntlProvider messages={messages} locale={lang} defaultLocale="nl">
-          <ConfigContext.Provider value={{baseUrl: this.baseUrl}}>
+          <ConfigContext.Provider value={{baseUrl: this.baseUrl, basePath: this.basePath}}>
             <FormioTranslations.Provider value={{i18n: translations, language: lang}}>
               <Router basename={this.basePath}>
                 <App form={this.formObject} />
