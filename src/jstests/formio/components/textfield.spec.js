e012d55786dab5158c15c7fefec9896336bea93b
Sergei Maertens <sergei@maykinmedia.nl>
2022-08-23 17:13:16 +0200

    :green_heart: [open-formulieren/open-forms#1832] -- update tests for new (debounce) behaviour
    Added the tests from open-formulieren/open-forms#1717 as well.

--- src/jstests/formio/components/textfield.spec.js
+++ src/jstests/formio/components/textfield.spec.js
@@ -23,6 +23,7 @@ describe('TextField Component', () => {
       form.setPristine(false);
       const componentCity = form.getComponent('city');
       componentCity.handleSettingLocationData({postcode: '1111AA', houseNumber: '1'});
+      componentCity._debouncedSetLocationData.flush();
 
       setTimeout(() => {
         expect(componentCity.getValue()).toEqual('Amsterdam');
@@ -32,7 +33,31 @@ describe('TextField Component', () => {
     }).catch(done);
   });
 
-  test('TextField with address prefill refreshes city on invalid data', (done) => {
+  test('TextField (readonly) with address prefill refreshes city on invalid data', (done) => {
+    let formJSON = _.cloneDeep(addressPrefillForm);
+    formJSON.components[2].disabled = true;
+    formJSON.components[3].disabled = true;
+    apiModule.get.mockResolvedValue({});
+
+    const element = document.createElement('div');
+
+    Formio.createForm(element, formJSON).then(form => {
+      form.setPristine(false);
+      const componentCity = form.getComponent('city');
+      componentCity.setValue('Amsterdam');
+
+      componentCity.handleSettingLocationData({postcode: '0000AA', houseNumber: '0'});
+      componentCity._debouncedSetLocationData.flush();
+
+      setTimeout(() => {
+        expect(componentCity.getValue()).toEqual('');
+        done();
+      }, 300);
+
+    }).catch(done);
+  });
+
+  test('TextField (editable) with address prefill does not modify city if already filled', (done) => {
     let formJSON = _.cloneDeep(addressPrefillForm);
     apiModule.get.mockResolvedValue({});
 
@@ -44,9 +69,11 @@ describe('TextField Component', () => {
       componentCity.setValue('Amsterdam');
 
       componentCity.handleSettingLocationData({postcode: '0000AA', houseNumber: '0'});
+      componentCity.setLocationData;  // access so that the debounced method is created
+      componentCity._debouncedSetLocationData.flush();
 
       setTimeout(() => {
-        expect(componentCity.getValue()).toEqual('');
+        expect(componentCity.getValue()).toEqual('Amsterdam');
         done();
       }, 300);
 
@@ -63,6 +90,7 @@ describe('TextField Component', () => {
       form.setPristine(false);
       const componentStreet = form.getComponent('streetName');
       componentStreet.handleSettingLocationData({postcode: '1111AA', houseNumber: '1'});
+      componentStreet._debouncedSetLocationData.flush();
 
       setTimeout(() => {
         expect(componentStreet.getValue()).toEqual('Beautiful Street');
@@ -72,7 +100,31 @@ describe('TextField Component', () => {
     }).catch(done);
   });
 
-  test('TextField with address prefill refreshes street on invalid data', (done) => {
+  test('TextField (readonly) with address prefill refreshes street on invalid data', (done) => {
+    let formJSON = _.cloneDeep(addressPrefillForm);
+    formJSON.components[2].disabled = true;
+    formJSON.components[3].disabled = true;
+    apiModule.get.mockResolvedValue({});
+
+    const element = document.createElement('div');
+
+    Formio.createForm(element, formJSON).then(form => {
+      form.setPristine(false);
+      const componentStreet = form.getComponent('streetName');
+      componentStreet.setValue('Beautiful Street');
+
+      componentStreet.handleSettingLocationData({postcode: '0000AA', houseNumber: '0'});
+      componentStreet._debouncedSetLocationData.flush();
+
+      setTimeout(() => {
+        expect(componentStreet.getValue()).toEqual('');
+        done();
+      }, 300);
+
+    }).catch(done);
+  });
+
+  test('TextField (editable) with address prefill (invalid data) does not modify street if already filled', (done) => {
     let formJSON = _.cloneDeep(addressPrefillForm);
     apiModule.get.mockResolvedValue({});
 
@@ -84,9 +136,11 @@ describe('TextField Component', () => {
       componentStreet.setValue('Beautiful Street');
 
       componentStreet.handleSettingLocationData({postcode: '0000AA', houseNumber: '0'});
+      componentStreet.setLocationData;  // access so that the debounced method is created
+      componentStreet._debouncedSetLocationData.flush();
 
       setTimeout(() => {
-        expect(componentStreet.getValue()).toEqual('');
+        expect(componentStreet.getValue()).toEqual('Beautiful Street');
         done();
       }, 300);
 
