6a3efc478fa2cef688817351e35d93270d6b8f67
Sergei Maertens <sergei@maykinmedia.nl>
2022-10-05 17:02:34 +0200

    :lock: Ensure that the CSRF token is sent in file upload/delete calls
    When/if the backend enforces the CSRF checks, we need to ensure that:
    
    * the CSRF token is sent as an HTTP header
    * the CSRF cookie is sent along
    
    This unfortunately required duplicating a lot of Formio code :(

--- src/formio/components/FileField.js
+++ src/formio/components/FileField.js
@@ -1,4 +1,138 @@
 import { Formio } from 'react-formio';
+import { default as formioUrlStorage } from 'formiojs/providers/storage/url';
+
+import { CSRFToken } from 'headers';
+
+const addCSRFToken = (xhr) => {
+  const csrfTokenValue = CSRFToken.getValue();
+  if (csrfTokenValue != null && csrfTokenValue) {
+    xhr.setRequestHeader(CSRFToken.headerName, csrfTokenValue);
+  }
+}
+
+
+const CSRFEnabledUrl = (formio) => {
+  const defaultUrlStorage = formioUrlStorage(formio);
+
+  // Taken and modified from Formio's formiojs/providers/storage/url.js
+  const xhrRequest = (url, name, query, data, options, progressCallback, abortCallback) => {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      const json = (typeof data === 'string');
+      const fd = new FormData();
+
+      if (typeof progressCallback === 'function') {
+        xhr.upload.onprogress = progressCallback;
+      }
+
+      if (typeof abortCallback === 'function') {
+        abortCallback(() => xhr.abort());
+      }
+
+      if (!json) {
+        for (const key in data) {
+          fd.append(key, data[key]);
+        }
+      }
+
+      xhr.onload = () => {
+        if (xhr.status >= 200 && xhr.status < 300) {
+          // Need to test if xhr.response is decoded or not.
+          let respData = {};
+          try {
+            respData = (typeof xhr.response === 'string') ? JSON.parse(xhr.response) : {};
+            respData = (respData && respData.data) ? respData.data : respData;
+          }
+          catch (err) {
+            respData = {};
+          }
+
+          // Get the url of the file.
+          let respUrl = respData.hasOwnProperty('url') ? respData.url : `${xhr.responseURL}/${name}`;
+
+          // If they provide relative url, then prepend the url.
+          if (respUrl && respUrl[0] === '/') {
+            respUrl = `${url}${respUrl}`;
+          }
+          resolve({ url: respUrl, data: respData });
+        }
+        else {
+          reject(xhr.response || 'Unable to upload file');
+        }
+      };
+
+      xhr.onerror = () => reject(xhr);
+      xhr.onabort = () => reject(xhr);
+
+      let requestUrl = url + (url.indexOf('?') > -1 ? '&' : '?');
+      for (const key in query) {
+        requestUrl += `${key}=${query[key]}&`;
+      }
+      if (requestUrl[requestUrl.length - 1] === '&') {
+        requestUrl = requestUrl.substr(0, requestUrl.length - 1);
+      }
+
+      xhr.open('POST', requestUrl);
+      if (json) {
+        xhr.setRequestHeader('Content-Type', 'application/json');
+      }
+      // Open Forms: removed formio JWT token - we don't use this
+
+      // Open Forms: added CSRF token header
+      addCSRFToken(xhr);
+
+      //Overrides previous request props
+      if (options) {
+        const parsedOptions = typeof options === 'string' ? JSON.parse(options) : options;
+        for (const prop in parsedOptions) {
+          xhr[prop] = parsedOptions[prop];
+        }
+      }
+      xhr.send(json ? data : fd);
+    });
+  };
+
+  return {
+    ...defaultUrlStorage,
+    title: 'CSRFEnabledUrl',
+    // Taken from Formio's formiojs/providers/storage/url.js to replace xhrRequest call
+    uploadFile(file, name, dir, progressCallback, url, options, fileKey, groupPermissions, groupId, abortCallback) {
+      const uploadRequest = function(form) {
+        return xhrRequest(url, name, {
+          baseUrl: encodeURIComponent(formio.projectUrl),
+          project: form ? form.project : '',
+          form: form ? form._id : ''
+        }, {
+          [fileKey]:file,
+          name,
+          dir
+        }, options, progressCallback, abortCallback).then(response => {
+          // Store the project and form url along with the metadata.
+          response.data = response.data || {};
+          response.data.baseUrl = formio.projectUrl;
+          response.data.project = form ? form.project : '';
+          response.data.form = form ? form._id : '';
+          return {
+            storage: 'url',
+            name,
+            url: response.url,
+            size: file.size,
+            type: file.type,
+            data: response.data
+          };
+        });
+      };
+      if (file.private && formio.formId) {
+        return formio.loadForm().then((form) => uploadRequest(form));
+      }
+      else {
+        return uploadRequest();
+      }
+    },
+  };
+};
+
+CSRFEnabledUrl.title = 'CSRFEnabledUrl';
 
 /**
  * Extend the default file field to modify it to our needs.
@@ -75,4 +209,5 @@ class FileField extends Formio.Components.components.file {
 }
 
 
+export {CSRFEnabledUrl};
 export default FileField;
