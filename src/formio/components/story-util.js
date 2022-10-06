901e82ed4d682cf89197ea7162f8686cbe54dcd0
Sergei Maertens <sergei@maykinmedia.nl>
2022-10-06 10:50:12 +0200

    :bug: Add missing formio options to storybook config
    This fixes some funky styles

--- src/formio/components/story-util.js
+++ src/formio/components/story-util.js
@@ -1,12 +1,26 @@
 import { Form } from 'react-formio';
 
+import {PREFIX}  from 'formio/constants';
+
 export const FormioForm = ({ form }) => (<Form form={form} />);
 
 export const FormioComponent = ({ component }) => (
-  <Form form={{
-    type: 'form',
-    components: [component]
-  }}/>);
+  <Form
+    form={{
+      type: 'form',
+      components: [component],
+    }}
+    options={{
+      noAlerts: true,
+      language: 'nl', // TODO - get this from the i18n addon
+      // i18n: formioTranslations.i18n, // TODO - get this from backend/bake into build?
+      evalContext: {
+        ofPrefix: `${PREFIX}-`,
+        requiredFieldsWithAsterisk: true, // TODO: create addon to configure this?
+      },
+    }}
+  />
+);
 
 export const FormDecorator = (Story) => (
   <div
