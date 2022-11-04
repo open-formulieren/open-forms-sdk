235dc654869066c256852bed04b1e42d91791a44
Chris Wesseling <chris@maykinmedia.nl>
2022-11-04 12:06:27 +0100

    💄 [#2253] Add the LanguageSelection to the UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,42 @@
 import React from 'react';
+import ReactDOM from "react-dom";
 import {Switch, Route} from 'react-router-dom';
 
 import Form from 'components/Form';
 import { Layout, LayoutRow } from 'components/Layout';
 import ManageAppointment from 'components/appointments/ManageAppointment';
 
+import LanguageSelection from "components/LanguageSelection";
 
 /*
 Top level router - routing between an actual form or supporting screens.
  */
-const App = (props) => {
+const App = ({ languageSelectorTarget, ...formProps }) => {
+  const LanguageSwitcher = () =>
+    languageSelectorTarget ? (
+      ReactDOM.createPortal(<LanguageSelection />, languageSelectorTarget)
+    ) : (
+      <LayoutRow>
+        <LanguageSelection />
+      </LayoutRow>
+    );
+
   return (
     <Layout>
+      <LanguageSwitcher />
       <LayoutRow>
-
         <Switch>
-
           {/* Anything dealing with appointments gets routed to it's own sub-router */}
           <Route path="/afspraak*" component={ManageAppointment} />
 
           {/* All the rest goes to the actual form flow */}
           <Route path="/">
-            <Form {...props} />
+            <Form {...formProps} />
           </Route>
-
         </Switch>
-
       </LayoutRow>
     </Layout>
   );
 };
-
 
 export default App;
