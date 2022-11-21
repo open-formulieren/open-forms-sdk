d9e4d5e7f07431b3999fdfdeabc1b238cd37fd36
Jiro Ghianni <jiro@maykinmedia.nl>
2022-11-21 12:30:45 +0100

    experiment with card-titles as page-titles

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useTitle } from 'react-use';
 
 import Caption from 'components/Caption';
 import { getBEMClassName } from 'utils';
@@ -30,6 +31,7 @@ CardTitle.propTypes = {
 };
 
 const Card = ({ children, title, titleComponent, caption, captionComponent, blockClassName='card', modifiers=[] }) => {
+  useTitle(title ? title : "Startformulier");
   const className = getBEMClassName(
     blockClassName,
     modifiers,
