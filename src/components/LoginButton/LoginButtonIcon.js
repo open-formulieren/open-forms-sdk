78454b77bbf78dadf2b32e33bb311c7a7fda1968
Jiro Ghianni <jiro@maykinmedia.nl>
2023-01-27 17:26:15 +0100

    :wheelchair: [#322] add branch

--- src/components/LoginButton/LoginButtonIcon.js
+++ src/components/LoginButton/LoginButtonIcon.js
@@ -1,14 +1,18 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import {getBEMClassName} from 'utils';
+
+
+import { getBEMClassName } from 'utils';
+
 
 const LoginButtonIcon = ({identifier, logo}) => {
   if (!logo) return null;
 
-  const {href, imageSrc, title} = logo;
+  const {href, imageSrc, title, logoAppearance} = logo;
   return (
     <a
+      // Implement CSS classname modifiers openforms-login-button-logo--{dark|light} depending on backend information
       className={getBEMClassName('login-button-logo')}
       href={href}
       key={identifier}
@@ -21,11 +25,14 @@ const LoginButtonIcon = ({identifier, logo}) => {
 };
 
 LoginButtonIcon.propTypes = {
+  // Add prop appearance with possible values dark and light to LoginButtonIcon component, which configures the modifier of the component
+  // Implement CSS classname modifiers openforms-login-button-logo--{dark|light} depending on backend information
   identifier: PropTypes.string.isRequired,
   logo: PropTypes.shape({
     title: PropTypes.string.isRequired,
     imageSrc: PropTypes.string.isRequired,
     href: PropTypes.string,
+    logoAppearance: PropTypes.string,
   }),
 };
 
