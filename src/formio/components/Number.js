21003970d88f171030a0e606b200eb00a43b043c
SilviaAmAm <silvia@maykinmedia.nl>
2023-03-20 16:31:45 +0100

    :adhesive_bandage: [open-formulieren/open-forms#2903] Fix emptying number/currency data
    Backport-Of: open-formulieren/open-forms-sdk#379

--- src/formio/components/Number.js
+++ src/formio/components/Number.js
@@ -1,5 +1,6 @@
-import { Formio } from 'react-formio';
-import { maskInput } from '@formio/vanilla-text-mask';
+import {maskInput} from '@formio/vanilla-text-mask';
+import {set} from 'lodash';
+import {Formio} from 'react-formio';
 
 import { applyPrefix } from '../utils';
 
@@ -49,6 +50,32 @@ class Number extends Formio.Components.components.number {
       mask: this.numberMask,
       shadowRoot: this.root ? this.root.shadowRoot : null,
     });
+  }
+
+  // Overwrite Formio method because of issue OF#2903
+  set dataValue(value) {
+    if (
+      !this.allowData ||
+      !this.key ||
+      (!this.visible && this.component.clearOnHide && !this.rootPristine)
+    ) {
+      return;
+    }
+    // Issue 2903 - make it possible to set the value to null. Otherwise, the user cannot clear the
+    // value of a number/currency field if it has already been saved.
+    if (value === undefined) {
+      this.unset();
+      return;
+    }
+
+    value = this.hook('setDataValue', value, this.key, this._data);
+    set(this._data, this.key, value);
+  }
+
+  // Inheritance of getter/setter: since we have overwritten the setter, we must define a
+  // getter or the property 'dataValue' will always return undefined
+  get dataValue() {
+    return super.dataValue;
   }
 }
 
