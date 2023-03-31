97c20f201eb460c70e3506071f50489a0da75360
Paul Schilling <mail@paulschilling.de>
2023-03-31 11:03:58 +0200

    [#2875] fix siteimprove
    * replace usePrevious hook for retrieving previous component state
    * send full URL to siteimprove instead of path

--- src/hooks/usePageViews.js
+++ src/hooks/usePageViews.js
@@ -1,12 +1,28 @@
-import {useEffect} from 'react';
+import {useEffect, useRef} from 'react';
 import {useLocation} from 'react-router-dom';
-import {usePrevious} from 'react-use';
+
+function usePrevious(value) {
+  const ref = useRef({
+    value: value,
+    prev: null,
+  });
+
+  const current = ref.current.value;
+
+  if (value !== current) {
+    ref.current = {
+      value: value,
+      prev: current,
+    };
+  }
+
+  return ref.current.prev;
+}
 
 const isDev = process.env.NODE_ENV === 'development';
 
 const ANALYTICS_PROVIDERS = {
-  debug: async location =>
-    isDev && console.log(`Tracking navigation to ${location.pathname}${location.hash}`),
+  debug: async location => isDev && console.log(`Tracking navigation to ${location.href}`),
   gtag: async location => {
     /* Google Analytics
     Hashrouting support: https://support.google.com/analytics/thread/20971249/track-hashtag-in-url-but-after-site-was-loaded
@@ -22,23 +38,20 @@ const ANALYTICS_PROVIDERS = {
   siteimprove: async (location, previousLocation) =>
     /* SiteImprove
     Docs: https://support.siteimprove.com/hc/en-gb/articles/115001615171-Siteimprove-Analytics-Custom-Visit-Tracking
-    Unsure if SiteImprove subscribes to URL changes or not, if they do - we'll fire double events here.
-    Hashrouting support: Unknown, so we follow the same concept as Matomo by pretending there is no hash.
+		Note: siteimprove requires the full URL to track pages, not merely the path
     */
     window._sz &&
     window._sz.push([
       'trackdynamic',
       {
-        url: location.pathname + location.hash.substring(1),
-        ref:
-          (previousLocation && previousLocation.pathname + previousLocation.hash.substring(1)) ||
-          window.location.pathname + window.location.hash.substring(1),
+        url: location.href,
+        ref: previousLocation.href,
         title: document.title,
       },
     ]),
   matomoOrPiwik: async (location, previousLocation) => {
     /* Matomo, Piwik and Piwik PRO are all supported
-    Matomo: https://matomo.org/docs/guides/spa-tracking/
+		Matomo: https://developer.matomo.org/guides/spa-tracking
     Piwik: https://piwik.org/docs/tracking-javascript-guide/
     Piwik PRO: https://developers.piwik.pro/en/latest/data_collection/web/guides.html
     Hashrouting support: https://developer.matomo.org/guides/spa-tracking / alternatively: https://www.npmjs.com/package/@datapunt/matomo-tracker-react (from Amsterdam)
@@ -71,21 +84,17 @@ const trackPageView = location => {
  * @return {Void}
  */
 const usePageViews = async () => {
-  const location = useLocation();
+  let location = useLocation();
+  // the value of useLocation only contains the path, but we need access to the full URL as well
+  location.href = window.location.href;
   const previousLocation = usePrevious(location);
   useEffect(() => {
     // if there's no change, do nothing
-    if (
-      previousLocation &&
-      previousLocation.pathname === location.pathname &&
-      previousLocation.hash === location.hash
-    )
-      return;
+    if (previousLocation && previousLocation.href === location.href) return;
     trackPageView(location, previousLocation);
   }, [location, previousLocation]);
 };
 
 export default usePageViews;
 export {ANALYTICS_PROVIDERS};
-// exporting makes it possible to plug in other providers that are not supported out of
-// the box.
+// exporting makes it possible to plug in other providers that are not supported out of the box.
