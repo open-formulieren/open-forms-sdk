e8d40c384b57a370cf8f0f81f39c1994f1fd7c1b
Paul Schilling <mail@paulschilling.de>
2023-04-18 16:03:42 +0200

    [#2875] fix siteimprove
    * replace usePrevious hook for retrieving previous component state
    * send full URL to siteimprove instead of path
    
    Backport-of: open-formulieren/open-forms-sdk#387

--- src/hooks/usePageViews.js
+++ src/hooks/usePageViews.js
@@ -1,29 +1,59 @@
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import usePrevious from 'react-use/esm/usePrevious';
+import {useEffect, useRef} from 'react';
+import {useLocation} from 'react-router-dom';
 
 const isDev = process.env.NODE_ENV === 'development';
 
+function usePrevious(value) {
+  const ref = useRef({
+    value: value,
+    prev: null,
+  });
+
+  const current = ref.current.value;
+
+  if (value !== current) {
+    ref.current = {
+      value: value,
+      prev: current,
+    };
+  }
+
+  return ref.current.prev;
+}
+
 const ANALYTICS_PROVIDERS = {
-  debug: async (location) => (isDev && console.log(`Tracking navigation to ${location.pathname}`)),
-  gtag: async (location) => {
-    return window.gtag && window.gtag('event', 'page_view', {
-      page_location: location.href,
-      page_path: location.pathname,
-    });
+  debug: async location =>
+    isDev && console.log(`Tracking navigation to ${window.location.origin}${location.pathname}`),
+  gtag: async location => {
+    return (
+      window.gtag &&
+      window.gtag('event', 'page_view', {
+        page_location: location.href,
+        page_path: location.pathname,
+      })
+    );
   },
-  // https://support.siteimprove.com/hc/en-gb/articles/115001615171-Siteimprove-Analytics-Custom-Visit-Tracking
-  // unsure if SiteImprove subscribes to URL changes or not, if they do - we'll fire double events here.
-  siteimprove: async (location, previousLocation) => (window._sz && window._sz.push(['trackdynamic', {
-    url: location.pathname,
-    ref: (previousLocation && previousLocation.pathname) || window.location.pathname,
-    title: document.title,
-  }])),
+  siteimprove: async (location, previousLocation) =>
+    /* SiteImprove
+    Docs: https://support.siteimprove.com/hc/en-gb/articles/115001615171-Siteimprove-Analytics-Custom-Visit-Tracking
+    Note: siteimprove requires the full URL to track pages, not merely the path
+    */
+    window._sz &&
+    window._sz.push([
+      'trackdynamic',
+      {
+        url: window.location.origin + location.pathname + location.hash.substring(1),
+        ref:
+          previousLocation &&
+          window.location.origin + previousLocation.pathname + previousLocation.hash.substring(1),
+        title: document.title,
+      },
+    ]),
   // from: https://developer.matomo.org/guides/spa-tracking
   // alternatively: https://www.npmjs.com/package/@datapunt/matomo-tracker-react (from Amsterdam)
   matomoOrPiwik: async (location, previousLocation) => {
     /* Matomo, Piwik and Piwik PRO are all supported
-    Matomo: https://matomo.org/docs/guides/spa-tracking/
+    Matomo: https://developer.matomo.org/guides/spa-tracking
     Piwik: https://piwik.org/docs/tracking-javascript-guide/
     Piwik PRO: https://developers.piwik.pro/en/latest/data_collection/web/guides.html
      */
@@ -40,10 +70,13 @@ const ANALYTICS_PROVIDERS = {
  * We assume that the provider has been included already in the global scope of the
  * containing page where the SDK is embedded.
  */
-const trackPageView = (location) => {
-  const promises = Object.values(ANALYTICS_PROVIDERS).map(callback => callback(location));
-  Promise.all(promises)
-  .catch(error => {throw error});
+const trackPageView = (location, previousLocation) => {
+  const promises = Object.values(ANALYTICS_PROVIDERS).map(callback =>
+    callback(location, previousLocation)
+  );
+  Promise.all(promises).catch(error => {
+    throw error;
+  });
 };
 
 
@@ -62,6 +95,5 @@ const usePageViews = async () => {
 };
 
 export default usePageViews;
-export { ANALYTICS_PROVIDERS };
-// exporting makes it possible to plug in other providers that are not supported out of
-// the box.
+export {ANALYTICS_PROVIDERS};
+// exporting makes it possible to plug in other providers that are not supported out of the box.
