83d374594a7f59ff9c8497bc9367aec6bc8eb444
Paul Schilling <mail@paulschilling.de>
2023-05-01 12:52:23 +0200

    [#2863] change order of tab elements

--- src/hooks/useTitle.js
+++ src/hooks/useTitle.js
@@ -5,7 +5,7 @@ import {ConfigContext} from 'Context';
 
 const useTitle = localTitle => {
   const {titlePrefix} = useContext(ConfigContext);
-  return reactUseTitle(titlePrefix ? `${titlePrefix} | ${localTitle}` : localTitle);
+  return reactUseTitle(titlePrefix ? `${localTitle} | ${titlePrefix}` : localTitle);
 };
 
 export default useTitle;
