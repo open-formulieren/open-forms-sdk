e3dfc29fc6d2dc4d37cb5db6b7b40f2d8bb48d02
Julian Roeland <julian@maykinmedia.nl>
2023-05-12 15:36:14 +0200

    ✨ [open-formulieren/open-forms#3061] Added tests/label/description to the selectField.
    - Added label
    - Added description
    - Fixed classnames
    - Added interaction tests

--- src/components/forms/SelectField/SelectField.js
+++ src/components/forms/SelectField/SelectField.js
@@ -1,3 +1,4 @@
+import {FormFieldDescription, FormLabel, Paragraph} from '@utrecht/component-library-react';
 import SelectAsync from 'react-select';
 
 import {getBEMClassName} from '../../../utils';
@@ -11,29 +12,38 @@ export const SelectField = ({
   invalid = false,
   options = [],
 }) => {
+  const labelClassName = getBEMClassName('label', [isRequired && 'required'].filter(Boolean));
+
   return (
-    <SelectAsync
-      inputId={id}
-      name={id}
-      components={{DropdownIndicator: () => null, IndicatorSeparator: () => null}}
-      classNames={{
-        control: state => {
-          return getBEMClassName('rs-select', [
-            state.isFocused && 'focus',
-            disabled && 'disabled',
-            invalid && 'invalid',
-            isRequired && 'required',
-          ]);
-        },
-        menu: () => {
-          return getBEMClassName('rs-select-menu');
-        },
-        option: state => {
-          return getBEMClassName('rs-select-option', [state.isFocused && 'focus']);
-        },
-      }}
-      options={options}
-      unstyled
-    ></SelectAsync>
+    <>
+      <Paragraph className={labelClassName}>
+        <FormLabel htmlFor={id}>{label}</FormLabel>
+      </Paragraph>
+      <SelectAsync
+        inputId={id}
+        name={id}
+        components={{DropdownIndicator: () => null, IndicatorSeparator: () => null}}
+        classNames={{
+          control: state => {
+            return getBEMClassName('rs-select', [
+              state.isFocused && 'focus',
+              disabled && 'disabled',
+              invalid && 'invalid',
+              isRequired && 'required',
+            ]);
+          },
+          menu: () => {
+            return getBEMClassName('rs-select-menu');
+          },
+          option: state => {
+            return getBEMClassName('rs-select-option', [state.isFocused && 'focus']);
+          },
+        }}
+        options={options}
+        unstyled
+        isDisabled={disabled}
+      ></SelectAsync>
+      {description && <FormFieldDescription invalid={invalid}>{description}</FormFieldDescription>}
+    </>
   );
 };
