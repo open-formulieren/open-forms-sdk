21197c2fbc0e6adbf8068dd1e7d65da9ea4a00df
Julian Roeland <julian@maykinmedia.nl>
2023-05-17 09:59:07 +0200

    :ok_hand: [open-formulieren/open-forms#3059] PR Changes applied
    - Fixed imports
    - Implemented EmailField as part of TextField for cleaner code, and less duplication.
    - Changed .toBeInDocument to .toBeVisible() in the tests.
    - Testing label with getByLabelText instead of getByText.

--- src/components/forms/EmailField/EmailField.js
+++ src/components/forms/EmailField/EmailField.js
@@ -1,44 +1,8 @@
-import {
-  FormFieldDescription,
-  FormLabel,
-  Paragraph,
-  Textbox,
-  FormField as UtrechtFormField,
-} from '@utrecht/component-library-react';
-import {Field} from 'formik';
-import React from 'react';
+import TextField from '../TextField/TextField';
 
-import {getBEMClassName} from 'utils';
+const EmailField = props => <TextField {...props} type="email" />;
 
-export const EmailField = ({
-  name = '',
-  label = '',
-  isRequired = false,
-  description = '',
-  id = '',
-  disabled = false,
-  invalid = false,
-}) => {
-  const labelClassName = getBEMClassName('label', [isRequired && 'required'].filter(Boolean));
+const {type, ...emailPropTypes} = TextField.propTypes;
+EmailField.propTypes = emailPropTypes;
 
-  const inputProps = {
-    name,
-    as: Textbox,
-    id,
-    disabled,
-    invalid,
-    type: 'email', // Set the input type to 'email'
-  };
-
-  return (
-    <UtrechtFormField type={'text'} invalid={invalid}>
-      <Paragraph className={labelClassName}>
-        <FormLabel htmlFor={id}>{label}</FormLabel>
-      </Paragraph>
-      <Paragraph>
-        <Field name={name} as={Textbox} {...inputProps} />
-      </Paragraph>
-      {description && <FormFieldDescription invalid={invalid}>{description}</FormFieldDescription>}
-    </UtrechtFormField>
-  );
-};
+export default EmailField;
