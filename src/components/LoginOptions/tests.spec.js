1a5685b7237dc60cb8798839d23110fe5c327f0c
SilviaAmAm <silvia@maykinmedia.nl>
2023-05-19 17:22:54 +0200

    :white_check_mark: [open-formulieren/open-forms#1530] Test cosign button has the right link

--- src/components/LoginOptions/tests.spec.js
+++ src/components/LoginOptions/tests.spec.js
@@ -1,10 +1,12 @@
 import {fireEvent} from '@testing-library/react';
+import {render as renderTest, screen} from '@testing-library/react';
 import messagesNL from 'i18n/compiled/nl.json';
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import {act} from 'react-dom/test-utils';
 import {IntlProvider} from 'react-intl';
 
+import {ConfigContext} from 'Context';
 import {buildForm} from 'api-mocks';
 import {LiteralsProvider} from 'components/Literal';
 
@@ -139,6 +141,58 @@ it('Login button has the right URL after cancelling log in', () => {
 
   const digidLoginButton = container.getElementsByTagName('a')[0];
   expect(digidLoginButton.href).toEqual(expectedUrl.toString());
+
+  window.location = location;
+});
+
+it('Login button has the right URL for cosign', () => {
+  const form = buildForm({
+    cosignLoginInfo: {
+      identifier: 'digid',
+      label: 'DigiD',
+      url: 'https://open-forms.nl/auth/form-slug/digid/start?next=https://open-forms.nl/submissions/form-slug/find/',
+      logo: {
+        title: 'DigiD simulatie',
+        imageSrc: '/digid.png',
+        href: 'https://www.digid.nl/',
+        appearance: 'light',
+      },
+      isForGemachtigde: false,
+    },
+  });
+
+  const onFormStart = jest.fn(e => e.preventDefault());
+
+  const {location} = window;
+  delete window.location;
+  window.location = {
+    href: 'https://open-forms.nl/form-slug',
+  };
+
+  renderTest(
+    <IntlProvider locale="nl" messages={messagesNL}>
+      <LiteralsProvider literals={{beginText: {resolved: 'Begin Form'}}}>
+        <ConfigContext.Provider
+          value={{
+            baseUrl: 'https://open-forms.nl/api/v2/',
+          }}
+        >
+          <LoginOptions form={form} onFormStart={onFormStart} />
+        </ConfigContext.Provider>
+      </LiteralsProvider>
+    </IntlProvider>,
+    container
+  );
+
+  const expectedUrl = new URL(
+    'https://open-forms.nl/auth/form-slug/digid/start?' +
+      'next=https%3A%2F%2Fopen-forms.nl%2Fsubmissions%2Fform-slug%2Ffind%2F%3F' +
+      'next%3Dhttps%253A%252F%252Fopen-forms.nl%252Fform-slug%252Fcosign%252Fcheck'
+  );
+
+  const cosignLoginButton = screen.queryByRole('link', {name: 'Inloggen met DigiD'});
+  expect(cosignLoginButton).toBeInTheDocument();
+  expect(cosignLoginButton.href).toEqual(expectedUrl.href);
 
   window.location = location;
 });
