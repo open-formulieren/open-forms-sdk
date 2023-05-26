cc35ddc62341bfbbdcb2da656cbe00f7bf2a535f
Sergei Maertens <sergei@maykinmedia.nl>
2023-05-26 14:03:24 +0200

    :tada: [open-formulieren/open-forms#3063] -- new component boilerplate

---
+++ src/components/appointments/ChooseProductStep.js
@@ -0,0 +1,10 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const ChooseProductStep = () => {
+  return <>ChooseProductStep</>;
+};
+
+ChooseProductStep.propTypes = {};
+
+export default ChooseProductStep;
