3b1085605193b8eb891e712e44155b00f30f7aa1
Sergei Maertens <sergei@maykinmedia.nl>
2023-05-26 14:54:34 +0200

    :sparkles: [open-formulieren/open-forms#3063] -- add 'add another product' button
    Co-authored-by: @Xaohs

--- src/components/appointments/ChooseProductStep.js
+++ src/components/appointments/ChooseProductStep.js
@@ -1,6 +1,11 @@
 import {FieldArray, useFormikContext} from 'formik';
 import PropTypes from 'prop-types';
 import React from 'react';
+import {FormattedMessage} from 'react-intl';
+
+import Button from 'components/Button';
+import FAIcon from 'components/FAIcon';
+import {getBEMClassName} from 'utils';
 
 import Product from './Product';
 
@@ -10,7 +15,7 @@ const ChooseProductStep = () => {
   return (
     <FieldArray name="products">
       {arrayHelpers => (
-        <>
+        <div className={getBEMClassName('editgrid')}>
           {products.map(({product: productId}, index) => (
             // blank blocks don't have a product selected yet -> so the index is added
             // to make the key guaranteed unique
@@ -18,7 +23,21 @@ const ChooseProductStep = () => {
               <Product namePrefix="products" index={index} />
             </div>
           ))}
-        </>
+
+          <div className={getBEMClassName('editgrid__add-button')}>
+            <Button
+              type="button"
+              variant="primary"
+              onClick={() => arrayHelpers.push({product: '', amount: 1})}
+            >
+              <FAIcon icon="plus" />
+              <FormattedMessage
+                description="Appointments: add additional product/service button text"
+                defaultMessage="Add another product"
+              />
+            </Button>
+          </div>
+        </div>
       )}
     </FieldArray>
   );
