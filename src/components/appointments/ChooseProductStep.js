fd70b7ddb7c1f4404437a0fe580a768d505a1830
Sergei Maertens <sergei@maykinmedia.nl>
2023-05-26 14:42:27 +0200

    :sparkles: [open-formulieren/open-forms#3063] -- incorporate formik fieldarray for product selection step
    Co-authored-by: @Xaohs

--- src/components/appointments/ChooseProductStep.js
+++ src/components/appointments/ChooseProductStep.js
@@ -1,8 +1,27 @@
+import {FieldArray, useFormikContext} from 'formik';
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import Product from './Product';
+
 const ChooseProductStep = () => {
-  return <>ChooseProductStep</>;
+  const {values} = useFormikContext();
+  const products = values?.products || [];
+  return (
+    <FieldArray name="products">
+      {arrayHelpers => (
+        <>
+          {products.map(({product: productId}, index) => (
+            // blank blocks don't have a product selected yet -> so the index is added
+            // to make the key guaranteed unique
+            <div key={`${productId}-${index}`}>
+              <Product namePrefix="products" index={index} />
+            </div>
+          ))}
+        </>
+      )}
+    </FieldArray>
+  );
 };
 
 ChooseProductStep.propTypes = {};
