da4d2e91d4c03f8de10f2155d380b30af7a97578
Sergei Maertens <sergei@maykinmedia.nl>
2023-06-07 12:03:46 +0200

    :alien: [open-formulieren/open-forms#3067] -- start moving towards data routers of react-router
    Available since 6.4, this looks like the direction we should be moving
    w/r to the router setup. Effectively this deprecates using JSX for
    routes and instead start lifting routes up, making matching/hooks
    more powerful.

--- src/story-utils/decorators.js
+++ src/story-utils/decorators.js
@@ -3,6 +3,7 @@ import {MemoryRouter, Route, Routes} from 'react-router-dom';
 
 import {ConfigContext} from 'Context';
 import {BASE_URL} from 'api-mocks';
+import {Layout, LayoutRow} from 'components/Layout';
 import {LiteralsProvider} from 'components/Literal';
 
 export const ConfigDecorator = (Story, {args}) => (
@@ -22,10 +23,11 @@ const RouterStoryWrapper = ({route = '', children}) => {
   );
 };
 
-export const RouterDecorator = (Story, {args: {routerArgs = {}}}) => {
+export const DeprecatedRouterDecorator = (Story, {args: {routerArgs = {}}}) => {
+  const {route, ...rest} = routerArgs;
   return (
-    <MemoryRouter {...routerArgs}>
-      <RouterStoryWrapper route={routerArgs.route}>
+    <MemoryRouter {...rest}>
+      <RouterStoryWrapper route={route}>
         <Story />
       </RouterStoryWrapper>
     </MemoryRouter>
@@ -69,3 +71,19 @@ export const LiteralDecorator = (Story, {args}) => (
     <Story />
   </LiteralsProvider>
 );
+
+const PaddedDiv = ({className, children}) => (
+  <div style={{padding: '1em 0'}} className={className}>
+    {children}
+  </div>
+);
+
+export const LayoutDecorator = Story => {
+  return (
+    <Layout component={PaddedDiv}>
+      <LayoutRow>
+        <Story />
+      </LayoutRow>
+    </Layout>
+  );
+};
