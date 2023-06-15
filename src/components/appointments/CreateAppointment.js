bb79971dffc1497ca548a8f80bf735a27bc4097a
Sergei Maertens <sergei@maykinmedia.nl>
2023-06-15 16:07:12 +0200

    :sparkles: [open-formulieren/open-forms#3067] -- set up initial appointment top-level component
    The CreateAppointment component is the entrypoint for creating
    appointments. It defines the child routes to hook up to the top-level
    router, eventually.
    
    The form state is managed with formik and submission navigates you
    through the different steps, while marking the steps correctly in the
    progress indicator.

---
+++ src/components/appointments/CreateAppointment.js
@@ -0,0 +1,202 @@
+import {Form, Formik, useFormikContext} from 'formik';
+import PropTypes from 'prop-types';
+import React, {useContext, useState} from 'react';
+import {defineMessage, useIntl} from 'react-intl';
+import {Navigate, Outlet, matchPath, resolvePath, useLocation, useNavigate} from 'react-router-dom';
+
+import {ConfigContext} from 'Context';
+import Card from 'components/Card';
+import ErrorBoundary from 'components/ErrorBoundary';
+import FormDisplay from 'components/FormDisplay';
+import ProgressIndicatorDisplay from 'components/ProgressIndicator/ProgressIndicatorDisplay';
+import Types from 'types';
+
+import ChooseProductStep, {isStepValid as isProductStepValid} from './ChooseProductStep';
+import LocationAndTimeStep, {
+  isStepValid as isLocationAndTimeStepValid,
+} from './LocationAndTimeStep';
+
+const APPOINTMENT_STEPS = [
+  {
+    path: 'producten',
+    element: <ChooseProductStep />,
+    name: defineMessage({
+      description: "Appointments navbar title for '' step",
+      defaultMessage: 'Product',
+    }),
+    validCheck: isProductStepValid,
+  },
+  {
+    path: 'kalender',
+    element: <LocationAndTimeStep />,
+    name: defineMessage({
+      description: "Appointments navbar title for '' step",
+      defaultMessage: 'Location and time',
+    }),
+    validCheck: isLocationAndTimeStepValid,
+  },
+  {
+    path: 'contactgegevens',
+    element: <>Contactgegevens</>,
+    name: defineMessage({
+      description: "Appointments navbar title for '' step",
+      defaultMessage: 'Contact details',
+    }),
+    validCheck: () => false,
+  },
+];
+
+const APPOINTMENT_STEP_PATHS = APPOINTMENT_STEPS.map(s => s.path);
+
+/**
+ * Route subtree for appointment forms.
+ */
+export const routes = [
+  {
+    path: '',
+    element: <Navigate replace to={APPOINTMENT_STEP_PATHS[0]} />,
+  },
+  ...APPOINTMENT_STEPS.map(({path, element}) => ({path, element})),
+];
+
+/**
+ * Check if a given relative path from the routes matches the current location.
+ * @param currentPathname The current router location.pathname, from useLocation.
+ * @param path            The relative path to check for matches
+ */
+const checkMatchesPath = (currentPathname, path) => {
+  // we need to transform the path into a parent-route lookup, instead of using the
+  // default relative ./<foo> behaviour. The idea is that this component is mounted
+  // somewhere in a larger route definition but the exact parent route is not relevant.
+  const resolvedPath = resolvePath(`../${path}`, currentPathname);
+  // if the relative path is not the current URL, matchPath returns null, otherwise
+  // a match object.
+  const match = matchPath(resolvedPath.pathname, currentPathname);
+  return match !== null;
+};
+
+const AppointmentProgress = ({title, currentStep}) => {
+  const config = useContext(ConfigContext);
+  const {values} = useFormikContext();
+  const intl = useIntl();
+  const {pathname: currentPathname} = useLocation();
+
+  const [expanded, setExpanded] = useState(false);
+
+  const currentStepIndex = APPOINTMENT_STEP_PATHS.indexOf(currentStep);
+  const steps = APPOINTMENT_STEPS.map(({path, name, validCheck}) => {
+    const index = APPOINTMENT_STEP_PATHS.indexOf(path);
+    const previousStepIndex = Math.max(index - 1, 0);
+    const previousStepCompleted = APPOINTMENT_STEPS[previousStepIndex].validCheck(values);
+    const isValid = validCheck(values);
+    return {
+      uuid: `appointments-${path}`,
+      href: path,
+      isCompleted: previousStepCompleted && isValid,
+      isApplicable: true,
+      isCurrent: checkMatchesPath(currentPathname, path),
+      canNavigateTo: previousStepCompleted || isValid,
+      formDefinition: intl.formatMessage(name),
+    };
+  });
+
+  const ProgressIndicatorDisplayComponent =
+    config?.displayComponents?.progressIndicator ?? ProgressIndicatorDisplay;
+  return (
+    <ProgressIndicatorDisplayComponent
+      activeStepTitle={steps[currentStepIndex].formDefinition}
+      formTitle={title}
+      steps={steps}
+      hasSubmission
+      isStartPage={false}
+      // TODO
+      isSummary={false}
+      isConfirmation={false}
+      isSubmissionComplete={false}
+      areApplicableStepsCompleted={false}
+      showOverview
+      showConfirmation
+      expanded={expanded}
+      onExpandClick={() => setExpanded(!expanded)}
+    />
+  );
+};
+
+AppointmentProgress.propTypes = {
+  title: PropTypes.string.isRequired,
+  currentStep: PropTypes.oneOf(APPOINTMENT_STEP_PATHS).isRequired,
+};
+
+const CreateAppointment = ({form}) => {
+  const config = useContext(ConfigContext);
+  const navigate = useNavigate();
+  const {pathname: currentPathname} = useLocation();
+
+  const currentStep =
+    APPOINTMENT_STEP_PATHS.find(step => checkMatchesPath(currentPathname, step)) ||
+    APPOINTMENT_STEP_PATHS[0];
+
+  const FormDisplayComponent = config?.displayComponents?.form ?? FormDisplay;
+  return (
+    <Formik
+      validateOnChange={false}
+      validateOnBlur
+      initialValues={{
+        products: [
+          {
+            product: '',
+            amount: 1,
+          },
+        ],
+        location: '',
+        date: '',
+        time: '',
+        lastName: '',
+        dateOfBirth: '',
+        phoneNumber: '',
+      }}
+      onSubmit={(values, {setSubmitting}) => {
+        switch (currentStep) {
+          // last step -> actually submit everything?
+          case APPOINTMENT_STEP_PATHS.at(-1): {
+            console.log(values);
+            // TODO: post to API endpoint, handle validation errors
+            setSubmitting(false);
+            break;
+          }
+          // any step other than the last -> navigate to the next step
+          default: {
+            const index = APPOINTMENT_STEP_PATHS.indexOf(currentStep);
+            const nextStep = APPOINTMENT_STEP_PATHS[index + 1];
+            setSubmitting(false);
+            navigate(nextStep);
+            return;
+          }
+        }
+      }}
+      // TODO: hook up validationSchema from zod, see #435
+    >
+      {/* TODO: don't do inline style */}
+      <Form style={{width: '100%'}}>
+        <FormDisplayComponent
+          router={
+            <Card title={form.name} titleComponent="h1" modifiers={['mobile-header-hidden']}>
+              <ErrorBoundary>
+                <Outlet />
+              </ErrorBoundary>
+            </Card>
+          }
+          progressIndicator={<AppointmentProgress title={form.name} currentStep={currentStep} />}
+          showProgressIndicator={form.showProgressIndicator}
+          isPaymentOverview={false}
+        />
+      </Form>
+    </Formik>
+  );
+};
+
+CreateAppointment.propTypes = {
+  form: Types.Form.isRequired,
+};
+
+export default CreateAppointment;
