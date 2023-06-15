61071bb3b9f80d6b3af799850960f2c6eaa5da72
Sergei Maertens <sergei@maykinmedia.nl>
2023-06-15 16:02:31 +0200

    :recycle: [open-formulieren/open-forms#3067] -- restructure the productstep
    * Output the subtitle (h2)
    * Use the SubmitRow to declutter the code
    * Implement utility to check if the step is valid - to be used in the
      progress indicator and determines if the submit button is enabled or
      not

--- src/components/appointments/ChooseProductStep.js
+++ src/components/appointments/ChooseProductStep.js
@@ -1,73 +1,122 @@
 import {FieldArray, useFormikContext} from 'formik';
 import React from 'react';
-import {FormattedMessage} from 'react-intl';
+import {FormattedMessage, useIntl} from 'react-intl';
 
 import Button from 'components/Button';
+import Card, {CardTitle} from 'components/Card';
 import FAIcon from 'components/FAIcon';
 import {Toolbar, ToolbarList} from 'components/Toolbar';
+import useTitle from 'hooks/useTitle';
 import {getBEMClassName} from 'utils';
 
 import Product from './Product';
+import SubmitRow from './SubmitRow';
+
+const isValidProduct = ({product, amount}) => {
+  if (!product) return false;
+  if (amount <= 0) return false;
+  return true;
+};
+
+// TODO: replace with ZOD validation, see #435
+export const isStepValid = data => {
+  const products = data?.products;
+  if (!products || products.length <= 0) {
+    return false;
+  }
+  const allProductsComplete = products.every(isValidProduct);
+  return allProductsComplete;
+};
 
 const ChooseProductStep = () => {
+  const intl = useIntl();
   const {values} = useFormikContext();
+  useTitle(
+    intl.formatMessage({
+      description: 'Appointments products step page title',
+      defaultMessage: 'Product',
+    })
+  );
+
   const products = values?.products || [];
   const numProducts = Math.max(products.length, 1);
   return (
-    <FieldArray name="products">
-      {arrayHelpers => (
-        <div className={getBEMClassName('editgrid')}>
-          <div className={getBEMClassName('editgrid__groups')}>
-            {products.map(({product: productId}, index) => (
-              // blank blocks don't have a product selected yet -> so the index is added
-              // to make the key guaranteed unique
-              <div key={`${productId}-${index}`} className={getBEMClassName('editgrid__group')}>
-                <div className={getBEMClassName('editgrid__group-label')}>
-                  <FormattedMessage
-                    description="Appointments: single product label/header"
-                    defaultMessage="Product {number}/{total}"
-                    values={{number: index + 1, total: numProducts}}
-                  />
+    <>
+      <CardTitle
+        title={
+          <FormattedMessage
+            description="Appointments products step title"
+            defaultMessage="Select your product(s)"
+          />
+        }
+        component="h2"
+        headingType="subtitle"
+        modifiers={['padded']}
+      />
+
+      <FieldArray name="products">
+        {arrayHelpers => (
+          <div className={getBEMClassName('editgrid')}>
+            <div className={getBEMClassName('editgrid__groups')}>
+              {products.map(({product: productId}, index) => (
+                // blank blocks don't have a product selected yet -> so the index is added
+                // to make the key guaranteed unique
+                <div key={`${productId}-${index}`} className={getBEMClassName('editgrid__group')}>
+                  <div className={getBEMClassName('editgrid__group-label')}>
+                    <FormattedMessage
+                      description="Appointments: single product label/header"
+                      defaultMessage="Product {number}/{total}"
+                      values={{number: index + 1, total: numProducts}}
+                    />
+                  </div>
+
+                  <Product namePrefix="products" index={index} />
+
+                  {numProducts > 1 && (
+                    <Toolbar modifiers={['reverse']}>
+                      <ToolbarList>
+                        <Button
+                          type="button"
+                          variant="danger"
+                          onClick={() => arrayHelpers.remove(index)}
+                        >
+                          <FormattedMessage
+                            description="Appointments: remove product/service button text"
+                            defaultMessage="Remove"
+                          />
+                        </Button>
+                      </ToolbarList>
+                    </Toolbar>
+                  )}
                 </div>
+              ))}
+            </div>
 
-                <Product namePrefix="products" index={index} />
-
-                {numProducts > 1 && (
-                  <Toolbar modifiers={['reverse']}>
-                    <ToolbarList>
-                      <Button
-                        type="button"
-                        variant="danger"
-                        onClick={() => arrayHelpers.remove(index)}
-                      >
-                        <FormattedMessage
-                          description="Appointments: remove product/service button text"
-                          defaultMessage="Remove"
-                        />
-                      </Button>
-                    </ToolbarList>
-                  </Toolbar>
-                )}
-              </div>
-            ))}
+            <div className={getBEMClassName('editgrid__add-button')}>
+              <Button
+                type="button"
+                variant="primary"
+                onClick={() => arrayHelpers.push({product: '', amount: 1})}
+              >
+                <FAIcon icon="plus" />
+                <FormattedMessage
+                  description="Appointments: add additional product/service button text"
+                  defaultMessage="Add another product"
+                />
+              </Button>
+            </div>
           </div>
+        )}
+      </FieldArray>
 
-          <div className={getBEMClassName('editgrid__add-button')}>
-            <Button
-              type="button"
-              variant="primary"
-              onClick={() => arrayHelpers.push({product: '', amount: 1})}
-            >
-              <FAIcon icon="plus" />
-              <FormattedMessage
-                description="Appointments: add additional product/service button text"
-                defaultMessage="Add another product"
-              />
-            </Button>
-          </div>
-        </div>
-      )}
-    </FieldArray>
+      <SubmitRow
+        canSubmit={isStepValid(values)}
+        nextText={intl.formatMessage({
+          description: 'Appointments products step: next step text',
+          defaultMessage: 'Confirm products',
+        })}
+      />
+    </>
   );
 };
 
