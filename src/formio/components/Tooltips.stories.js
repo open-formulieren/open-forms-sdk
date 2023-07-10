9d53e1039c0ba1c9da4d4266da43c519c1068589
Chris Wesseling <chris@maykinmedia.nl>
2023-07-10 14:05:26 +0200

    🔥[#3209] Remove failing interaction test
    `userEvent.pointer` does not exist so an attempt to keep touching the icon
    with something like
    `userEvent.pointer([{keys: '[TouchA>]', target: left_tooltip_icon}])`
    also fails.
    
    To run storybook tests headless like in CI, and isolate it from your
    desktop pointer, install `@storybook/test-runner`.

--- src/formio/components/Tooltips.stories.js
+++ src/formio/components/Tooltips.stories.js
@@ -1,6 +1,3 @@
-import {expect} from '@storybook/jest';
-import {userEvent, within} from '@storybook/testing-library';
-
 import {withUtrechtDocument} from 'story-utils/decorators';
 
 import {SingleFormioComponent} from './story-util';
@@ -63,71 +60,5 @@ export const Default = {
         },
       ],
     },
-  },
-};
-
-export const RightPlacement = {
-  render: SingleFormioComponent,
-  args: {
-    key: 'cols1',
-    type: 'columns',
-    label: 'Columns 1',
-    extraComponentProperties: {
-      columns: [
-        {
-          size: 2,
-          components: [
-            {
-              key: 'howManyDroidsDidYouDestroy',
-              type: 'textfield',
-              label: '# droids destroyed?',
-              tooltip: 'This tooltip gets the default right placement.',
-            },
-          ],
-        },
-        {
-          size: 6,
-          components: [
-            {
-              key: 'whatAboutDroidekas',
-              type: 'textfield',
-              label: "What about Droideka's?",
-            },
-          ],
-        },
-        {
-          size: 4,
-          components: [
-            {
-              key: 'doYouTrustYourClones',
-              type: 'textfield',
-              label: 'Do you trust your clones?',
-              tooltip:
-                "This tooltip should automatically be placed  to the left, because there's no room to the right.",
-            },
-          ],
-        },
-      ],
-    },
-  },
-  play: async ({canvasElement, step}) => {
-    const canvas = within(canvasElement);
-
-    const left_label = canvas.getByText('# droids destroyed?');
-    const left_tooltip_icon = left_label.firstElementChild;
-
-    // tooltip element doesn't exist yet.
-    let left_tooltip;
-
-    await step('Hover the left tooltip', async () => {
-      userEvent.hover(left_tooltip_icon);
-      left_tooltip = await within(left_label).getByRole('tooltip');
-      expect(left_tooltip).toBeVisible();
-    });
-
-    await step('Stop hovering the left tooltip', async () => {
-      await userEvent.unhover(left_tooltip_icon);
-      await expect(left_tooltip).not.toBeVisible();
-    });
   },
 };
