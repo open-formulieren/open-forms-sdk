74df60d1d0fec2903c22e0284211f48b46052c99
Sergei Maertens <sergei@maykinmedia.nl>
2023-08-03 15:42:58 +0200

    :pencil: [open-formulieren/open-forms#3203] Add stories for Form.io React components

---
+++ src/components/formio/index.stories.js
@@ -0,0 +1,68 @@
+import {FormikDecorator} from 'story-utils/decorators';
+
+import {FormioComponent} from '.';
+
+export default {
+  title: 'Private API / Formio',
+  component: FormioComponent,
+  decorators: [FormikDecorator],
+};
+
+export const TextField = {
+  args: {
+    component: {
+      key: 'textfield',
+      type: 'textfield',
+      label: 'Text field',
+      validate: {
+        required: true,
+      },
+    },
+  },
+};
+
+export const EmailField = {
+  args: {
+    component: {
+      key: 'email',
+      type: 'email',
+      label: 'Email field',
+      validate: {
+        required: true,
+      },
+    },
+  },
+};
+
+export const DateField = {
+  args: {
+    component: {
+      key: 'date',
+      type: 'date',
+      label: 'Datefield',
+      validate: {
+        required: true,
+      },
+      openForms: {
+        widget: 'inputGroup',
+      },
+    },
+  },
+};
+
+export const Radio = {
+  args: {
+    component: {
+      key: 'radio',
+      type: 'radio',
+      label: 'Radio',
+      validate: {
+        required: true,
+      },
+      values: [
+        {value: 'opt1', label: 'Option 1'},
+        {value: 'opt2', label: 'Option 2'},
+      ],
+    },
+  },
+};
