c9c74307b002902f93df6b4a58186b660d907e73
vasileios <zigras00@gmail.com>
2023-08-25 14:36:27 +0200

    [#309] Added story for cosign

---
+++ src/formio/components/Cosign.stories.js
@@ -0,0 +1,40 @@
+import {withUtrechtDocument} from 'story-utils/decorators';
+
+import {SingleFormioComponent} from './story-util';
+
+export default {
+  title: 'Form.io components / Custom / Cosign',
+  decorators: [withUtrechtDocument],
+  args: {
+    key: 'cosign',
+    type: 'cosign',
+    label: 'Co-signer email address',
+    validateOn: 'blur',
+    authPlugin: 'digid',
+    extraComponentProperties: {},
+    evalContext: {},
+  },
+  argTypes: {
+    key: {type: {required: true}},
+    label: {type: {required: true}},
+    type: {table: {disable: true}},
+    validateOn: {table: {disable: true}},
+    authPlugin: {table: {disable: true}},
+    extraComponentProperties: {
+      description: `Any additional Form.io component properties, recursively merged into the
+        component definition.`,
+    },
+    evalContext: {table: {disable: true}},
+  },
+  parameters: {
+    controls: {sort: 'requiredFirst'},
+  },
+};
+
+export const Cosign = {
+  render: SingleFormioComponent,
+  args: {
+    key: 'cosign',
+    label: 'Co-signer email address',
+  },
+};
