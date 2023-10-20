8908faacf6ac9fd48726d8c5320ba3794f9d85db
Sergei Maertens <sergei@maykinmedia.nl>
2023-10-20 15:59:34 +0200

    :recycle: [#571] Refactor formio content component styling
    The class name is now set directly on the WYSIWYG container and the
    custom classes are applied as modifiers instead of standalone/
    utility classes, allowing us to clean up our CSS.
    
    Additionally, design tokens (with backwards compatible fallbacks) are
    introduced for the theming of each content variant.

--- src/formio/components/Content.js
+++ src/formio/components/Content.js
@@ -1,15 +1,16 @@
 import {Formio} from 'react-formio';
 
+import {getBEMClassName} from 'utils';
+
 /**
  * Extend the default Content field to modify it to our needs.
  */
 class ContentComponent extends Formio.Components.components.content {
   init() {
     super.init();
-    if (!this.component.customClass) {
-      this.component.customClass = '';
-    }
-    this.component.customClass += ' formio-component-content';
+    const {customClass = ''} = this.component;
+    // upstream template is broken (?) and doesn't actually call this.className
+    this.component.className = getBEMClassName('formio-content', customClass ? [customClass] : []);
   }
 }
 
