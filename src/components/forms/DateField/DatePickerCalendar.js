cd4a6bb0bddeb925b14fa32e7c06e844d8721ff9
SilviaAmAm <silvia@maykinmedia.nl>
2023-11-07 11:09:47 +0100

    :bug: [#467] Fix date picker component

--- src/components/forms/DateField/DatePickerCalendar.js
+++ src/components/forms/DateField/DatePickerCalendar.js
@@ -1,6 +1,6 @@
 // Calendar component documentation:
 // https://nl-design-system.github.io/utrecht/storybook-react/index.html?path=/docs/react-component-calendar--docs
-import {Calendar} from '@utrecht/component-library-react/dist/Calendar.mjs';
+import {Calendar} from '@utrecht/component-library-react/dist/Calendar';
 import {enGB, nl} from 'date-fns/locale';
 import React from 'react';
 import {FormattedMessage, useIntl} from 'react-intl';
