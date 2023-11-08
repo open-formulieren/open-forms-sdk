959901ff8ef67d95bdd2b8cb8912b8a1bc7786c2
Sergei Maertens <sergei@maykinmedia.nl>
2023-11-08 13:07:40 +0100

    :bug: [open-formulieren/open-forms#3577] Fix overflow issue because of duplicated error styles
    Suppress the formio styles alltogether and apply more NL DS styles
    for a more consistent look.
    
    The explicit width: 100% declarations are removed as they cause
    overflows due to box-sizing rules, but the behaviour without
    setting these appears to be correct in our and other's themes.

--- src/formio/components/Select.stories.js
+++ src/formio/components/Select.stories.js
@@ -1,6 +1,9 @@
-import {withUtrechtDocument} from 'story-utils/decorators';
+import {userEvent, within} from '@storybook/testing-library';
 
-import {SingleFormioComponent} from './story-util';
+import {withUtrechtDocument} from 'story-utils/decorators';
+import {sleep} from 'utils';
+
+import {MultipleFormioComponents, SingleFormioComponent} from './story-util';
 
 export default {
   title: 'Form.io components / Vanilla / Select',
@@ -47,5 +50,54 @@ export const Select = {
   args: {
     key: 'select',
     label: 'Welke optie is het meest onduidelijk?',
+  },
+};
+
+export const RequiredWithValidationError = {
+  render: MultipleFormioComponents,
+  args: {
+    components: [
+      {
+        type: 'select',
+        key: 'selectRequired',
+        label: 'Required select',
+        data: {
+          values: [
+            {
+              label: 'Optie A',
+              value: 'radioA',
+            },
+            {
+              label: 'Optie B',
+              value: 'radioB',
+            },
+            {
+              label: 'Optie C',
+              value: 'radioC',
+            },
+          ],
+        },
+        validate: {
+          required: true,
+        },
+      },
+      {
+        label: 'Check validations',
+        showValidations: true,
+        key: 'submit1',
+        type: 'button',
+        input: true,
+        action: 'notSubmit',
+      },
+    ],
+  },
+
+  play: async ({canvasElement}) => {
+    const canvas = within(canvasElement);
+
+    // just form.io things...
+    await sleep(100);
+
+    await userEvent.click(canvas.getByRole('button'));
   },
 };
