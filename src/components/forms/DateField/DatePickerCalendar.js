bac71e21242d72fe629fc5927c012bd2a4532ef3
SilviaAmAm <silvia@maykinmedia.nl>
2023-11-08 11:47:33 +0100

    :rewind: [#467] Restore old import that worked

--- src/components/forms/DateField/DatePickerCalendar.js
+++ src/components/forms/DateField/DatePickerCalendar.js
@@ -1,6 +1,6 @@
 // Calendar component documentation:
 // https://nl-design-system.github.io/utrecht/storybook-react/index.html?path=/docs/react-component-calendar--docs
-import {Calendar} from '@utrecht/component-library-react/dist/Calendar';
+import {Calendar} from '@utrecht/component-library-react';
 import {enGB, nl} from 'date-fns/locale';
 import React from 'react';
 import {FormattedMessage, useIntl} from 'react-intl';
