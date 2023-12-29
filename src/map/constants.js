46003b8017da260d2e715d3d3b1ec61062d5288b
Viicos <65306057+Viicos@users.noreply.github.com>
2023-12-29 17:32:41 +0100

    [#614] Update sort plugin to take ignore comment into account

--- src/map/constants.js
+++ src/map/constants.js
@@ -1,7 +1,7 @@
 // sort-imports-ignore
 // a hack - this library has side effects because it patches L from leaflet.
-import {CRS_RD, TILES_ATTRIBUTION, TILE_LAYER_RD} from '@open-formulieren/leaflet-tools';
 import 'proj4leaflet';
+import {CRS_RD, TILES_ATTRIBUTION, TILE_LAYER_RD} from '@open-formulieren/leaflet-tools';
 
 // Roughly the center of the Netherlands
 const DEFAULT_LAT_LNG = [52.1326332, 5.291266];
