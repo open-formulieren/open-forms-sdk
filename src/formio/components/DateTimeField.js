99d984275e27283aa460bf84670b89c1b421320e
SilviaAmAm <silvia@maykinmedia.nl>
2024-01-18 14:48:27 +0100

    :bug: [open-formulieren/open-forms#3755] Validate min/max datetime
    If the datetime is entered with the keyboard instead of the widget, no validation was being performed. This adds the validation in the same way as for the date component (see https://github.com/open-formulieren/open-forms/issues/3443)

--- src/formio/components/DateTimeField.js
+++ src/formio/components/DateTimeField.js
@@ -1,8 +1,21 @@
 import {Formio} from 'react-formio';
 
+import MinMaxDatetimeValidator from 'formio/validators/minMaxDatetimeValidator';
+
 const DateTimeFormio = Formio.Components.components.datetime;
 
 class DateTimeField extends DateTimeFormio {
+  constructor(component, options, data) {
+    super(component, options, data);
+
+    if (component.datePicker.minDate || component.datePicker.maxDate) {
+      component.validate.datetimeMinMax = true;
+    }
+
+    this.validators.push('datetimeMinMax');
+    this.validator.validators['datetimeMinMax'] = MinMaxDatetimeValidator;
+  }
+
   get inputInfo() {
     const info = super.inputInfo;
     // apply NLDS CSS classes
---
+++ src/formio/validators/utils.js
@@ -0,0 +1,39 @@
+import {parseISO} from 'date-fns';
+
+export const validateBoundaries = (componentType, minBoundary, maxBoundary, value) => {
+  const parsedMinBoundary = minBoundary ? new Date(minBoundary) : null;
+  const parsedMaxBoundary = maxBoundary ? new Date(maxBoundary) : null;
+
+  if (!parsedMinBoundary && !parsedMaxBoundary) {
+    return {isValid: true};
+  }
+
+  const parsedValue = parseISO(value, 'yyyy-MM-dd', new Date());
+
+  let errorKeyMinValue, errorKeyMaxValue;
+  if (componentType === 'date') {
+    errorKeyMinValue = 'minDate';
+    errorKeyMaxValue = 'maxDate';
+  } else if (componentType === 'datetime') {
+    errorKeyMinValue = 'minDatetime';
+    errorKeyMaxValue = 'maxDatetime';
+  }
+
+  if (parsedMinBoundary && parsedMaxBoundary) {
+    const isValid = parsedValue >= parsedMinBoundary && parsedValue <= parsedMaxBoundary;
+    let errorKeys = [];
+    if (!isValid) {
+      if (parsedValue < parsedMinBoundary) {
+        errorKeys.push(errorKeyMinValue);
+      } else {
+        errorKeys.push(errorKeyMaxValue);
+      }
+    }
+    return {isValid, errorKeys};
+  }
+
+  if (parsedMinBoundary)
+    return {isValid: parsedValue >= parsedMinBoundary, errorKeys: [errorKeyMinValue]};
+  if (parsedMaxBoundary)
+    return {isValid: parsedValue <= parsedMaxBoundary, errorKeys: [errorKeyMaxValue]};
+};
