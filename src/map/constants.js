cf2fe8766dc67911591d81277825bba7bb16e6f8
Sergei Maertens <sergei@maykinmedia.nl>
2024-01-22 14:50:06 +0100

    :art: [#614] Remove unused import/export
    The tile layer attribution is included in the TILE_LAYER_RD already.

--- src/map/constants.js
+++ src/map/constants.js
@@ -1,10 +1,10 @@
 // sort-imports-ignore
 // a hack - this library has side effects because it patches L from leaflet.
 import 'proj4leaflet';
-import {CRS_RD, TILES_ATTRIBUTION, TILE_LAYER_RD} from '@open-formulieren/leaflet-tools';
+import {CRS_RD, TILE_LAYER_RD} from '@open-formulieren/leaflet-tools';
 
 // Roughly the center of the Netherlands
 const DEFAULT_LAT_LNG = [52.1326332, 5.291266];
 const DEFAULT_ZOOM = 13;
 
-export {CRS_RD, DEFAULT_LAT_LNG, DEFAULT_ZOOM, TILES_ATTRIBUTION, TILE_LAYER_RD};
+export {CRS_RD, DEFAULT_LAT_LNG, DEFAULT_ZOOM, TILE_LAYER_RD};
