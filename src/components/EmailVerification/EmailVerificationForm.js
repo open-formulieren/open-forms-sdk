58da42df55671a753c079f7298eae517d2171d6a
Sergei Maertens <sergei@maykinmedia.nl>
2024-09-10 12:52:49 +0200

    :children_crossing: Display warning if no email address was specified
    Otherwise users try to click submit buttons that are either
    disabled or result in validation errors that can't be
    displayed properly.
    
    Better would be to disable the modal until an email address
    is entered, but that is too hard to deal with Formio.

--- src/components/EmailVerification/EmailVerificationForm.js
+++ src/components/EmailVerification/EmailVerificationForm.js
@@ -10,6 +10,7 @@ import {ConfigContext} from 'Context';
 import {post} from 'api';
 import Body from 'components/Body';
 import {DisplayError} from 'components/Errors/ErrorBoundary';
+import ErrorMessage from 'components/Errors/ErrorMessage';
 import {Toolbar, ToolbarList} from 'components/Toolbar';
 import {TextField} from 'components/forms';
 import {ValidationError} from 'errors';
@@ -101,46 +102,57 @@ const EmailVerificationForm = ({submissionUrl, componentKey, emailAddress, onVer
           </Body>
         ) : (
           <Body component="form" onSubmit={handleSubmit}>
-            <Body modifiers={['big']}>
-              <FormattedMessage
-                description="Email verification modal body text"
-                defaultMessage={`You are verifying the email address <link>{emailAddress}</link>.
-              First, we need to send you a verification code on this email address. Then,
-              you need to enter the code to confirm it.`}
-                values={{
-                  emailAddress,
-                  link: chunks => (
-                    <UtrechtLink href={`mailto:${emailAddress}`}>{chunks}</UtrechtLink>
-                  ),
-                }}
-              />
-            </Body>
-
-            <div className="openforms-form-field-container">
-              <ModeField />
-              {mode === 'enterCode' && (
-                <TextField
-                  name="code"
-                  isRequired
-                  label={
-                    <FormattedMessage
-                      description="Email verification: code input field label"
-                      defaultMessage="Enter the six-character code"
-                    />
-                  }
-                  pattern="[A-Z0-9]{6}"
-                  description={
-                    <FormattedMessage
-                      description="Email verification: code input field description"
-                      defaultMessage="The code is exactly six characters long and consists of only uppercase letters and numbers."
-                    />
-                  }
-                  onChange={event => {
-                    setFieldValue('code', event.target.value.toUpperCase());
-                  }}
+            {!emailAddress ? (
+              <ErrorMessage modifier="warning">
+                <FormattedMessage
+                  description="Email verification modal: warning that no email is specified"
+                  defaultMessage="You haven't entered an email address yet."
                 />
-              )}
-            </div>
+              </ErrorMessage>
+            ) : (
+              <>
+                <Body modifiers={['big']}>
+                  <FormattedMessage
+                    description="Email verification modal body text"
+                    defaultMessage={`You are verifying the email address <link>{emailAddress}</link>.
+                  First, we need to send you a verification code on this email address. Then,
+                  you need to enter the code to confirm it.`}
+                    values={{
+                      emailAddress,
+                      link: chunks => (
+                        <UtrechtLink href={`mailto:${emailAddress}`}>{chunks}</UtrechtLink>
+                      ),
+                    }}
+                  />
+                </Body>
+
+                <div className="openforms-form-field-container">
+                  <ModeField />
+                  {mode === 'enterCode' && (
+                    <TextField
+                      name="code"
+                      isRequired
+                      label={
+                        <FormattedMessage
+                          description="Email verification: code input field label"
+                          defaultMessage="Enter the six-character code"
+                        />
+                      }
+                      pattern="[A-Z0-9]{6}"
+                      description={
+                        <FormattedMessage
+                          description="Email verification: code input field description"
+                          defaultMessage="The code is exactly six characters long and consists of only uppercase letters and numbers."
+                        />
+                      }
+                      onChange={event => {
+                        setFieldValue('code', event.target.value.toUpperCase());
+                      }}
+                    />
+                  )}
+                </div>
+              </>
+            )}
 
             <Toolbar modifiers={['bottom', 'reverse']}>
               <ToolbarList>
