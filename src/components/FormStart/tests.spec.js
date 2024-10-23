6bae4e5f57f051661dc7f7cf76dd3610bdcc8076
Sergei Maertens <sergei@maykinmedia.nl>
2024-10-23 22:30:13 +0200

    :art: Clean up act warnings in FormStart jest tests
    These tests are now fully converted to testing-library instead of react
    testing tools.

--- src/components/FormStart/tests.spec.js
+++ src/components/FormStart/tests.spec.js
@@ -1,8 +1,6 @@
-import {render as renderTest, screen} from '@testing-library/react';
+import {render, screen, waitFor} from '@testing-library/react';
 import messagesEN from 'i18n/compiled/en.json';
 import React from 'react';
-import {createRoot} from 'react-dom/client';
-import {act} from 'react-dom/test-utils';
 import {IntlProvider} from 'react-intl';
 import {MemoryRouter} from 'react-router-dom';
 
@@ -16,92 +14,70 @@ jest.mock('hooks/useQuery');
 let scrollIntoViewMock = jest.fn();
 window.HTMLElement.prototype.scrollIntoView = scrollIntoViewMock;
 
-let container = null;
-let root = null;
-beforeEach(() => {
-  // setup a DOM element as a render target
-  container = document.createElement('div');
-  document.body.appendChild(container);
-  root = createRoot(container);
-});
-
-afterEach(() => {
-  // cleanup on exiting
-  act(() => {
-    root.unmount();
-    container.remove();
-    root = null;
-    container = null;
-  });
-});
-
 const Wrap = ({children}) => (
   <IntlProvider locale="en" messages={messagesEN}>
     <MemoryRouter>{children}</MemoryRouter>
   </IntlProvider>
 );
 
-it('Form start page start if _start parameter is present', () => {
+it('Form start page start if _start parameter is present', async () => {
   const testLocation = new URLSearchParams('?_start=1');
   useQuery.mockReturnValue(testLocation);
 
   const onFormStart = jest.fn();
   const onDestroySession = jest.fn();
 
-  act(() => {
-    root.render(
+  render(
+    <Wrap>
+      <FormStart form={testForm} onFormStart={onFormStart} onDestroySession={onDestroySession} />
+    </Wrap>
+  );
+
+  // waitFor required to not have act(...) warnings!
+  await waitFor(() => {
+    expect(onFormStart).toHaveBeenCalled();
+  });
+});
+
+it.each([
+  [
+    '_digid-message=error',
+    'Inloggen bij deze organisatie is niet gelukt. Probeert u het later nog een keer. ' +
+      'Lukt het nog steeds niet? Log in bij Mijn DigiD. Zo controleert u of uw DigiD goed ' +
+      'werkt. Mogelijk is er een storing bij de organisatie waar u inlogt.',
+  ],
+  ['_digid-message=login-cancelled', 'Je hebt het inloggen met DigiD geannuleerd.'],
+  [
+    '_eherkenning-message=error',
+    'Er is een fout opgetreden bij het inloggen met EHerkenning. Probeer het later opnieuw.',
+  ],
+  ['_eherkenning-message=login-cancelled', 'Je hebt het inloggen met EHerkenning geannuleerd.'],
+])(
+  'Form start does not start if there are auth errors / %s',
+  async (testQuery, expectedMessage) => {
+    const onFormStart = jest.fn();
+    const onDestroySession = jest.fn();
+
+    const testLocation = new URLSearchParams(`?_start=1&${testQuery}`);
+    useQuery.mockReturnValue(testLocation);
+
+    render(
       <Wrap>
         <FormStart form={testForm} onFormStart={onFormStart} onDestroySession={onDestroySession} />
       </Wrap>
     );
-  });
 
-  expect(onFormStart).toHaveBeenCalled();
-});
-
-it('Form start does not start if there are auth errors', () => {
-  const onFormStart = jest.fn();
-  const onDestroySession = jest.fn();
-
-  const testQueries = {
-    '_digid-message=error':
-      'Inloggen bij deze organisatie is niet gelukt. Probeert u ' +
-      'het later nog een keer. Lukt het nog steeds niet? Log in bij Mijn DigiD. Zo ' +
-      'controleert u of uw DigiD goed werkt. Mogelijk is er een storing bij ' +
-      'de organisatie waar u inlogt.',
-    '_digid-message=login-cancelled': 'Je hebt het inloggen met DigiD geannuleerd.',
-    '_eherkenning-message=error':
-      'Er is een fout opgetreden bij het inloggen met EHerkenning. Probeer het later opnieuw.',
-    '_eherkenning-message=login-cancelled': 'Je hebt het inloggen met EHerkenning geannuleerd.',
-  };
-
-  for (const [testQuery, expectedMessage] of Object.entries(testQueries)) {
-    const testLocation = new URLSearchParams(`?_start=1&${testQuery}`);
-    useQuery.mockReturnValue(testLocation);
-
-    act(() => {
-      root.render(
-        <Wrap>
-          <FormStart
-            form={testForm}
-            onFormStart={onFormStart}
-            onDestroySession={onDestroySession}
-          />
-        </Wrap>
-      );
-    });
-
-    expect(container.textContent).toContain(expectedMessage);
+    expect(await screen.findByText(expectedMessage)).toBeVisible();
     expect(onFormStart).toHaveBeenCalledTimes(0);
   }
-});
+);
 
-it('Form start page does not show login buttons if an active submission is present', () => {
+it('Form start page does not show login buttons if an active submission is present', async () => {
   useQuery.mockReturnValue(new URLSearchParams());
   const onFormStart = jest.fn();
   const onDestroySession = jest.fn();
 
-  renderTest(
+  render(
     <Wrap>
       <FormStart
         form={testForm}
@@ -109,20 +85,20 @@ it('Form start page does not show login buttons if an active submission is prese
         onDestroySession={onDestroySession}
         submission={buildSubmission({isAuthenticated: false})}
       />
-    </Wrap>,
-    container
+    </Wrap>
   );
 
-  expect(screen.queryByRole('button', {name: 'Continue existing submission'})).toBeInTheDocument();
+  const continueButton = await screen.findByRole('button', {name: 'Continue existing submission'});
+  expect(continueButton).toBeInTheDocument();
   expect(screen.queryByRole('button', {name: 'Abort submission'})).toBeInTheDocument();
 });
 
-it('Form start page with initial_data_reference', () => {
+it('Form start page with initial_data_reference', async () => {
   useQuery.mockReturnValue(new URLSearchParams());
   const onFormStart = jest.fn();
   const onDestroySession = jest.fn();
 
-  renderTest(
+  render(
     <Wrap>
       <FormStart
         form={testLoginForm}
@@ -130,10 +106,10 @@ it('Form start page with initial_data_reference', () => {
         onDestroySession={onDestroySession}
         initialDataReference="1234"
       />
-    </Wrap>,
-    container
+    </Wrap>
   );
-  const loginLink = screen.getByRole('link', {name: 'Login with DigiD'});
+
+  const loginLink = await screen.findByRole('link', {name: 'Login with DigiD'});
   expect(loginLink).toHaveAttribute(
     'href',
     'https://openforms.nl/auth/form-name/digid/start?initial_data_reference=1234&next=http%3A%2F%2Flocalhost%2F%3F_start%3D1'
