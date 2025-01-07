2747462f058524b9ad44c94831250b994feb9278
Sergei Maertens <sergei@maykinmedia.nl>
2025-01-07 23:41:26 +0100

    :recycle: [open-formulieren/open-forms#4929] Refactor cosign <Routes> to not require any props
    Split the state up in the components used to render each route/path,
    which allows localizing the state. For the synchronization between
    routes, React context is used.

---
+++ src/components/CoSign/CosignCheck.jsx
@@ -0,0 +1,46 @@
+import {useContext, useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+
+import {ConfigContext} from 'Context';
+import {destroy} from 'api';
+import {CosignSummary} from 'components/Summary';
+import useFormContext from 'hooks/useFormContext';
+
+import {useCosignContext} from './Context';
+
+/**
+ * Fetch the submission summary data and display it, together with the controls to
+ * confirm the submission and buttons to log out.
+ */
+const CosignCheck = () => {
+  const navigate = useNavigate();
+  const config = useContext(ConfigContext);
+  const form = useFormContext();
+  const {onCosignComplete} = useCosignContext();
+
+  const [submission, setSubmission] = useState(null);
+  const [summaryData, setSummaryData] = useState([]);
+
+  const onDestroySession = async () => {
+    await destroy(`${config.baseUrl}authentication/${submission.id}/session`);
+    setSubmission(null);
+    setSummaryData([]);
+    navigate('/');
+  };
+
+  return (
+    <CosignSummary
+      form={form}
+      submission={submission}
+      summaryData={summaryData}
+      onSubmissionLoaded={submission => setSubmission(submission)}
+      onDataLoaded={({summaryData}) => setSummaryData(summaryData)}
+      onCosignComplete={onCosignComplete}
+      onDestroySession={onDestroySession}
+    />
+  );
+};
+
+CosignCheck.propTypes = {};
+
+export default CosignCheck;
