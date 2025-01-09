09a01de8acbeb5fdf2a5c26c17bf43c44a1cabe4
Sergei Maertens <sergei@maykinmedia.nl>
2025-01-09 08:43:11 +0100

    :white_check_mark: [open-formulieren/open-forms#4929] Add test for navigating the beginning of a form
    * Navigate from the start to introduction page and first step
    * Navigate from start directly to first step when no introduction
      page is used.

--- src/components/Form.spec.jsx
+++ src/components/Form.spec.jsx
@@ -146,3 +146,51 @@ test.each([
     expect(loginLink).toHaveAttribute('href', expectedUrl);
   }
 );
+
+test('Navigation through form with introduction page', async () => {
+  const user = userEvent.setup();
+  mswServer.use(mockSubmissionPost(), mockAnalyticsToolConfigGet(), mockSubmissionStepGet());
+  const form = buildForm({
+    introductionPageContent: '<p>Introduction page content</p>',
+    loginRequired: false,
+  });
+
+  // initial render should display the introduction page
+  render(<Wrapper form={form} initialEntry="" />);
+  await screen.findByText('Introduction page content');
+
+  // navigating to the next page takes you to the start page
+  const continueLink = screen.getByRole('link', {name: 'Continue'});
+  await user.click(continueLink);
+  const startButton = await screen.findByRole('button', {name: 'Begin'});
+  expect(startButton).toBeVisible();
+
+  // clicking the start button starts the submission and navigates to step 1
+  await user.click(startButton);
+  const stepTitle = await screen.findByRole('heading', {name: 'Step 1'});
+  expect(stepTitle).toBeVisible();
+  const formInput = await screen.findByLabelText('Component 1');
+  expect(formInput).toBeVisible();
+});
+
+test('Navigation through form without introduction page', async () => {
+  const user = userEvent.setup();
+  mswServer.use(mockSubmissionPost(), mockAnalyticsToolConfigGet(), mockSubmissionStepGet());
+  const form = buildForm({
+    introductionPageContent: '',
+    loginRequired: false,
+  });
+
+  // initial render should display the introduction page
+  render(<Wrapper form={form} initialEntry="" />);
+
+  const startButton = await screen.findByRole('button', {name: 'Begin'});
+  expect(startButton).toBeVisible();
+
+  // clicking the start button starts the submission and navigates to step 1
+  await user.click(startButton);
+  const stepTitle = await screen.findByRole('heading', {name: 'Step 1'});
+  expect(stepTitle).toBeVisible();
+  const formInput = await screen.findByLabelText('Component 1');
+  expect(formInput).toBeVisible();
+});
