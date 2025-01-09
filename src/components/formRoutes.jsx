9e51db6feeff66417ffd0db6881151d30875111e
Sergei Maertens <sergei@maykinmedia.nl>
2025-01-09 17:48:30 +0100

    :recycle: [open-formulieren/open-forms#4929] Refactor IntroductionPage component to require no props
    Instead of passing the extra parameters, we extract the initial data
    reference using the hook from the LandingPage component refactor.

--- src/components/formRoutes.jsx
+++ src/components/formRoutes.jsx
@@ -1,9 +1,14 @@
 import FormLandingPage from 'components/FormLandingPage';
+import IntroductionPage from 'components/IntroductionPage';
 
 const formRoutes = [
   {
     path: '',
     element: <FormLandingPage />,
+  },
+  {
+    path: 'introductie',
+    element: <IntroductionPage />,
   },
 ];
 
