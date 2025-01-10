cde10d2f35aecf667a0c64b8cf9052b9419cf1cc
Sergei Maertens <sergei@maykinmedia.nl>
2025-01-10 14:10:15 +0100

    :white_check_mark: [open-formulieren/open-forms#4929] Update form start tests
    Updated the existing tests for the new component structure and
    injection of dependencies, and some tests were moved from Form
    to FormStart tests because the behaviour being tested was also moved.
    
    Less mocks are now used and the tests should generally be a bit more
    robust.

--- src/components/FormStart/tests.spec.jsx
+++ src/components/FormStart/tests.spec.jsx
@@ -1,58 +1,216 @@
 import {render, screen, waitFor} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import messagesEN from 'i18n/compiled/en.json';
+import {useState} from 'react';
 import {IntlProvider} from 'react-intl';
 import {RouterProvider, createMemoryRouter} from 'react-router-dom';
 
-import {FormContext} from 'Context';
-import {buildForm, buildSubmission} from 'api-mocks';
+import {ConfigContext, FormContext} from 'Context';
+import {BASE_URL, buildForm, buildSubmission} from 'api-mocks';
+import mswServer from 'api-mocks/msw-server';
+import {mockSubmissionPost} from 'api-mocks/submissions';
+import {SubmissionProvider} from 'components/Form';
 
 import FormStart from './index';
 
 let scrollIntoViewMock = vi.fn();
 window.HTMLElement.prototype.scrollIntoView = scrollIntoViewMock;
 
-beforeEach(() => {
-  localStorage.clear();
-});
-
-afterEach(() => {
-  localStorage.clear();
-});
-
 afterAll(() => {
   vi.clearAllMocks();
 });
 
-const Wrap = ({form = buildForm(), children, currentUrl = '/startpagina'}) => {
+const Wrap = ({
+  form = buildForm(),
+  currentUrl = '/startpagina',
+  initialSubmission = null,
+  onSubmissionObtained = undefined,
+}) => {
   const parsedUrl = new URL(currentUrl, 'http://dummy');
-  const routes = [{path: parsedUrl.pathname, element: <>{children}</>}];
+  const routes = [
+    {path: parsedUrl.pathname, element: <FormStart />},
+    {path: '/stap/:slug', element: <h1>Step page</h1>},
+  ];
   const router = createMemoryRouter(routes, {initialEntries: [currentUrl]});
+  const [submission, setSubmission] = useState(initialSubmission);
   return (
-    <IntlProvider locale="en" messages={messagesEN}>
-      <FormContext.Provider value={form}>
-        <RouterProvider router={router} />
-      </FormContext.Provider>
-    </IntlProvider>
+    <ConfigContext.Provider
+      value={{
+        baseUrl: BASE_URL,
+        clientBaseUrl: 'http://localhost/',
+        basePath: '',
+        baseTitle: '',
+        requiredFieldsWithAsterisk: true,
+      }}
+    >
+      <IntlProvider locale="en" messages={messagesEN}>
+        <FormContext.Provider value={form}>
+          <SubmissionProvider
+            submission={submission}
+            onSubmissionObtained={submission => {
+              setSubmission(submission);
+              onSubmissionObtained?.();
+            }}
+            onDestroySession={() => {}}
+          >
+            <RouterProvider router={router} />
+          </SubmissionProvider>
+        </FormContext.Provider>
+      </IntlProvider>
+    </ConfigContext.Provider>
   );
 };
 
-it('Form start page start if _start parameter is present', async () => {
-  const onFormStart = vi.fn();
-  const onDestroySession = vi.fn();
-
-  render(
-    <Wrap currentUrl="/startpagina?_start=1">
-      <FormStart onFormStart={onFormStart} onDestroySession={onDestroySession} />
-    </Wrap>
-  );
-
-  // waitFor required to not have act(...) warnings!
-  await waitFor(() => {
-    expect(onFormStart).toHaveBeenCalled();
+test('Start form without having logged in', async () => {
+  const user = userEvent.setup();
+  mswServer.use(mockSubmissionPost());
+  let startSubmissionRequest;
+  mswServer.events.on('request:match', async ({request}) => {
+    const url = new URL(request.url);
+    if (request.method === 'POST' && url.pathname.endsWith('/api/v2/submissions')) {
+      startSubmissionRequest = request;
+    }
   });
+  // form with only anonymous login option
+  const form = buildForm({loginOptions: [], loginRequired: false});
+
+  render(<Wrap form={form} />);
+
+  const startButton = screen.getByRole('button', {name: 'Begin'});
+  await user.click(startButton);
+
+  expect(startSubmissionRequest).not.toBeUndefined();
+  const requestBody = await startSubmissionRequest.json();
+  expect(requestBody.anonymous).toBe(true);
 });
 
-it.each([
+test('Start form with having logged in', async () => {
+  const form = buildForm({
+    loginOptions: [
+      {
+        identifier: 'digid',
+        label: 'DigiD',
+        url: 'https://openforms.nl/auth/form-name/digid/start',
+        logo: {
+          title: 'DigiD',
+          imageSrc: 'https://openforms.nl/static/img/digid-46x46.71ea68346bbb.png',
+          href: 'https://www.digid.nl/',
+          appearance: 'dark',
+        },
+        isForGemachtigde: false,
+      },
+    ],
+    loginRequired: true,
+  });
+  mswServer.use(mockSubmissionPost());
+  let startSubmissionRequest;
+  mswServer.events.on('request:match', async ({request}) => {
+    const url = new URL(request.url);
+    if (request.method === 'POST' && url.pathname.endsWith('/api/v2/submissions')) {
+      startSubmissionRequest = request;
+    }
+  });
+
+  // we simulate the redirect flow by the backend
+  render(<Wrap form={form} />);
+  const digidLink = await screen.findByRole('link', {name: 'Login with DigiD'});
+  const parsedDigidLink = new URL(digidLink.getAttribute('href'));
+  const nextUrl = new URL(parsedDigidLink.searchParams.get('next'));
+  expect(nextUrl).not.toBeNull();
+  render(<Wrap form={form} currentUrl={`${nextUrl.pathname}${nextUrl.search}`} />);
+
+  await waitFor(() => {
+    expect(startSubmissionRequest).not.toBeUndefined();
+  });
+  const requestBody = await startSubmissionRequest.json();
+  expect(requestBody.anonymous).toBe(false);
+});
+
+test('Start form with object reference query param', async () => {
+  const form = buildForm({
+    loginOptions: [
+      {
+        identifier: 'digid',
+        label: 'DigiD',
+        url: 'https://openforms.nl/auth/form-name/digid/start',
+        logo: {
+          title: 'DigiD',
+          imageSrc: 'https://openforms.nl/static/img/digid-46x46.71ea68346bbb.png',
+          href: 'https://www.digid.nl/',
+          appearance: 'dark',
+        },
+        isForGemachtigde: false,
+      },
+    ],
+    loginRequired: true,
+  });
+  mswServer.use(mockSubmissionPost());
+  let startSubmissionRequest;
+  mswServer.events.on('request:match', async ({request}) => {
+    const url = new URL(request.url);
+    if (request.method === 'POST' && url.pathname.endsWith('/api/v2/submissions')) {
+      startSubmissionRequest = request;
+    }
+  });
+
+  // we simulate the redirect flow by the backend
+  render(<Wrap form={form} currentUrl="/startpagina?initial_data_reference=foo" />);
+  const digidLink = await screen.findByRole('link', {name: 'Login with DigiD'});
+  const parsedDigidLink = new URL(digidLink.getAttribute('href'));
+  const nextUrl = new URL(parsedDigidLink.searchParams.get('next'));
+  expect(nextUrl).not.toBeNull();
+  render(<Wrap form={form} currentUrl={`${nextUrl.pathname}${nextUrl.search}`} />);
+
+  await waitFor(() => {
+    expect(startSubmissionRequest).not.toBeUndefined();
+  });
+  const requestBody = await startSubmissionRequest.json();
+  expect(requestBody.initialDataReference).toBe('foo');
+});
+
+test('Start form without object reference query param', async () => {
+  const form = buildForm({
+    loginOptions: [
+      {
+        identifier: 'digid',
+        label: 'DigiD',
+        url: 'https://openforms.nl/auth/form-name/digid/start',
+        logo: {
+          title: 'DigiD',
+          imageSrc: 'https://openforms.nl/static/img/digid-46x46.71ea68346bbb.png',
+          href: 'https://www.digid.nl/',
+          appearance: 'dark',
+        },
+        isForGemachtigde: false,
+      },
+    ],
+    loginRequired: true,
+  });
+  mswServer.use(mockSubmissionPost());
+  let startSubmissionRequest;
+  mswServer.events.on('request:match', async ({request}) => {
+    const url = new URL(request.url);
+    if (request.method === 'POST' && url.pathname.endsWith('/api/v2/submissions')) {
+      startSubmissionRequest = request;
+    }
+  });
+
+  // we simulate the redirect flow by the backend
+  render(<Wrap form={form} currentUrl="/startpagina" />);
+  const digidLink = await screen.findByRole('link', {name: 'Login with DigiD'});
+  const parsedDigidLink = new URL(digidLink.getAttribute('href'));
+  const nextUrl = new URL(parsedDigidLink.searchParams.get('next'));
+  expect(nextUrl).not.toBeNull();
+  render(<Wrap form={form} currentUrl={`${nextUrl.pathname}${nextUrl.search}`} />);
+
+  await waitFor(() => {
+    expect(startSubmissionRequest).not.toBeUndefined();
+  });
+  const requestBody = await startSubmissionRequest.json();
+  expect(requestBody.initialDataReference).toBeUndefined();
+});
+
+test.each([
   [
     '_digid-message=error',
     'Inloggen bij deze organisatie is niet gelukt. Probeert u het later nog een keer. ' +
@@ -68,111 +226,31 @@ it.each([
 ])(
   'Form start does not start if there are auth errors / %s',
   async (testQuery, expectedMessage) => {
-    const onFormStart = vi.fn();
-    const onDestroySession = vi.fn();
+    let requestsMade = false;
+    mswServer.events.on('request:start', async () => {
+      requestsMade = true;
+    });
+    const onSubmissionObtained = vi.fn();
 
-    const url = `/startpagina?_start=1&${testQuery}`;
     render(
-      <Wrap currentUrl={url}>
-        <FormStart onFormStart={onFormStart} onDestroySession={onDestroySession} />
-      </Wrap>
+      <Wrap
+        currentUrl={`/startpagina?_start=1&${testQuery}`}
+        onSubmissionObtained={onSubmissionObtained}
+      />
     );
 
     expect(await screen.findByText(expectedMessage)).toBeVisible();
-    expect(onFormStart).not.toHaveBeenCalled();
+    expect(requestsMade).toBe(false);
+    expect(onSubmissionObtained).not.toHaveBeenCalled();
   }
 );
 
-it('Form start page does not show login buttons if an active submission is present', async () => {
-  const onFormStart = vi.fn();
-  const onDestroySession = vi.fn();
+test('Form start page does not show login buttons if an active submission is present', async () => {
+  const submission = buildSubmission({isAuthenticated: false});
 
-  render(
-    <Wrap>
-      <FormStart
-        onFormStart={onFormStart}
-        onDestroySession={onDestroySession}
-        submission={buildSubmission({isAuthenticated: false})}
-      />
-    </Wrap>
-  );
+  render(<Wrap initialSubmission={submission} />);
 
   const continueButton = await screen.findByRole('button', {name: 'Continue existing submission'});
   expect(continueButton).toBeInTheDocument();
-  expect(screen.queryByRole('button', {name: 'Cancel submission'})).toBeInTheDocument();
-});
-
-it('Form start page with initial_data_reference', async () => {
-  const onFormStart = vi.fn();
-  const onDestroySession = vi.fn();
-  const form = buildForm({
-    loginOptions: [
-      {
-        identifier: 'digid',
-        label: 'DigiD',
-        url: 'https://openforms.nl/auth/form-name/digid/start',
-        logo: {
-          title: 'DigiD',
-          imageSrc: 'https://openforms.nl/static/img/digid-46x46.71ea68346bbb.png',
-          href: 'https://www.digid.nl/',
-          appearance: 'dark',
-        },
-        isForGemachtigde: false,
-      },
-    ],
-  });
-
-  render(
-    <Wrap form={form} currentUrl="/startpagina?initial_data_reference=1234">
-      <FormStart onFormStart={onFormStart} onDestroySession={onDestroySession} />
-    </Wrap>
-  );
-
-  const loginLink = await screen.findByRole('link', {name: 'Login with DigiD'});
-  const href = loginLink.getAttribute('href');
-  expect(href).toBeTruthy();
-
-  const nextParam = new URL(href).searchParams.get('next');
-  expect(nextParam).toBeTruthy();
-
-  const next = new URL(nextParam);
-  expect(next.searchParams.get('initial_data_reference')).toBe('1234');
-});
-
-it('Form start page without initial_data_reference', async () => {
-  const onFormStart = vi.fn();
-  const onDestroySession = vi.fn();
-  const form = buildForm({
-    loginOptions: [
-      {
-        identifier: 'digid',
-        label: 'DigiD',
-        url: 'https://openforms.nl/auth/form-name/digid/start',
-        logo: {
-          title: 'DigiD',
-          imageSrc: 'https://openforms.nl/static/img/digid-46x46.71ea68346bbb.png',
-          href: 'https://www.digid.nl/',
-          appearance: 'dark',
-        },
-        isForGemachtigde: false,
-      },
-    ],
-  });
-
-  render(
-    <Wrap form={form} currentUrl="/startpagina?initial_data_reference=">
-      <FormStart onFormStart={onFormStart} onDestroySession={onDestroySession} />
-    </Wrap>
-  );
-
-  const loginLink = await screen.findByRole('link', {name: 'Login with DigiD'});
-  const href = loginLink.getAttribute('href');
-  expect(href).toBeTruthy();
-
-  const nextParam = new URL(href).searchParams.get('next');
-  expect(nextParam).toBeTruthy();
-
-  const next = new URL(nextParam);
-  expect(nextParam).not.toContain('initial_data_reference');
-  expect(next.searchParams.get('initial_data_reference')).toBeNull();
+  expect(screen.getByRole('button', {name: 'Cancel submission'})).toBeInTheDocument();
 });
