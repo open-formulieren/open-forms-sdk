69db15448c46df9aca7e919626bd7f31e64d42eb
Sergei Maertens <sergei@maykinmedia.nl>
2025-01-22 21:56:04 +0100

    :wrench: [open-formulieren/open-forms#4929] Enable remaining flags
    These are for features that we don't make use of yet,
    following the docs to just enable them and rely on our
    test suites.

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,6 +2,10 @@ export {default} from './app';
 
 export const FUTURE_FLAGS = {
   v7_relativeSplatPath: true,
+  v7_fetcherPersist: true,
+  v7_normalizeFormMethod: true,
+  v7_partialHydration: true,
+  v7_skipActionErrorRevalidation: true,
 };
 
 export const PROVIDER_FUTURE_FLAGS = {
