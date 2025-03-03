35e6e4326bfdb72aa885616e366f094717347143
Sergei Maertens <sergei@maykinmedia.nl>
2025-03-03 21:55:07 +0100

    :label: [#445] Convert the Body component and stories to TS
    Turns out we need the babel presets after all for the storybook
    processing :( I'm not sure why things work without in the renderer
    repo, but we'll figure it out at some point.
    
    This strongly types the Body component with its 'component' prop,
    allowing strings of intrinsic elements to be passed. Support for
    custom react components is dropped, as it doesn't appear to be
    used anywhere so this simplifies things a bit.

--- src/components/Body.stories.tsx
+++ src/components/Body.stories.tsx
@@ -1,8 +1,14 @@
+import type {Meta, StoryObj} from '@storybook/react';
+
 import Body, {VARIANTS} from './Body';
 
 export default {
   title: 'Pure React components / Body',
   component: Body,
+  args: {
+    children: 'Body',
+    modifiers: [],
+  },
   argTypes: {
     children: {table: {disable: true}},
     component: {control: {disable: true}},
@@ -16,35 +22,27 @@ export default {
   parameters: {
     controls: {sort: 'requiredFirst'},
   },
-};
+} satisfies Meta<typeof Body>;
 
-const render = ({content = 'Body', modifiers = []}) => {
-  const isWysiwyg = modifiers.includes('wysiwyg');
-  const extra = isWysiwyg
-    ? {
-        dangerouslySetInnerHTML: {__html: content},
-        component: 'div',
-      }
-    : {};
-  return (
-    <Body modifiers={modifiers} {...extra}>
-      {isWysiwyg ? null : content}
-    </Body>
-  );
-};
+type Story = StoryObj<typeof Body>;
 
-export const Default = {
-  render,
+export const Default: Story = {
   args: {
-    content: 'Lorem ipsum...',
+    children: 'Lorem ipsum...',
     modifiers: [],
   },
 };
 
-export const WYSIWYG = {
-  render,
+export const WYSIWYG: Story = {
+  render: ({children, modifiers}) => (
+    <Body
+      modifiers={modifiers}
+      component="div"
+      dangerouslySetInnerHTML={{__html: children as string}}
+    />
+  ),
   args: {
-    content:
+    children:
       '<p>Lorem ipsum with a <a href="https://example.com" target="_blank" rel="noopener nofollower">clickable</a> link...<p>',
     modifiers: ['wysiwyg'],
   },
