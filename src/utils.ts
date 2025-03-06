ba9b58c1463ae86a0b2463ac00c142f2a72bbd62
Sergei Maertens <sergei@maykinmedia.nl>
2025-03-06 21:40:05 +0100

    :label: [#445] Add some types to utilities

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import type {IntlShape} from 'react-intl';
 
 import {getEnv} from './env';
 
@@ -8,12 +9,12 @@ const VERSION = getEnv('VERSION');
 
 export const PREFIX = 'openforms';
 
-export const getFormattedDateString = (intl, dateString) => {
+export const getFormattedDateString = (intl: IntlShape, dateString: string): string => {
   if (!dateString) return '';
   return intl.formatDate(new Date(dateString));
 };
 
-export const getFormattedTimeString = (intl, dateTimeString) => {
+export const getFormattedTimeString = (intl: IntlShape, dateTimeString: string): string => {
   if (!dateTimeString) return '';
   return intl.formatTime(new Date(dateTimeString));
 };
@@ -21,17 +22,17 @@ export const getFormattedTimeString = (intl, dateTimeString) => {
 /**
  * Prefix a name/string/identifier with the Open Forms specific prefix.
  */
-export const applyPrefix = name => {
+export const applyPrefix = (name: string): string => {
   return `${PREFIX}-${name}`;
 };
 
-export const getBEMClassName = (base, modifiers = []) => {
+export const getBEMClassName = (base: string, modifiers: string[] = []): string => {
   const prefixedBase = applyPrefix(base);
   const prefixedModifiers = modifiers.map(mod => applyPrefix(`${base}--${mod}`));
   return clsx(prefixedBase, ...prefixedModifiers);
 };
 
 // usage: await sleep(3000);
-export const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+export const sleep = (ms: number): Promise<void> => new Promise(resolve => setTimeout(resolve, ms));
 
-export const getVersion = () => VERSION || 'unknown';
+export const getVersion = (): string => VERSION || 'unknown';
