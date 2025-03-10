7dc4a84381a32eedd3321442c1ae3f2f92bb1fbf
Sergei Maertens <sergei@maykinmedia.nl>
2025-03-10 14:40:17 +0100

    :recycle: [#445] Simplify AppDebug implementation
    Dropped the value prop in favour of always passing children.

--- src/components/AppDebug.tsx
+++ src/components/AppDebug.tsx
@@ -6,14 +6,13 @@ import {getVersion} from '@/utils';
 
 export interface DebugInfoProps {
   label: string;
-  value?: string;
-  children?: React.ReactNode;
+  children: React.ReactNode;
 }
 
-const DebugInfo: React.FC<DebugInfoProps> = ({label, value, children}) => (
+const DebugInfo: React.FC<DebugInfoProps> = ({label, children}) => (
   <div className="debug-info">
     <div className="debug-info__label">{label}</div>
-    <div className="debug-info__value">{value ?? children}</div>
+    <div className="debug-info__value">{children}</div>
   </div>
 );
 
@@ -22,7 +21,7 @@ const AppDebug: React.FC = () => {
   const [{expiry}] = useGlobalState(sessionExpiresAt);
   return (
     <div className="debug-info-container" title="Debug information (only available in dev)">
-      <DebugInfo label="Current locale" value={locale} />
+      <DebugInfo label="Current locale">{locale}</DebugInfo>
       <DebugInfo label="Session expires at">
         {expiry ? (
           <>
