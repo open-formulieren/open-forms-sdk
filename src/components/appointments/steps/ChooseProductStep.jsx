452e3077f863fa9c9bff289804ed1e5f2d6b80bf
Sergei Maertens <sergei@maykinmedia.nl>
2025-03-18 21:30:26 +0100

    :art: [open-formulieren/formio-renderer#36] Replace own edit grid in SDK with library version
    This significantly cleans up the appointments product step code.

--- src/components/appointments/steps/ChooseProductStep.jsx
+++ src/components/appointments/steps/ChooseProductStep.jsx
@@ -1,4 +1,5 @@
-import {FieldArray, Form, Formik} from 'formik';
+import {EditGrid} from '@open-formulieren/formio-renderer';
+import {Form, Formik} from 'formik';
 import {produce} from 'immer';
 import PropTypes from 'prop-types';
 import {useContext} from 'react';
@@ -8,10 +9,7 @@ import {useNavigate, useSearchParams} from 'react-router';
 import {z} from 'zod';
 import {toFormikValidationSchema} from 'zod-formik-adapter';
 
-import {OFButton} from 'components/Button';
 import {CardTitle} from 'components/Card';
-import {EditGrid, EditGridButtonGroup, EditGridItem} from 'components/EditGrid';
-import FAIcon from 'components/FAIcon';
 import useTitle from 'hooks/useTitle';
 
 import {AppointmentConfigContext} from '../Context';
@@ -31,69 +29,38 @@ const productSchema = z
 const chooseSingleProductSchema = z.object({products: productSchema.max(1)});
 const chooseMultiProductSchema = z.object({products: productSchema});
 
-const ChooseProductStepFields = ({values: {products = []}, validateForm}) => {
+const ChooseProductStepFields = ({values: {products = []}}) => {
   const intl = useIntl();
   const {supportsMultipleProducts} = useContext(AppointmentConfigContext);
   const selectedProductIds = products.map(p => p.productId).filter(Boolean);
-
-  /**
-   * Decorate an arrayHelper callback to invoke the validate function.
-   *
-   * Formik supports the `validateOnChange` prop op `FieldArray`, _but that only
-   * works if `validateOnChange` is enabled on the parent `Formik`_, which is not the
-   * case here.
-   *
-   * So, we wrap the helper and invoke the validate function ourselves, and we have to
-   * do this in a particular way so that the validate runs against the new state rather
-   * than the encapsulated state in the callback or stale state due to React 18's
-   * batching - we do this by flushing the array helper synchronously.
-   */
-  const withValidate = callback => () => {
-    flushSync(callback);
-    validateForm();
-  };
-
   const numProducts = Math.max(products.length, 1);
   return (
-    // TODO: don't do inline style
-    <Form style={{width: '100%'}}>
-      <FieldArray name="products">
-        {arrayHelpers => (
-          <EditGrid
-            addButtonLabel={
-              <>
-                <FAIcon icon="plus" />{' '}
-                <FormattedMessage
-                  description="Appointments: add additional product/service button text"
-                  defaultMessage="Add another product"
-                />
-              </>
-            }
-            onAddItem={
-              supportsMultipleProducts
-                ? withValidate(() => arrayHelpers.push({productId: '', amount: 1}))
-                : undefined
-            }
-          >
-            {products.map(({productId}, index) => (
-              // blank blocks don't have a product selected yet -> so the index is added
-              // to make the key guaranteed unique
-              <ProductWrapper
-                key={`${productId}-${index}`}
-                index={index}
-                numProducts={numProducts}
-                onRemove={withValidate(() => arrayHelpers.remove(index))}
-              >
-                <Product
-                  namePrefix="products"
-                  index={index}
-                  selectedProductIds={selectedProductIds}
-                />
-              </ProductWrapper>
-            ))}
-          </EditGrid>
-        )}
-      </FieldArray>
+    <Form>
+      {supportsMultipleProducts ? (
+        <EditGrid
+          name="products"
+          emptyItem={{productId: '', amount: 1}}
+          addButtonLabel={
+            <FormattedMessage
+              description="Appointments: add additional product/service button text"
+              defaultMessage="Add another product"
+            />
+          }
+          getItemHeading={(_, index) => (
+            <FormattedMessage
+              description="Appointments: single product label/header"
+              defaultMessage="Product {number}/{total}"
+              values={{number: index + 1, total: numProducts}}
+            />
+          )}
+          getItemBody={(_, index) => (
+            <Product namePrefix="products" index={index} selectedProductIds={selectedProductIds} />
+          )}
+          canRemoveItem={() => numProducts > 1}
+        />
+      ) : (
+        <Product namePrefix="products" index={0} selectedProductIds={selectedProductIds} />
+      )}
 
       <SubmitRow
         canSubmit
@@ -108,42 +75,6 @@ const ChooseProductStepFields = ({values: {products = []}, validateForm}) => {
 
 ChooseProductStepFields.propTypes = {
   values: PropTypes.object.isRequired,
-  validateForm: PropTypes.func.isRequired,
-  isValid: PropTypes.bool.isRequired,
-  dirty: PropTypes.bool.isRequired,
-};
-
-const ProductWrapper = ({index, numProducts, onRemove, children}) => {
-  const {supportsMultipleProducts} = useContext(AppointmentConfigContext);
-  if (!supportsMultipleProducts) {
-    return <>{children}</>;
-  }
-
-  const buttonRow = numProducts > 1 && (
-    <EditGridButtonGroup>
-      <OFButton appearance="primary-action-button" hint="danger" onClick={onRemove}>
-        <FormattedMessage
-          description="Appointments: remove product/service button text"
-          defaultMessage="Remove"
-        />
-      </OFButton>
-    </EditGridButtonGroup>
-  );
-
-  return (
-    <EditGridItem
-      heading={
-        <FormattedMessage
-          description="Appointments: single product label/header"
-          defaultMessage="Product {number}/{total}"
-          values={{number: index + 1, total: numProducts}}
-        />
-      }
-      buttons={buttonRow}
-    >
-      {children}
-    </EditGridItem>
-  );
 };
 
 const INITIAL_VALUES = {
