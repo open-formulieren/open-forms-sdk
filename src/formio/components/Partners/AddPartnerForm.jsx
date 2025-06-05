959d40daf8cd1f1ec544431e9667474ded96d8d7
Sergei Maertens <sergei@maykinmedia.nl>
2025-06-05 12:16:36 +0200

    :alembic: [#824] Use new renderer?

--- src/formio/components/Partners/AddPartnerForm.jsx
+++ src/formio/components/Partners/AddPartnerForm.jsx
@@ -1,24 +1,23 @@
-import {TextField} from '@open-formulieren/formio-renderer';
+import {FormioForm} from '@open-formulieren/formio-renderer';
 import {ButtonGroup} from '@utrecht/button-group-react';
-import {subDays, subYears} from 'date-fns';
-import {Formik} from 'formik';
 import PropTypes from 'prop-types';
-import {FormattedMessage, useIntl} from 'react-intl';
-import {toFormikValidationSchema} from 'zod-formik-adapter';
-
-import Body from 'components/Body';
-import ErrorMessage from 'components/Errors/ErrorMessage';
-import {DateField} from 'components/forms';
+import {useMemo} from 'react';
+import {useIntl} from 'react-intl';
 
 import EnterPartnerButton from './EnterPartnerButton';
-import getValidationSchema from './validationSchema';
+import PARTNER_COMPONENTS from './definition';
 
 const AddPartnerForm = ({partner, onSave, closeModal}) => {
   const intl = useIntl();
 
-  const today = new Date();
-  const maxDateOfBirth = subDays(today, 1);
-  const minDateOfBirth = subYears(today, 120);
+  const components = useMemo(
+    () =>
+      PARTNER_COMPONENTS.map(component => ({
+        ...component,
+        label: intl.formatMessage(component.label),
+      })),
+    [intl]
+  );
 
   const onSubmit = async values => {
     if (onSave) {
@@ -28,8 +27,9 @@ const AddPartnerForm = ({partner, onSave, closeModal}) => {
   };
 
   return (
-    <Formik
-      initialValues={
+    <FormioForm
+      components={components}
+      values={
         partner ?? {
           bsn: '',
           initials: '',
@@ -38,92 +38,14 @@ const AddPartnerForm = ({partner, onSave, closeModal}) => {
           dateOfBirth: '',
         }
       }
-      validationSchema={toFormikValidationSchema(getValidationSchema(intl))}
       onSubmit={onSubmit}
+      // TODO: grab from context
+      requiredFieldsWithAsterisk
     >
-      {({handleSubmit, values, setFieldValue}) => (
-        <Body component="form" onSubmit={handleSubmit}>
-          {!values ? (
-            <ErrorMessage level="warning">
-              <FormattedMessage
-                description="Add partner modal: warning that no data is added"
-                defaultMessage="You haven't entered any data yet."
-              />
-            </ErrorMessage>
-          ) : (
-            <div className="openforms-form-field-container">
-              <TextField
-                name="bsn"
-                isRequired
-                label={
-                  <FormattedMessage description="Label for partners BSN" defaultMessage="BSN" />
-                }
-                onChange={event => {
-                  setFieldValue('bsn', event.target.value);
-                }}
-              />
-              <TextField
-                name="initials"
-                label={
-                  <FormattedMessage
-                    description="Label for partners initials"
-                    defaultMessage="Initials"
-                  />
-                }
-                onChange={event => {
-                  setFieldValue('initials', event.target.value);
-                }}
-              />
-              <TextField
-                name="affixes"
-                label={
-                  <FormattedMessage
-                    description="Label for partners affixes"
-                    defaultMessage="Affixes"
-                  />
-                }
-                onChange={event => {
-                  setFieldValue('affixes', event.target.value);
-                }}
-              />
-              <TextField
-                name="lastName"
-                isRequired
-                label={
-                  <FormattedMessage
-                    description="Label for partners lastName"
-                    defaultMessage="Lastname"
-                  />
-                }
-                onChange={event => {
-                  setFieldValue('lastName', event.target.value);
-                }}
-              />
-              <DateField
-                name="dateOfBirth"
-                widget="datepicker"
-                isRequired
-                label={
-                  <FormattedMessage
-                    description="Label for partners dateOfBirth"
-                    defaultMessage="Date of birth"
-                  />
-                }
-                minDate={minDateOfBirth}
-                maxDate={maxDateOfBirth}
-                onChange={event => {
-                  setFieldValue('dateOfBirth', event.target.value);
-                }}
-              />
-            </div>
-          )}
-
-          <ButtonGroup direction="column" className="openforms-form-navigation">
-            <EnterPartnerButton />
-          </ButtonGroup>
-        </Body>
-      )}
-    </Formik>
+      <ButtonGroup direction="column" className="openforms-form-navigation">
+        <EnterPartnerButton />
+      </ButtonGroup>
+    </FormioForm>
   );
 };
 
