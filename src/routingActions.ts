12b7a68dcedc9e6d0491a620e003784e04359db7
Sergei Maertens <sergei@maykinmedia.nl>
2025-06-27 17:44:16 +0200

    :label: [#445] Convert routingActions to typescript
    The type definitions now match what's defined in the backend and
    create some more documentation. The file is also moved to the
    root of the source code, since it is not a component.

---
+++ src/routingActions.ts
@@ -0,0 +1,91 @@
+interface CosignInit {
+  action: 'cosign-init';
+  params: {
+    code: string;
+  };
+}
+
+interface Cosign {
+  action: 'cosign';
+  params: {
+    submission_uuid: string;
+  };
+}
+
+interface CancelAppointment {
+  action: 'afspraak-annuleren';
+  params: {
+    submission_uuid: string;
+    time: string;
+  };
+}
+
+interface CreateAppointment {
+  action: 'afspraak-maken';
+  params: null;
+}
+
+interface Resume {
+  action: 'resume';
+  params: {
+    submission_uuid: string;
+    step_slug: string;
+  };
+}
+
+interface Payment {
+  action: 'payment';
+  params: {
+    of_payment_status: string;
+    of_payment_id: string;
+    of_payment_action: string;
+    of_submission_status: string;
+  };
+}
+
+type Action = CosignInit | Cosign | CancelAppointment | CreateAppointment | Resume | Payment;
+
+interface RedirectParams {
+  path: string;
+  query?: URLSearchParams;
+}
+
+/**
+ * Get the correct redirect path for an action.
+ */
+export const getRedirectParams = ({action, params}: Action): RedirectParams => {
+  switch (action) {
+    case 'cosign-init': {
+      return {
+        path: 'cosign/start',
+        query: new URLSearchParams(params),
+      };
+    }
+    case 'cosign':
+      return {
+        path: 'cosign/check',
+        query: new URLSearchParams(params),
+      };
+    case 'afspraak-annuleren':
+      return {
+        path: 'afspraak-annuleren',
+        query: new URLSearchParams(params),
+      };
+    case 'afspraak-maken':
+      return {path: 'afspraak-maken'};
+    case 'resume':
+      return {
+        path: `stap/${params.step_slug}`,
+        query: new URLSearchParams({submission_uuid: params.submission_uuid}),
+      };
+    case 'payment':
+      return {
+        path: 'betalen',
+        query: new URLSearchParams(params),
+      };
+    default: {
+      const exhaustiveCheck: never = action;
+      throw new Error(`Action ${exhaustiveCheck} is not handled.`);
+    }
+  }
+};
