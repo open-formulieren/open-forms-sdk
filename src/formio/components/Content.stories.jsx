afbc696d90cc2e5fea464be1c7af711b459627b8
vasileios <zigras00@gmail.com>
2025-07-21 11:28:37 +0200

    Added some extra elements to the Content component (Headings)

--- src/formio/components/Content.stories.jsx
+++ src/formio/components/Content.stories.jsx
@@ -41,9 +41,27 @@ export const Content = {
     key: 'content',
     label: '(not rendered)',
     customClass: null,
-    html: `<p>Lorem ipsum <a href="#">dolor</a> sit amet, consectetur adipiscing elit.
-    Quisque a felis ante. Nunc dictum, dui et scelerisque euismod, ex dui sodales magna,
-    quis vehicula nulla justo sed urna. Integer maximus tempus tellus vel commodo.
-    Orci varius natoque penatibus et magnis dis parturient montes.</p>`,
+    html: `<article>
+        <h1>Heading 1</h1>
+        <h2>Heading 2</h2>
+        <h3>Heading 3</h3>
+        <h4>Heading 4</h4>
+        <h5>Heading 5</h5>
+        <h6>Heading 6</h6>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed 
+        do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+        sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p><strong>Paragraph</strong>: Lorem ipsum dolor sit amet, <em>consectetur 
+        adipiscing elit</em>, sed do eiusmod tempor incididunt ut labore et dolore magna 
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+        ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+        anim id <a href="https://example.com" target="_blank" rel="external"> 
+        est laborum</a>.</p>
+        `,
   },
 };
