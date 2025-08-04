de66732bf69ec76ba9e8b11aa7080bb191c9f1fc
Sergei Maertens <sergei@maykinmedia.nl>
2025-08-04 22:59:27 +0200

    :white_check_mark: [#445] Add story for cosign summary view
    There's no test coverage for this and I'm about to refactor the
    implementation a bit, so let's make sure we have some guard rails in
    place first.

---
+++ src/components/Summary/CosignSummary.stories.ts
@@ -0,0 +1,45 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import {fn} from '@storybook/test';
+import {withRouter} from 'storybook-addon-remix-react-router';
+
+import {buildForm} from '@/api-mocks';
+import {
+  buildSubmission,
+  mockSubmissionGet,
+  mockSubmissionSummaryGet,
+} from '@/api-mocks/submissions';
+import {ConfigDecorator} from '@/story-utils/decorators';
+
+import CosignSummary from './CosignSummary';
+
+const submission = buildSubmission();
+
+export default {
+  title: 'Views / Cosign / Summary',
+  decorators: [withRouter, ConfigDecorator],
+  component: CosignSummary,
+  args: {
+    form: buildForm(),
+    submission,
+    summaryData: [],
+    onSubmissionLoaded: fn(),
+    onDataLoaded: fn(),
+    onCosignComplete: fn(),
+    onDestroySession: fn(),
+  },
+  argTypes: {
+    form: {table: {disable: true}},
+    submission: {table: {disable: true}},
+  },
+  parameters: {
+    msw: {
+      handlers: {
+        summary: [mockSubmissionGet(submission), mockSubmissionSummaryGet()],
+      },
+    },
+  },
+} satisfies Meta<typeof CosignSummary>;
+
+type Story = StoryObj<typeof CosignSummary>;
+
+export const SummaryDataLoaded: Story = {};
