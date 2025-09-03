0718223d792ac0fa0793f95de6f83eac8113515e
robinvandermolen <robin@maykinmedia.nl>
2025-09-03 12:38:40 +0200

    :label: [open-formulieren/open-forms#2179] Updated Map component to use Typescript

---
+++ src/components/Map/types.ts
@@ -0,0 +1,24 @@
+export type Coordinates = [number, number];
+
+type PointGeometry = {
+  type: 'Point';
+  coordinates: Coordinates;
+};
+
+type LineGeometry = {
+  type: 'LineString';
+  coordinates: Coordinates[];
+};
+
+type PolygonGeometry = {
+  type: 'Polygon';
+  coordinates: Coordinates[][];
+};
+
+export type GeoJsonGeometry = PointGeometry | LineGeometry | PolygonGeometry;
+
+export type MapInteractions = {
+  marker: boolean;
+  polygon: boolean;
+  polyline: boolean;
+};
