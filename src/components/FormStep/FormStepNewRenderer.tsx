6dbc4152b31b28c8c9fa70469365136a84dc1acc
Sergei Maertens <sergei@maykinmedia.nl>
2025-09-04 12:53:44 +0200

    :alembic: [open-formulieren/open-forms#5133] Add experimental new form step implementation
    The new form step implementation is based on our own Formio renderer
    and now in experimental preview. The core functionality should be
    covered:
    
    * navigating to previous step and displaying the current position
    * loading and displaying the step data from the backend
    * submitting (and validating) the step data to the backend, both for
      normal submission and suspending the form
    * changes in form/submission data trigger logic evaluation from the
      backend
    * logic check results are processed:
      - updates to the formio configuration because of logic are applied
      - updates to the step data because of logic are applied
    * form step save modal works
    
    Notable things that don't work yet:
    
    * email verification, as this requires changes in the renderer to
      support this. See also open-formulieren/open-forms#5328 for the UX
      improvements related to this.
    * a bunch of component types are not yet supported in the renderer
    * adding/removing components from the formio configuration causes
      crashes. This should not pose a problem because we don't allow such
      operations via logic, only mutations of the configuration (toggle
      hidden state, toggle required state... etc)
    
    The stories are also broken up/organized a bit differently to declutter:
    * the form navigation button states/labels are split off in a separate
      directory. The interaction between submission state and component
      props is important enough to keep these stories around
    * a separate directory for the new renderer so that we can compare
      behaviour of legacy vs. new in the same storybook
    * added some more explicit stories/tests for the logic evaluation
      routines
    
    Some thoughts that haven't been addressed yet:
    
    * should we check the abort controller again after the logic check has
      completed? it kind of depends whether we observe race conditions or
      not in appying the result. Formio.js is very slow to update and
      requires the full submission data to be provided, our own renderer
      can handle partial updates properly
    * we need to come up with a way to get access to the components with
      validation errors, so we can exclude that from the data being sent
      to the backend for the logic check. this is turning a bit into a
      leaky abstraction, but it already is currently to figure out the
      valid/validating/submitting state anyway. I think we can manage it
      for now as long as it stays contained - the tradeoff with technical
      complexity in the renderer is not a nice one.

---
+++ src/components/FormStep/FormStepNewRenderer.tsx
@@ -0,0 +1,227 @@
+import {FormioForm} from '@open-formulieren/formio-renderer';
+import type {FormStateRef} from '@open-formulieren/formio-renderer/components/FormioForm.js';
+import {JSONObject} from '@open-formulieren/formio-renderer/types.js';
+import type {AnyComponentSchema} from '@open-formulieren/types';
+import isEqual from 'fast-deep-equal';
+import {useFormikContext} from 'formik';
+import {useCallback, useRef, useState} from 'react';
+import {useNavigate, useNavigation} from 'react-router';
+
+import {LiteralsProvider} from 'components/Literal';
+import FormStepSaveModal from 'components/modals/FormStepSaveModal';
+
+import {get} from '@/api';
+import {useDebugContext} from '@/components/AppDebug';
+import Card, {CardTitle} from '@/components/Card';
+import FormNavigation, {StepSubmitButton} from '@/components/FormNavigation';
+import type {FormNavigationProps} from '@/components/FormNavigation/FormNavigation';
+import Loader from '@/components/Loader';
+import PreviousLink from '@/components/PreviousLink';
+import {assertSubmission, useSubmissionContext} from '@/components/SubmissionProvider';
+import {type SubmissionStep, saveStepData} from '@/data/submission-steps';
+import type {Submission} from '@/data/submissions';
+import {ValidationError} from '@/errors';
+import useFormContext from '@/hooks/useFormContext';
+
+import Progress from './Progress';
+import {useCheckStepLogic, useLoadStep, useResolveStepUrl} from './hooks';
+import {StepState} from './utils';
+
+/**
+ * Route component for a single step in the form.
+ *
+ * The core functionality is:
+ *   - rendering the Formio definition specified from the backend
+ *   - reacting to user input changes by firing backend logic check calls
+ *   - submitting the user input to the server and proceed to the next step
+ */
+const FormStepNewRenderer: React.FC = () => {
+  const {state: navigationState} = useNavigation();
+  const navigate = useNavigate();
+  const form = useFormContext();
+  const {setStepValues: setDebugStepValues} = useDebugContext();
+  const {submission, onSubmissionObtained, onDestroySession} = useSubmissionContext();
+  assertSubmission(submission);
+
+  const [stepSaveModalOpen, setStepSaveModalOpen] = useState<boolean>(false);
+
+  const [components, setComponents] = useState<AnyComponentSchema[]>([]);
+  const formRef = useRef<FormStateRef>(null);
+  // keep track of the current values in a mutable ref to avoid re-renders when values
+  // change, which take time and can produce 'lag'. We typically only need to read the
+  // values in callbacks at this component level (in the tree), while the `FormioForm`
+  // component itself is responsible for managing the state.
+  const valuesRef = useRef<JSONObject | null>(null);
+  const onStepLoaded = useCallback(
+    (step: SubmissionStep) => {
+      valuesRef.current = step.data;
+      setComponents(step.formStep.configuration.components);
+      setDebugStepValues(step.data, true);
+    },
+    [setDebugStepValues]
+  );
+
+  const {formStep, submissionStep: sparseStep} = useResolveStepUrl(form, submission);
+  const state = useLoadStep(sparseStep.url, onStepLoaded);
+
+  /**
+   * Process the logic check result.
+   *
+   * The updated components from the logic check result (new) form configuration is
+   * compared (deep-equality) against the current components/form definition in the
+   * state. Only if there are differences is the state updated, which drives updating
+   * the rendered form via `FormioForm`.
+   *
+   * Similarly, the backend replies only with a diff of data to update, so we only need
+   * to update the values if there's actually any work to do.
+   */
+  const onLogicCheckResult = (updatedSubmission: Submission, updatedStep: SubmissionStep) => {
+    onSubmissionObtained(updatedSubmission);
+    // update the components that may be updated by backend logic
+    const newComponents = updatedStep.formStep.configuration.components;
+    if (!isEqual(newComponents, components)) {
+      setComponents(updatedStep.formStep.configuration.components);
+    }
+
+    const updatedValues = updatedStep.data;
+    if (updatedValues && Object.keys(updatedValues).length) {
+      formRef.current?.updateValues(updatedValues);
+    }
+  };
+  const {scheduleLogicCheck, inProgress: logicCheckInProgress} = useCheckStepLogic(
+    sparseStep.url,
+    valuesRef,
+    onLogicCheckResult
+  );
+
+  if (state.error) throw state.error;
+  const step = state.value;
+
+  const isLoading = state.loading || navigationState !== 'idle';
+
+  // check our current position in the form
+  const stepState = new StepState(form, submission, formStep);
+  const {previousTo, isLastStep} = stepState;
+
+  return (
+    <LiteralsProvider literals={formStep.literals}>
+      <Card title={form.name} mobileHeaderHidden>
+        <PreviousLink to={previousTo} position="start" />
+        <Progress form={form} submission={submission} currentStep={sparseStep} />
+        <CardTitle title={sparseStep.name} headingType="subtitle" padded />
+
+        {isLoading && <Loader modifiers={['centered']} />}
+
+        {!isLoading && (
+          <FormioForm
+            ref={formRef}
+            components={components}
+            values={step!.data ?? undefined}
+            onChange={values => {
+              valuesRef.current = values;
+              scheduleLogicCheck();
+              setDebugStepValues(values, false);
+            }}
+            onSubmit={async values => {
+              try {
+                // XXX: do we still need the validate endpoint? Perhaps this could use
+                // a header for the PUT endpoint instead?
+                await saveStepData(sparseStep.url, values, {skipValidation: false});
+              } catch (error: unknown) {
+                // rethrow what we can't handle
+                if (!(error instanceof ValidationError)) {
+                  throw error;
+                }
+
+                const {initialErrors: serverErrors} = error.asFormikProps();
+                formRef.current?.updateErrors(serverErrors);
+                return;
+              }
+
+              // refresh the submission
+              const updatedSubmission = (await get<Submission>(submission.url))!;
+              onSubmissionObtained(updatedSubmission);
+
+              // we must use the refreshed submission to get the update applicable states
+              const {nextTo} = new StepState(form, updatedSubmission, formStep);
+              navigate(nextTo);
+            }}
+            requiredFieldsWithAsterisk={form.requiredFieldsWithAsterisk}
+          >
+            <FormStepNavigation
+              submissionAllowed={submission.submissionAllowed}
+              isLastStep={isLastStep}
+              isCheckingLogic={logicCheckInProgress}
+              onFormSave={
+                form.suspensionAllowed
+                  ? event => {
+                      event.preventDefault();
+                      setStepSaveModalOpen(true);
+                    }
+                  : undefined
+              }
+              previousPage={previousTo}
+              isAuthenticated={submission.isAuthenticated}
+              onDestroySession={onDestroySession}
+            />
+          </FormioForm>
+        )}
+      </Card>
+
+      <FormStepSaveModal
+        isOpen={stepSaveModalOpen}
+        closeModal={() => setStepSaveModalOpen(false)}
+        onSaveConfirm={async () => {
+          const values = valuesRef.current;
+          if (values === null) throw new Error('Cannot save non-existent values');
+          // XXX: check what happens if invalidly shaped data is submitted during save!
+          await saveStepData(sparseStep.url, values, {skipValidation: true});
+        }}
+        onSessionDestroyed={onDestroySession}
+        suspendFormUrl={`${submission.url}/_suspend`}
+        suspendFormUrlLifetime={form.resumeLinkLifetime}
+        submissionId={submission.id}
+      />
+    </LiteralsProvider>
+  );
+};
+
+interface FormStepNavigationProps
+  extends Pick<
+    FormNavigationProps,
+    'onFormSave' | 'previousPage' | 'isAuthenticated' | 'onDestroySession'
+  > {
+  submissionAllowed: Submission['submissionAllowed'];
+  isLastStep: boolean;
+  isCheckingLogic: boolean;
+}
+
+const FormStepNavigation: React.FC<FormStepNavigationProps> = ({
+  submissionAllowed,
+  isLastStep,
+  isCheckingLogic,
+  onFormSave,
+  previousPage,
+  isAuthenticated,
+  onDestroySession,
+}) => {
+  const {isValid, isValidating, isSubmitting} = useFormikContext<JSONObject>();
+  return (
+    <FormNavigation
+      submitButton={
+        <StepSubmitButton
+          canSubmitForm={submissionAllowed}
+          canSubmitStep={isValid && !isValidating && !isSubmitting}
+          isLastStep={isLastStep}
+          isCheckingLogic={isCheckingLogic || isSubmitting}
+        />
+      }
+      onFormSave={onFormSave}
+      previousPage={previousPage}
+      isAuthenticated={isAuthenticated}
+      onDestroySession={onDestroySession}
+    />
+  );
+};
+
+export default FormStepNewRenderer;
