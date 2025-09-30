08c646aa2bf18f0f618e49fe8af29a80dd8f8d4a
robinvandermolen <robin@maykinmedia.nl>
2025-09-30 09:14:34 +0200

    :sparkles: [open-formulieren/open-forms#5576] Add WFS layers to overlays control
    Leaflet doesn't support WFS layers out of the box. Contrary to WMS layers, WFS need to be rendered as geojson objects.
    
    Our custom WFS tile layer component fetches the WFS data when moving the map, renders it as geojson into a LayerGroup, which is then placed in the map. By rendering it into an actual LayerGroup component, we can still toggle the layer using the `LeafletLayersControl.Overlay`.
    
    PDOK, a popular Dutch provider for map layer data, doesn't support multiple Feature types when requesting the output to be json. To still support one overlay with multiple feature types, we have to spread the WFS data request to multiple individual requests, and merge the results into one LayerGroup.

--- src/components/Map/LeafletMapLayersControl.tsx
+++ src/components/Map/LeafletMapLayersControl.tsx
@@ -1,5 +1,12 @@
 import type {MapComponentSchema} from '@open-formulieren/types';
-import {LayersControl as LeafletLayersControl, WMSTileLayer} from 'react-leaflet';
+import * as Leaflet from 'leaflet';
+import {useEffect, useRef} from 'react';
+import {
+  LayerGroup,
+  LayersControl as LeafletLayersControl,
+  WMSTileLayer,
+  useMap,
+} from 'react-leaflet';
 
 import './LeafletMapLayersControl.scss';
 import type {Overlay} from './types';
@@ -22,10 +29,88 @@ const WMSTileLayerControl: React.FC<TileLayerControlProps> = ({index, overlay}) 
   </LeafletLayersControl.Overlay>
 );
 
+interface WFSTileLayerProps {
+  url: string;
+  featureTypes: string[];
+}
+
+const WFSTileLayer: React.FC<WFSTileLayerProps> = ({url, featureTypes}) => {
+  const map = useMap();
+  const groupRef = useRef<Leaflet.LayerGroup>(null);
+
+  useEffect(() => {
+    if (!groupRef.current) return;
+    const group = groupRef.current;
+
+    const fetchData = async () => {
+      const bounds = map.getBounds();
+      const bbox = `${bounds.getSouth()},${bounds.getWest()},${bounds.getNorth()},${bounds.getEast()},EPSG:4326`;
+
+      const baseUrl = new URL(url).href.split('?')[0];
+
+      // PDOK doesn't support multiple feature types in one request, for the outputFormat
+      // JSON. So we spread them out to multiple requests, that are then merged into one
+      // layer.
+      const requests = featureTypes.map(async typeName => {
+        const requestUrl =
+          `${baseUrl}?service=WFS&version=2.0.0&request=GetFeature&typeNames=${typeName}` +
+          `&outputFormat=application/json&srsName=urn:ogc:def:crs:EPSG::4326&bbox=${bbox}`;
+        const response = await fetch(requestUrl);
+        return await response.json();
+      });
+
+      // Remove the previous WFS shapes, and add the new ones
+      group.clearLayers();
+
+      const results = await Promise.all(requests);
+      results.forEach(data => {
+        Leaflet.geoJSON(data, {
+          // Styling for features/pins
+          pointToLayer: (_, latlng) =>
+            Leaflet.marker(latlng, {
+              icon: Leaflet.divIcon({
+                className:
+                  'openforms-leaflet-map-overlay-feature openforms-leaflet-map-overlay-feature--marker',
+              }),
+            }),
+          // Styling for polylines and polygons
+          style: {
+            className:
+              'openforms-leaflet-map-overlay-feature openforms-leaflet-map-overlay-feature--poly',
+          },
+        }).addTo(group);
+      });
+    };
+
+    // Fetch new WFS overlay data after moving the map
+    map.on('moveend', fetchData);
+    fetchData();
+
+    // Add LayerGroup to map
+    group.addTo(map);
+
+    return () => {
+      map.off('moveend', fetchData);
+      map.removeLayer(group);
+    };
+  }, [map, url, featureTypes]);
+
+  return <LayerGroup ref={groupRef} />;
+};
+
+const WFSTileLayerControl: React.FC<TileLayerControlProps> = ({index, overlay}) => {
+  return (
+    <LeafletLayersControl.Overlay name={overlay.label} key={index} checked>
+      <WFSTileLayer url={overlay.url} featureTypes={overlay.layers} />
+    </LeafletLayersControl.Overlay>
+  );
+};
+
 export const overlayTileLayerControls: Partial<
   Record<Overlay['type'], React.ElementType<TileLayerControlProps>>
 > = {
   wms: WMSTileLayerControl,
+  wfs: WFSTileLayerControl,
 };
 
 interface LayersControlProps {
