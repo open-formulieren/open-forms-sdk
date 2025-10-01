389a954fdd54b07d68701d865050d8cd07821f35
robinvandermolen <robin@maykinmedia.nl>
2025-10-01 11:44:18 +0200

    :sparkles: [open-formulieren/open-forms#5576] Add WFS feature popup
    The WFS features are now clickable, triggering a popup showing all its properties.
    
    Because we have to render the WFS geometry using the base Leaflet (instead of the React adaptation), we have to render the popup content as html string.

--- src/components/Map/LeafletMapLayersControl.tsx
+++ src/components/Map/LeafletMapLayersControl.tsx
@@ -1,6 +1,9 @@
 import type {MapComponentSchema} from '@open-formulieren/types';
+import type {Feature, FeatureCollection} from 'geojson';
 import * as Leaflet from 'leaflet';
-import {useEffect, useRef} from 'react';
+import {useCallback, useEffect, useRef} from 'react';
+import ReactDOMServer from 'react-dom/server';
+import {useIntl} from 'react-intl';
 import {
   LayerGroup,
   LayersControl as LeafletLayersControl,
@@ -36,36 +39,60 @@ interface WFSTileLayerProps {
 
 const WFSTileLayer: React.FC<WFSTileLayerProps> = ({url, featureTypes}) => {
   const map = useMap();
+  const intl = useIntl();
   const groupRef = useRef<Leaflet.LayerGroup>(null);
+
+  const getPopupContent = (feature: Feature): string => {
+    const popupContent = (
+      <table className="openforms-leaflet-map-overlay-popup-table">
+        {Object.entries(feature?.properties || {}).map(([key, value]) => (
+          <tr key={key}>
+            <th className="openforms-leaflet-map-overlay-popup-table__item-key">{key}</th>
+            <td className="openforms-leaflet-map-overlay-popup-table__item-value">
+              {value || '-'}
+            </td>
+          </tr>
+        ))}
+      </table>
+    );
+    return ReactDOMServer.renderToString(popupContent);
+  };
+
+  const fetchFeatureCollections = useCallback(async (): Promise<FeatureCollection[]> => {
+    const bounds = map.getBounds();
+    const bbox = `${bounds.getSouth()},${bounds.getWest()},${bounds.getNorth()},${bounds.getEast()},EPSG:4326`;
+
+    const baseUrl = new URL(url).href.split('?')[0];
+
+    // PDOK doesn't support multiple feature types in one request, for the outputFormat
+    // JSON. So we spread them out to multiple requests, that are then merged into one
+    // layer.
+    const requests = featureTypes.map(async typeName => {
+      const requestUrl =
+        `${baseUrl}?service=WFS&version=2.0.0&request=GetFeature&typeNames=${typeName}` +
+        `&outputFormat=application/json&srsName=urn:ogc:def:crs:EPSG::4326&bbox=${bbox}`;
+      const response = await fetch(requestUrl);
+      return await response.json();
+    });
+
+    return Promise.all(requests);
+  }, [featureTypes, map, url]);
 
   useEffect(() => {
     if (!groupRef.current) return;
     const group = groupRef.current;
 
-    const fetchData = async () => {
-      const bounds = map.getBounds();
-      const bbox = `${bounds.getSouth()},${bounds.getWest()},${bounds.getNorth()},${bounds.getEast()},EPSG:4326`;
+    const updateFeatureCollections = async () => {
+      const featureCollections = await fetchFeatureCollections();
 
-      const baseUrl = new URL(url).href.split('?')[0];
-
-      // PDOK doesn't support multiple feature types in one request, for the outputFormat
-      // JSON. So we spread them out to multiple requests, that are then merged into one
-      // layer.
-      const requests = featureTypes.map(async typeName => {
-        const requestUrl =
-          `${baseUrl}?service=WFS&version=2.0.0&request=GetFeature&typeNames=${typeName}` +
-          `&outputFormat=application/json&srsName=urn:ogc:def:crs:EPSG::4326&bbox=${bbox}`;
-        const response = await fetch(requestUrl);
-        return await response.json();
-      });
-
-      // Remove the previous WFS shapes, and add the new ones
+      // Remove the previous WFS shapes
       group.clearLayers();
 
-      const results = await Promise.all(requests);
-      results.forEach(data => {
-        Leaflet.geoJSON(data, {
-          // Styling for features/pins
+      // Mount the new WFS shapes
+      featureCollections.forEach(featureCollection => {
+        // We have to use the base Leaflet functions, because the React version of
+        // Leaflet gets very confused when we try to update the WFS data.
+        Leaflet.geoJSON(featureCollection, {
           pointToLayer: (_, latlng) =>
             Leaflet.marker(latlng, {
               icon: Leaflet.divIcon({
@@ -73,27 +100,31 @@ const WFSTileLayer: React.FC<WFSTileLayerProps> = ({url, featureTypes}) => {
                   'openforms-leaflet-map-overlay-feature openforms-leaflet-map-overlay-feature--marker',
               }),
             }),
-          // Styling for polylines and polygons
           style: {
             className:
               'openforms-leaflet-map-overlay-feature openforms-leaflet-map-overlay-feature--poly',
+          },
+          onEachFeature: (feature, layer) => {
+            layer.bindPopup(getPopupContent(feature), {
+              maxHeight: 200,
+            });
           },
         }).addTo(group);
       });
     };
 
     // Fetch new WFS overlay data after moving the map
-    map.on('moveend', fetchData);
-    fetchData();
+    map.on('moveend', updateFeatureCollections);
+    updateFeatureCollections();
 
     // Add LayerGroup to map
     group.addTo(map);
 
     return () => {
-      map.off('moveend', fetchData);
+      map.off('moveend', updateFeatureCollections);
       map.removeLayer(group);
     };
-  }, [map, url, featureTypes]);
+  }, [map, url, featureTypes, intl, fetchFeatureCollections]);
 
   return <LayerGroup ref={groupRef} />;
 };
