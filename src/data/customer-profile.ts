aca6cf0e1ba05d551ab462287e3f8481962372ce
robinvandermolen <robin@maykinmedia.nl>
2025-11-25 15:53:37 +0100

    :building_construction: [formio-renderer#178] Add component parameters for customerProfile component

---
+++ src/data/customer-profile.ts
@@ -0,0 +1,28 @@
+import type {DigitalAddressType} from '@open-formulieren/types';
+
+import {get} from '@/api';
+import {logError} from '@/components/Errors';
+
+export interface DigitalAddressGroup {
+  type: DigitalAddressType;
+  addresses: string[];
+  preferred?: string;
+}
+
+type FetchCommunicationPreferencesResult = DigitalAddressGroup[];
+
+export const fetchCommunicationPreferences = async (
+  baseUrl: string,
+  submissionId: string,
+  componentKey: string
+): Promise<FetchCommunicationPreferencesResult | null> => {
+  try {
+    const result = await get<FetchCommunicationPreferencesResult>(
+      `${baseUrl}prefill/plugins/customer-interactions/communication-preferences/${submissionId}/component/${componentKey}`
+    );
+    return result!;
+  } catch (error) {
+    logError(error);
+    return null;
+  }
+};
