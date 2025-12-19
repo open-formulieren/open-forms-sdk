bfb0cfce8da5224ba1b25ed222873b290170d68a
Sergei Maertens <sergei@maykinmedia.nl>
2025-12-19 10:33:26 +0100

    :arrow_up: Upgrade to latest renderer beta
    See also: 155fb895e37fa9e0191e2b2eb5af7f3a74d95e34
    
    Co-authored-by: robinvandermolen <robin@maykinmedia.nl>

--- src/data/customer-profile.ts
+++ src/data/customer-profile.ts
@@ -3,24 +3,32 @@ import type {DigitalAddressType} from '@open-formulieren/types';
 import {get} from '@/api';
 import {logError} from '@/components/Errors';
 
-export interface DigitalAddressGroup {
+/**
+ * @see `#/components/schemas/CommunicationPreferences` in the API spec.
+ */
+export interface CommunicationPreferences {
   type: DigitalAddressType;
-  addresses: string[];
-  preferred?: string;
+  options: string[];
+  preferred: string | null;
 }
 
-type FetchCommunicationPreferencesResult = DigitalAddressGroup[];
+type FetchCommunicationPreferencesResult = CommunicationPreferences[];
+
+type NormalizedCommunicationPreferences = Omit<CommunicationPreferences, 'preferred'> & {
+  preferred: string;
+};
 
 export const fetchCommunicationPreferences = async (
   baseUrl: string,
   submissionId: string,
   componentKey: string
-): Promise<FetchCommunicationPreferencesResult | null> => {
+): Promise<NormalizedCommunicationPreferences[] | null> => {
   try {
     const result = await get<FetchCommunicationPreferencesResult>(
       `${baseUrl}prefill/plugins/customer-interactions/communication-preferences/${submissionId}/component/${componentKey}`
     );
-    return result!;
+
+    return result!.map(prefs => ({...prefs, preferred: prefs.preferred ?? ''}));
   } catch (error) {
     logError(error);
     return null;
