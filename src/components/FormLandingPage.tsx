45b9ea79c8d25b4ba2e7357b8fc56bb0905ea584
annashamray <anna.shamray@maykinmedia.nl>
2026-01-28 16:43:47 +0100

    :sparkles: [https://github.com/open-formulieren/open-forms/issues/5319] add useAuthVisible hook to add 'auth_visible' query param to the inroductory and start pages in case of hard refresh

--- src/components/FormLandingPage.tsx
+++ src/components/FormLandingPage.tsx
@@ -1,13 +1,17 @@
 import {Navigate} from 'react-router';
 
+import useAuthVisible from '@/hooks/useAuthVisible';
 import useFormContext from '@/hooks/useFormContext';
 import useInitialDataReference from '@/hooks/useInitialDataReference';
 
 const FormLandingPage: React.FC = () => {
   const {introductionPageContent = ''} = useFormContext();
   const {addInitialDataReference} = useInitialDataReference();
+  const {addToUrl: addAuthVisible} = useAuthVisible();
   const startPageUrl = introductionPageContent ? 'introductie' : 'startpagina';
-  return <Navigate replace to={addInitialDataReference(startPageUrl)} />;
+
+  const targetUrl = addAuthVisible(addInitialDataReference(startPageUrl));
+  return <Navigate replace to={targetUrl} />;
 };
 
 export default FormLandingPage;
--- src/hooks/useInitialDataReference.ts
+++ src/hooks/useInitialDataReference.ts
@@ -1,4 +1,4 @@
-import {useSearchParams} from 'react-router';
+import useQueryParam from '@/hooks/useQueryParam';
 
 export const PARAM_NAME = 'initial_data_reference';
 
@@ -12,32 +12,12 @@ export interface UseInitialDataReferenceResult {
  * (new) URLs.
  */
 const useInitialDataReference = (): UseInitialDataReferenceResult => {
-  const [searchParams] = useSearchParams();
-  const initialDataReference: string | null = searchParams?.get(PARAM_NAME);
+  const {value, addToUrl} = useQueryParam(PARAM_NAME);
 
   return {
-    initialDataReference,
-    addInitialDataReference: (url: string) =>
-      setInitialDataReferenceParameter(url, initialDataReference),
+    initialDataReference: value,
+    addInitialDataReference: addToUrl,
   };
-};
-
-/**
- * Set (or remove) the initial data reference parameter on the (relative) URL.
- * @param  url       The (relative) path, as you would pass it to react router.
- * @param  reference The initial data reference. If empty, the parameter will be removed.
- */
-const setInitialDataReferenceParameter = (url: string, reference: string | null): string => {
-  // URL handling in JS requires a proper base since you can't just feed `foo` or `/foo`
-  // to the constructor. We only extract the pathname + query string again at the end.
-  const base = window.location.origin;
-  const parsedUrl = new URL(url, base);
-  if (reference) {
-    parsedUrl.searchParams.set(PARAM_NAME, reference);
-  } else {
-    parsedUrl.searchParams.delete(PARAM_NAME);
-  }
-  return `${parsedUrl.pathname}${parsedUrl.search}`;
 };
 
 export default useInitialDataReference;
