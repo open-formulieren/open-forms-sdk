7f74fba0370eca307059d9c95d43c83228efc8be
Sergei Maertens <sergei@maykinmedia.nl>
2026-01-29 15:44:32 +0100

    :alien: Ensure the storybook-react-intl decorators are installed in vitest browser mode
    Not sure why these run in a real browser but not in vitest
    browser mode...

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,7 +1,13 @@
 import {setProjectAnnotations} from '@storybook/react-vite';
+import * as reactIntlAnnotations from 'storybook-react-intl/preview';
+import {beforeAll} from 'vitest';
 
 import * as projectAnnotations from './.storybook/preview';
 
 // This is an important step to apply the right configuration when testing your stories.
 // More info at: https://storybook.js.org/docs/api/portable-stories/portable-stories-vitest#setprojectannotations
-setProjectAnnotations([projectAnnotations]);
+const annotations = setProjectAnnotations([reactIntlAnnotations, projectAnnotations]);
+
+console.log(annotations);
+
+beforeAll(annotations.beforeAll);
