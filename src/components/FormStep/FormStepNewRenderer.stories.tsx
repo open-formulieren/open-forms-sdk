2b66bff2821a16d0e277416b86a2dd23c4c6d37f
Sergei Maertens <sergei@maykinmedia.nl>
2026-05-01 15:56:39 +0200

    :white_check_mark: [open-formulieren/open-forms#6245] Add test for expected address derivation behaviour

--- src/components/FormStep/FormStepNewRenderer.stories.tsx
+++ src/components/FormStep/FormStepNewRenderer.stories.tsx
@@ -3,6 +3,7 @@ import {reactRouterParameters, withRouter} from 'storybook-addon-remix-react-rou
 import {expect, fn, userEvent, waitFor, within} from 'storybook/test';
 
 import {BASE_URL, buildForm, buildSubmission, buildSubmissionStep} from '@/api-mocks';
+import {mockAddressAutoComplete} from '@/api-mocks/geo';
 import {
   mockSubmissionCheckLogicPost,
   mockSubmissionGet,
@@ -16,7 +17,9 @@ import {
 } from '@/components/EmailVerification/mocks';
 import SubmissionProvider, {type SubmissionContextType} from '@/components/SubmissionProvider';
 import {withForm} from '@/sb-decorators';
+import {sleep} from '@/utils';
 
+import {LOCATION_AUTOCOMPLETE_DEBOUNCE} from './AddressAutoFillObservers';
 import FormStepNewRenderer from './FormStepNewRenderer';
 
 const DEFAULT_SUBMISSION = buildSubmission();
@@ -493,5 +496,201 @@ export const SummaryProgressNotVisible: Story = {
     const canvas = within(canvasElement);
 
     expect(canvas.queryByText(/Stap 1 van 1/)).toBeNull();
+  },
+};
+
+const LOCATION_DERIVATION_SUBMISSION_STEP_DETAIL_BODY = buildSubmissionStep({
+  components: [
+    {
+      id: 'postcode',
+      type: 'postcode',
+      key: 'postcode',
+      label: 'Postcode',
+      validate: {pattern: '^[1-9][0-9]{3} ?(?!sa|sd|ss|SA|SD|SS)[a-zA-Z]{2}$'},
+    },
+    {
+      id: 'houseNumber',
+      type: 'number',
+      key: 'houseNumber',
+      label: 'House number',
+    },
+    {
+      id: 'street',
+      type: 'textfield',
+      key: 'street',
+      label: 'Street',
+      deriveStreetName: true,
+      derivePostcode: 'postcode',
+      deriveHouseNumber: 'houseNumber',
+    },
+    {
+      id: 'city',
+      type: 'textfield',
+      key: 'city',
+      label: 'City',
+      deriveCity: true,
+      derivePostcode: 'postcode',
+      deriveHouseNumber: 'houseNumber',
+    },
+    {
+      id: 'editgrid',
+      type: 'editgrid',
+      key: 'editgrid',
+      label: 'Repeating group',
+      groupLabel: 'Address',
+      disableAddingRemovingRows: false,
+      components: [
+        {
+          id: 'postcode2',
+          type: 'postcode',
+          key: 'postcode2',
+          label: 'Nested postcode',
+          validate: {pattern: '^[1-9][0-9]{3} ?(?!sa|sd|ss|SA|SD|SS)[a-zA-Z]{2}$'},
+        },
+        {
+          id: 'houseNumber2',
+          type: 'number',
+          key: 'houseNumber2',
+          label: 'Nested house number',
+        },
+        {
+          id: 'street2',
+          type: 'textfield',
+          key: 'street2',
+          label: 'Nested street',
+          deriveStreetName: true,
+          derivePostcode: 'editgrid.postcode',
+          deriveHouseNumber: 'editgrid.houseNumber',
+        },
+        {
+          id: 'city2',
+          type: 'textfield',
+          key: 'city2',
+          label: 'Nested city',
+          deriveCity: true,
+          derivePostcode: 'editgrid.postcode',
+          deriveHouseNumber: 'editgrid.houseNumber',
+        },
+      ],
+    },
+  ],
+});
+
+export const LocationDerivation: Story = {
+  parameters: {
+    msw: {
+      handlers: {
+        submissionStep: [
+          mockSubmissionStepGet(LOCATION_DERIVATION_SUBMISSION_STEP_DETAIL_BODY),
+          mockSubmissionStepPut(LOCATION_DERIVATION_SUBMISSION_STEP_DETAIL_BODY, 201),
+        ],
+        logicCheck: [
+          mockSubmissionCheckLogicPost(
+            DEFAULT_SUBMISSION,
+            LOCATION_DERIVATION_SUBMISSION_STEP_DETAIL_BODY,
+            400
+          ),
+        ],
+        addressAutoComplete: [
+          mockAddressAutoComplete({streetName: 'Kingsfordweg', city: 'Amsterdam'}),
+        ],
+      },
+    },
+  },
+  play: async ({canvasElement}) => {
+    const canvas = within(canvasElement);
+
+    const postcode = await canvas.findByLabelText('Postcode');
+    await userEvent.type(postcode, '1234 AB');
+    const houseNumber = canvas.getByLabelText('House number');
+    await userEvent.type(houseNumber, '99');
+
+    await waitFor(() => {
+      expect(canvas.getByLabelText('Street')).toHaveDisplayValue('Kingsfordweg');
+      expect(canvas.getByLabelText('City')).toHaveDisplayValue('Amsterdam');
+    });
+  },
+};
+
+export const LocationDerivationDoesNotOverwriteValues: Story = {
+  parameters: {
+    msw: {
+      handlers: {
+        submissionStep: [
+          mockSubmissionStepGet(LOCATION_DERIVATION_SUBMISSION_STEP_DETAIL_BODY),
+          mockSubmissionStepPut(LOCATION_DERIVATION_SUBMISSION_STEP_DETAIL_BODY, 201),
+        ],
+        logicCheck: [
+          mockSubmissionCheckLogicPost(
+            DEFAULT_SUBMISSION,
+            LOCATION_DERIVATION_SUBMISSION_STEP_DETAIL_BODY,
+            400
+          ),
+        ],
+        addressAutoComplete: [
+          mockAddressAutoComplete({streetName: 'Kingsfordweg', city: 'Amsterdam'}),
+        ],
+      },
+    },
+  },
+  play: async ({canvasElement}) => {
+    const canvas = within(canvasElement);
+
+    // fill out some dummy values in the street name & city fields that should not be
+    // cleared
+    const street = await canvas.findByLabelText('Street');
+    await userEvent.type(street, 'Stationsstraat');
+    const city = await canvas.findByLabelText('City');
+    await userEvent.type(city, 'Vlerckendam');
+
+    // now fill out postcode & house nummer as well - we expect the values not to change
+    const postcode = await canvas.findByLabelText('Postcode');
+    await userEvent.type(postcode, '1234 AB');
+    const houseNumber = canvas.getByLabelText('House number');
+    await userEvent.type(houseNumber, '99');
+
+    // wait for the debounce to complete
+    await sleep(LOCATION_AUTOCOMPLETE_DEBOUNCE + 20);
+
+    await waitFor(() => {
+      expect(canvas.getByLabelText('Street')).toHaveDisplayValue('Stationsstraat');
+      expect(canvas.getByLabelText('City')).toHaveDisplayValue('Vlerckendam');
+    });
+  },
+};
+
+export const LocationDerivationInRepeatingGroup: Story = {
+  parameters: {
+    msw: {
+      handlers: {
+        submissionStep: [
+          mockSubmissionStepGet(LOCATION_DERIVATION_SUBMISSION_STEP_DETAIL_BODY),
+          mockSubmissionStepPut(LOCATION_DERIVATION_SUBMISSION_STEP_DETAIL_BODY, 201),
+        ],
+        logicCheck: [
+          mockSubmissionCheckLogicPost(
+            DEFAULT_SUBMISSION,
+            LOCATION_DERIVATION_SUBMISSION_STEP_DETAIL_BODY,
+            400
+          ),
+        ],
+        addressAutoComplete: [
+          mockAddressAutoComplete({streetName: 'Kingsfordweg', city: 'Amsterdam'}),
+        ],
+      },
+    },
+  },
+  play: async ({canvasElement}) => {
+    const canvas = within(canvasElement);
+
+    await userEvent.click(await canvas.findByRole('button', {name: 'Nog één toevoegen'}));
+
+    const postcode = await canvas.findByLabelText('Nested postcode');
+    await userEvent.type(postcode, '1234 AB');
+    const houseNumber = canvas.getByLabelText('Nested house number');
+    await userEvent.type(houseNumber, '99');
+    await userEvent.tab();
+
+    await userEvent.click(await canvas.findByRole('button', {name: 'Opslaan'}));
   },
 };
