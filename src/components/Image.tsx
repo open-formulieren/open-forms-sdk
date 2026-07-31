ad5e5b7d0ce0fee4359ad1dc5a36af00631e1341
Sergei Maertens <sergei@maykinmedia.nl>
2026-07-31 10:34:23 +0200

    :sparkles: [open-formulieren/open-forms#5787] Support additional classnames and image inline-size design token
    More specific image variants can provide their own class name to
    override the inline size of the image through a design token,
    rather than being stuck with a hardcoded value.

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,12 +1,14 @@
 import {Image as UtrechtImage} from '@utrecht/component-library-react';
+import {clsx} from 'clsx';
 
 export interface ImageProps {
   src: string;
   alt?: string;
+  className?: string;
 }
 
-const Image: React.FC<ImageProps> = ({src, alt = ''}) => {
-  return <UtrechtImage className="openforms-image" src={src} alt={alt} />;
+const Image: React.FC<ImageProps> = ({src, alt = '', className}) => {
+  return <UtrechtImage className={clsx('openforms-image', className)} src={src} alt={alt} />;
 };
 
 export default Image;
